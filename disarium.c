6974f28d61c580f5e3c8186441f51bc07d64ff60
ashish4321 <ashishagarwal2223@gmail.com>
2017-09-19 10:35:53 +0530

    Update disarium.c
    added comments and indented properly

--- disarium.c
+++ disarium.c
@@ -1,21 +1,22 @@
+// a disarium number is a number such that sum of the digits of the number raised to their respective position equals the number
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
 
 int main() 
 {
  int n,i=0,j,sum=0,l,k;
  scanf("%d",&n);
- k=n;
+ k=n; //number stored in temporary variable
  j=k;
  while(n>0)
  {
   d=n%10;
- n=n/10;
- i++;
-   }
+  n=n/10;
+  i++; //to get the number of digits which will be the position for last digit
+ }
    while(k>0)
    {  
-    l=k%10;
+    l=k%10; //to get the digit
     sum=sum+pow(l,i);
     k=k/10;
     i--;
