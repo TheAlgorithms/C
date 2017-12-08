5ebfcf4148f6bf73e5e60f0b16cbd758482b9c7e
Christian Bender <christianbender89@web.de>
2017-12-08 18:00:49 +0100

    simple modular and generic stack
    See the README.md

---
+++ Data Structures/stack/stack.h
@@ -0,0 +1,43 @@
+/*
+    author: Christian Bender
+
+    This header represents the public stack-interface.
+    The stack is generic and self growing. 
+*/
+
+
+#ifndef __STACK__
+#define __STACK__
+
+/*
+    initStack: initializes the stack with a capacity of 10 elements.
+*/
+void initStack();
+
+/*
+    push: pushs the argument onto the stack 
+*/
+void push(void * object);
+
+/*
+    pop: pops the top element of the stack from the stack.
+    assumes: stack not empty.
+*/
+void  * pop();
+
+/*
+    size: gets the number of elements of the stack.
+*/
+int size();
+
+/*
+    isEmpty(): returns 1 if stack is empty otherwise 0.
+*/
+int isEmpty();
+
+/*
+    top: returns the top element from the stack without removing it.
+*/
+void * top(); 
+
+#endif
