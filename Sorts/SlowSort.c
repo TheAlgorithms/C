63f7b4e9c9aa2e71d9e66e25bcd9fee0b8ff00c8
Naveen Hegde <hegdenaveen1@gmail.com>
2017-12-24 01:16:18 +0530

    Update SlowSort.c

--- Sorts/SlowSort.c
+++ Sorts/SlowSort.c
@@ -9,13 +9,13 @@ void SlowSort(int a[], int i, int j)
     return;
   int m=i+(j-i)/2;
   int temp;
-  slowsort(a, i, m);
-  slowsort(a, m + 1, j);
+  SlowSort(a, i, m);
+  SlowSort(a, m + 1, j);
   if(a[j]<a[m])
   {
     temp=a[j];
     a[j]=a[m];
     a[m]=temp;
   }
-  slowsort(a, i, j - 1);
+  SlowSort(a, i, j - 1);
 }
