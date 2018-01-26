730ebb9053b935627075040d751e71719724176d
Christian Bender <christianbender89@web.de>
2018-01-26 19:38:12 +0100

    Add files via upload

--- exercism/hello-world/hello_world.c
+++ exercism/hello-world/hello_world.c
@@ -1,7 +1,12 @@
 #include "hello_world.h"
+#include <stdlib.h>
+#include <string.h>
 
 const char *hello(void)
 {
+    char * ans = malloc(sizeof(char) * strlen("Hello, World!"));
+    strcpy(ans,"Hello, World!");
+
     /* string is pointer of the first character */
-    return  "Hello, World!";
+    return  ans;
 }
