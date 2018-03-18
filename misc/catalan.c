41dede7144e2eddfd76415b69b2bd9fde03ce25f
ashishtheman <37515765+ashishtheman@users.noreply.github.com>
2018-03-19 01:17:14 +0530

    Create catalan.c

---
+++ misc/catalan.c
@@ -0,0 +1,28 @@
+/*
+code for computing nth catalan number
+*/
+#include<stdio.h>
+long int factorial(int x)    //long int for more than 10 factorial
+{
+     int i;
+     long int fac;   //fac stores x factorial  
+     fac=x;
+     for(i=1;i<x;i++)    //loop to calculate x factorial
+     {
+        fac=fac*(x-i);
+     }
+     return fac;   //returning x factorial
+}
+int main()
+{
+    long int f1,f2,f3;  //long int for more than 10 factorial
+    int n;
+    float C;    //C is catalan number for n;
+    scanf("%d",&n);
+    f1=factorial(2*n);
+    f2=factorial(n+1);
+    f3=factorial(n);
+    C=f1/(f2*f3);   //formula for catalan number for n
+    printf("%0.2f",C);
+    return 0;
+}
