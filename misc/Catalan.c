6c59e330d2696d7aabe7d5a1b3209631e6ca3dd1
Anuj Shah <34171673+anujms1999@users.noreply.github.com>
2018-03-24 02:08:47 +0530

    Create Catalan.c
    Program for printing Catalan numbers. Many problems can be solved easily if we know Catalan numbers and above program can be used to find Catalan

---
+++ misc/Catalan.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+long int catalan(int n)
+{
+    // Base case
+    if (n <= 1) return 1;
+ 
+    // catalan(n) is sum of catalan(i)*catalan(n-i-1)
+    long int res = 0;
+    for (int i=0; i<n; i++)
+        res += catalan(i)*catalan(n-i-1);
+ 
+    return res;
+}
+
+int main() {
+    int n; scanf("%d", &n);
+    
+    printf("%ld", catalan(n));
+    return 0;
+}
