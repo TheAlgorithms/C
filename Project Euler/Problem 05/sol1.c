10283335cbd88627369c8df2062de04baf140e9d
buffet <niclas@countingsort.com>
2018-10-06 15:15:39 +0200

    Added naive implementation for Problem 5

---
+++ Project Euler/Problem 05/sol1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Pretty naive implementation
+// Just checks every number if it's devisable by 1 through 20
+
+static int
+check_number(unsigned long long n)
+{
+	for (unsigned long long i = 1; i <= 20; ++i) {
+		if (n % i != 0) {
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
+int
+main(void)
+{
+	for (unsigned long long n = 1;
+		;
+		++n) {
+		if (check_number(n)) {
+			printf("Result: %llu\n", n);
+			break;
+		}
+	}
+}
