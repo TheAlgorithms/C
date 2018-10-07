6c5e2ad94fe458a3383a095429356b9d6c1addf2
Sambhav <mjaiswl128@gmail.com>
2018-10-07 17:34:26 +0530

    Created a new file of conversion without array.

---
+++ Conversions/decimal-to-binary-without-array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+	int rem, div, num;
+	// Taking large value for i and sum
+	long int i = 1, sum = 0;
+
+	// Asking for the number from user
+	printf("Enter any number");
+	scanf("%d" , &num);
+
+	while(div>0)
+	{
+		rem = num%2;
+		div = num/2;
+		num = div;
+		// Taking the number as a sum in an integer.
+		sum = sum + (rem*i);
+		 i  = i*10;
+	} 
+
+	// Printing the desired value as sum.
+	printf("%ld" , sum);
+}
