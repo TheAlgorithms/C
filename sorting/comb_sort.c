dd62175b6e5e86e7330b144b7a19b15b9308fd65
Gabriele Bruno Franco <gabrielebrunofranco@gmail.com>
2018-10-15 09:16:21 +0200

    comb_sort added

---
+++ sorting/comb_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define SHRINK 1.3	//suggested shrink factor value
+
+void sort (int *numbers, int size) 
+{
+	int gap = size;
+	while (gap > 1) 	//gap = 1 means that the array is sorted 
+	{
+		gap = gap/SHRINK;
+		int i = 0;
+		while ((i + gap) < size) 
+		{	//similiar to the Shell Sort
+			if (numbers[i] > numbers[i + gap])
+			{
+				int tmp = numbers[i];
+				numbers[i] = numbers[i + gap];
+				numbers[i + gap] = tmp;
+			}
+			i++;
+		}
+	}
+}
+int main()
+{
+	int size = 6;
+	int *numbers = malloc(size*sizeof(int));
+	printf("Insert %d unsorted numbers: \n", size);
+	for (int i = 0; i < size; ++i)
+		scanf("%d", &numbers[i]);
+	printf("Initial array: ");
+	for (int i = 0; i < size; ++i)
+		printf("%d ", numbers[i]);
+	printf("\n");
+	sort(numbers, size);
+	printf("Sorted array: ");
+	for (int i = 0; i < size; ++i)
+		printf("%d ", numbers[i]);
+	printf("\n");
+	return 0;
+}
