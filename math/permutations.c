ab79cd7b1c433425ea433e23f702d72afd78802a
Gabriele Bruno Franco <gabrielebrunofranco@gmail.com>
2018-10-17 14:26:35 +0200

    permutations.c updated indentation

--- math/permutations.c
+++ math/permutations.c
@@ -13,27 +13,27 @@ void permutations (int pos, int *numbers, int *prnt, int *mark, int size)
 	for (i = 0; i < size; ++i)
 		if (mark[i] == 0) 
 		{
-		      prnt[pos] = numbers[i];
-		      mark[i] = 1;
-		      permutations (pos+1, numbers, prnt, mark, size);
-		      mark[i] = 0;
+			prnt[pos] = numbers[i];
+			mark[i] = 1;
+			permutations (pos+1, numbers, prnt, mark, size);
+			mark[i] = 0;
 		}	
 }
 int main()
 {
-        int size = 3;
-        int *numbers = malloc(size*sizeof(int));
-        int *prnt = malloc(size*sizeof(int));
+	int size = 3;
+	int *numbers = malloc(size*sizeof(int));
+	int *prnt = malloc(size*sizeof(int));
 	int *mark = malloc(size*sizeof(int));
-        printf("Insert %d numbers: \n", size);
-        for (int i = 0; i < size; ++i)
+	printf("Insert %d numbers: \n", size);
+	for (int i = 0; i < size; ++i)
 	{
-                scanf("%d", &numbers[i]);
+		scanf("%d", &numbers[i]);
 		mark[i] = 0;
 	}
-        printf("\n");
+	printf("\n");
 	printf("The permutations of the given array are: \n");
 	permutations(0, numbers, prnt, mark, size);
-        return 0;
+	return 0;
 }
 
