20a18226d6c0a6aef53f4e7a846129f25fe6b7a6
Gabriele Bruno Franco <gabrielebrunofranco@gmail.com>
2018-10-18 21:24:02 +0200

    updated main of combinations.c

--- math/combinations.c
+++ math/combinations.c
@@ -21,25 +21,22 @@ void combinations (int pos, int *numbers, int *prnt, int size, int subset_size, 
 int main()
 {
 	int size;
-	int subset_size;
 	printf("Insert size of the array: \n");
 	scanf("%d", &size);
+
+	int subset_size;
 	int *numbers = malloc(size*sizeof(int));
 	int *prnt = malloc(size*sizeof(int));
-	int *mark = malloc(size*sizeof(int));
+
 	printf("Insert %d numbers: \n", size);
 	for (int i = 0; i < size; ++i)
-	{
 		scanf("%d", &numbers[i]);
-		mark[i] = 0;
-	}
-	printf("Insert subset dimension:\n");
+	printf("Insert subset dimension: ");
 	scanf("%d", &subset_size);
 	printf("\n");
 	printf("The %d-combinations of the given array are: \n", subset_size);
 	combinations(0, numbers, prnt, size, subset_size, 0);
 	free(numbers);
 	free(prnt);
-	free(mark);
 	return 0;
 }
