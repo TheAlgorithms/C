7c80185b5bb5074681bebf27b0cba38028496003
Gabriele Bruno Franco <gabrielebrunofranco@gmail.com>
2018-10-18 21:20:22 +0200

    added combinations.c

---
+++ math/combinations.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void combinations (int pos, int *numbers, int *prnt, int size, int subset_size, int start) 
+{
+	int i;
+	if (pos == subset_size) 
+	{
+		for (i = 0; i < subset_size; ++i)
+			printf("%d ", prnt[i]);
+		printf("\n");
+		return;
+	}
+	for (i = start; i < size; ++i) 
+	{
+		prnt[pos] = numbers[i];
+		combinations(pos+1, numbers, prnt, size, subset_size, i+1);
+	}
+}
+
+int main()
+{
+	int size;
+	int subset_size;
+	printf("Insert size of the array: \n");
+	scanf("%d", &size);
+	int *numbers = malloc(size*sizeof(int));
+	int *prnt = malloc(size*sizeof(int));
+	int *mark = malloc(size*sizeof(int));
+	printf("Insert %d numbers: \n", size);
+	for (int i = 0; i < size; ++i)
+	{
+		scanf("%d", &numbers[i]);
+		mark[i] = 0;
+	}
+	printf("Insert subset dimension:\n");
+	scanf("%d", &subset_size);
+	printf("\n");
+	printf("The %d-combinations of the given array are: \n", subset_size);
+	combinations(0, numbers, prnt, size, subset_size, 0);
+	free(numbers);
+	free(prnt);
+	free(mark);
+	return 0;
+}
