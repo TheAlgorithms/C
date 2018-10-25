7ef150d1086ea76b16d26abf668ed8e637000aef
shehani <shehanikandy@yahoo.com>
2018-10-25 03:57:44 -0700

    add new algorithm

---
+++ searching/binaryserch2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+ 
+int main()
+{
+   int c, first, last, middle, n, search1, array[100];
+ 
+   printf("Enter number of elements\n");
+   scanf("%d",&n);
+ 
+   printf("Enter %d integers\n", n);
+ 
+   for (c = 0; c < n; c++)
+      scanf("%d",&array[c]);
+ 
+   printf("Enter value to find\n");
+   scanf("%d", &search1);
+ 
+   first = 0;
+   last = n - 1;
+   middle = (first+last)/2;
+ 
+   while (first <= last) {
+      if (array[middle] < search1)
+         first = middle + 1;    
+      else if (array[middle] == search1) {
+         printf("%d found at location %d.\n", search1, middle+1);
+         break;
+      }
+      else
+         last = middle - 1;
+ 
+      middle = (first + last)/2;
+   }
+   if (first > last)
+      printf("Not found! %d isn't present in the list.\n", search1);
+ 
+   return 0;  
+}
