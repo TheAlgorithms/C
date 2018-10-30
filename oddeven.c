483b1b66a805a77520ff8edbcc435eed3311604e
Roshan <42915010+kumawat8560@users.noreply.github.com>
2018-10-30 16:37:11 +0530

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    // True if the number is perfectly divisible by 2
+    if(number % 2 == 0)
+        printf("%d is even.", number);
+    else
+        printf("%d is odd.", number);
+
+    return 0;
+}
