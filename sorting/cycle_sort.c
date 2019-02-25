79f1c6d65ee8dc15aaa1ac1d4aaaed1a5914c99b
Gabriele Bruno Franco <gabrielebrunofranco@gmail.com>
2019-02-25 19:23:50 +0100

    Updated spacing

--- sorting/cycle_sort.c
+++ sorting/cycle_sort.c
@@ -4,51 +4,52 @@
 /*I translated the python version on wikipedia to C */
 void sort (int *numbers, int size) 
 {
-	int i, item, pos;
-	for (i = 0; i < size-1; ++i)
-	{
-		item = numbers[i];
-		pos = i;
-		for (int j = i+1; j < size; ++j)
-			if (numbers[j] < item)
-				pos++;
-		if (pos == i)
-			continue;
-		while (item == numbers[pos])
-			pos++;
-		int tmp = numbers[pos];
-		numbers[pos] = item;
-		item = tmp;
+    int i, item, pos;
+    for (i = 0; i < size-1; ++i)
+    {
+        item = numbers[i];
+        pos = i;
+        for (int j = i+1; j < size; ++j)
+            if (numbers[j] < item)
+                pos++;
+        if (pos == i)
+            continue;
+        while (item == numbers[pos])
+            pos++;
+        int tmp = numbers[pos];
+        numbers[pos] = item;
+        item = tmp;
 
-		while (pos != i){
-			pos = i;
-			for (int j = i+1; j < size; ++j)
-				if (numbers[i] < item)
-					pos++;
-			while (item == numbers[pos])
-				pos++;
-			int tmp = numbers[pos];
-			numbers[pos] = item;
-			item = tmp;
-		}
-	}	
+        while (pos != i){
+            pos = i;
+            for (int j = i+1; j < size; ++j)
+                if (numbers[i] < item)
+                    pos++;
+            while (item == numbers[pos])
+                pos++;
+            int tmp = numbers[pos];
+            numbers[pos] = item;
+            item = tmp;
+        }
+    }	
 }
+
 int main()
 {
-	int size = 6;
-	int *numbers = malloc(size*sizeof(int));
-	printf("Insert %d unsorted numbers: \n", size);
-	for (int i = 0; i < size; ++i)
-		scanf("%d", &numbers[i]);
-	printf("Initial array: ");
-	for (int i = 0; i < size; ++i)
-		printf("%d ", numbers[i]);
-	printf("\n");
-	sort(numbers, size);
-	printf("Sorted array: ");
-	for (int i = 0; i < size; ++i)
-		printf("%d ", numbers[i]);
-	printf("\n");
+    int size = 6;
+    int *numbers = malloc(size*sizeof(int));
+    printf("Insert %d unsorted numbers: \n", size);
+    for (int i = 0; i < size; ++i)
+        scanf("%d", &numbers[i]);
+    printf("Initial array: ");
+    for (int i = 0; i < size; ++i)
+        printf("%d ", numbers[i]);
+    printf("\n");
+    sort(numbers, size);
+    printf("Sorted array: ");
+    for (int i = 0; i < size; ++i)
+        printf("%d ", numbers[i]);
+    printf("\n");
     free(numbers);
-	return 0;
+    return 0;
 }
