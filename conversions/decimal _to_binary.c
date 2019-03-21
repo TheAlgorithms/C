f1e6ca801fbae87352c181cf573677d7b1c03ac4
Ole H <cerberus885@gmail.com>
2019-03-21 22:10:14 +0100

    changed algorhytm to create binary number
    instead of using an array to save the binary value we write it immeaditly
    also added the possibility to enter negative umbers

--- conversions/decimal _to_binary.c
+++ conversions/decimal _to_binary.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXBITS 100
+void mask(int number){
+
+	//create bitmask
+	unsigned int mask = 0;
+    //crates mask 0b1111..1111
+	mask = ~((~(unsigned)0)>>1);
+    
+	while (mask != 0){
+		//if current bit is 1
+		if ((number & mask) != 0){
+			printf ("1");
+		}
+		else {
+			printf ("0");
+		}
+	//shift mask 1 position to the right
+	mask >>= 1;
+	}
+
+
+}
 
 int main()
 {
@@ -9,58 +29,13 @@ int main()
     // input of the user
     int inputNumber;
 
-    // for the remainder
-    int re;
-
-    // contains the bits 0/1
-    int bits[MAXBITS];
-
-    // for the loops
-    int j;
-    int i=0;
-
     printf("\t\tConverter decimal --> binary\n\n");
 
     // reads a decimal number from the user.
     printf("\nenter a positive integer number: ");
     scanf("%d",&inputNumber);
 
-    // make sure the input number is a positive integer.
-    if (inputNumber < 0)
-    {
-        printf("only positive integers >= 0\n");
-        return 1;
-    }
-
-    // actual processing
-    while(inputNumber>0)
-    {
-
-        // computes the remainder by modulo 2
-        re = inputNumber % 2;
-
-        // computes the quotient of division by 2
-        inputNumber = inputNumber / 2;
-
-        bits[i] = re;
-        i++;
-
-    }
-
-    printf("\n the number in binary is: ");
-
-    // iterates backwards over all bits
-    for(j=i-1; j>=0; j--)
-    {
-        printf("%d",bits[j]);
-    }
-
-    // for the case the input number is 0
-    if (i == 0)
-    {
-        printf("0");
-    }
+    mask(inputNumber);
 
     return 0;
 }
-
