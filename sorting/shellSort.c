cef21ccea92bfa3e7648da85d89ba9ba524852b9
Libin Yang <szuyanglb@outlook.com>
2019-05-05 15:34:27 +0800

    Update shellSort.c

--- sorting/shellSort.c
+++ sorting/shellSort.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 #define ELEMENT_NR 20
-#define ARRAY_LEN(x) (sizeof(x)/sizeof((x)[0]))
+#define ARRAY_LEN(x) (sizeof(x) / sizeof((x)[0]))
 const char *notation = "Shell Sort Big O Notation:\
 						\n--> Best Case: O(n log(n)) \
 						\n--> Average Case: depends on gap sequence \
@@ -11,58 +11,58 @@ const char *notation = "Shell Sort Big O Notation:\
 
 void show_data(int arr[], int len)
 {
-	int i;
+    int i;
 
-	for (i = 0; i < len; i++)
-		printf("%3d ", arr[i]);
-	printf("\n");
+    for (i = 0; i < len; i++)
+        printf("%3d ", arr[i]);
+    printf("\n");
 }
 
 void swap(int *a, int *b)
 {
-	int tmp;
+    int tmp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
 void shellSort(int array[], int len)
 {
-	int i, j, gap;
+    int i, j, gap;
 
-	for (gap = len / 2; gap > 0; gap = gap / 2)
-		for (i = gap; i < len; i++)
-			for (j = i - gap; j >= 0 && array[j] > array[j + gap]; j = j - gap)
-				swap(&array[j], &array[j + gap]);
+    for (gap = len / 2; gap > 0; gap = gap / 2)
+        for (i = gap; i < len; i++)
+            for (j = i - gap; j >= 0 && array[j] > array[j + gap]; j = j - gap)
+                swap(&array[j], &array[j + gap]);
 }
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int array[ELEMENT_NR];
-	int range = 500;
-	int size;
-	clock_t start, end;
-	double time_spent;
+    int i;
+    int array[ELEMENT_NR];
+    int range = 500;
+    int size;
+    clock_t start, end;
+    double time_spent;
 
-	srand(time(NULL));
-	for (i= 0; i < ELEMENT_NR; i++)
-		array[i] = rand() % range + 1;
+    srand(time(NULL));
+    for (i = 0; i < ELEMENT_NR; i++)
+        array[i] = rand() % range + 1;
 
-	size = ARRAY_LEN(array);
+    size = ARRAY_LEN(array);
 
-	show_data(array, size);
-	start = clock();
-	shellSort(array, size);
-	end = clock();
-	time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    show_data(array, size);
+    start = clock();
+    shellSort(array, size);
+    end = clock();
+    time_spent = (double)(end - start) / CLOCKS_PER_SEC;
 
-	printf("Data Sorted\n");
-	show_data(array, size);
+    printf("Data Sorted\n");
+    show_data(array, size);
 
-	printf("%s\n", notation);
-	printf("Time spent sorting: %f\n", time_spent);
+    printf("%s\n", notation);
+    printf("Time spent sorting: %f\n", time_spent);
 
-	return 0;
+    return 0;
 }
