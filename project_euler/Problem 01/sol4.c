f4a529eb9ae7230363e332d4da516b3d836a26b5
iamAafil <afil.umar007@gmail.com>
2019-06-11 19:02:34 +0530

    added sol4.c

---
+++ project_euler/Problem 01/sol4.c
@@ -0,0 +1,25 @@
+/*An Efficient code to print all the sum of all numbers that are multiples of 3 & 5 below N.*/
+
+#include <stdio.h>
+
+int main(){
+    int t; 
+    printf("Enter number of times you want to try");
+    scanf("%d",&t);
+    while(t--)
+    {
+        unsigned long long N,p=0,sum=0;	
+        printf("Enter the value of N ");
+         
+        scanf("%lld",&N);	//Take input of N from user
+        for(int i=0;i<N;i++)
+          {
+            if(i%3==0||i%5==0)
+               {
+                 sum=sum+i;
+                }
+           }
+        printf("%lld\n", sum);		//print the sum of all numbers that are multiples of 3 & 5 below N
+    }
+    return 0;
+}
