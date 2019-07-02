ab3173f5815944451b8cd98329c8a68cdccf3d5f
dang hai <haihoangdang91@gmail.com>
2019-07-02 16:02:55 -0700

    Fix indenting

--- data_structures/stack/stack_linkedlist/main.c
+++ data_structures/stack/stack_linkedlist/main.c
@@ -5,18 +5,18 @@
 
 int main() {
     Stack_T stk;
-	stk = Stack_init();
-	Stack_push(stk, (int *) 1);
-	Stack_push(stk, (int *) 2);
-	Stack_push(stk, (int *) 3);
-	Stack_push(stk, (int *) 4);
+    stk = Stack_init();
+    Stack_push(stk, (int *) 1);
+    Stack_push(stk, (int *) 2);
+    Stack_push(stk, (int *) 3);
+    Stack_push(stk, (int *) 4);
     printf("Size: %d\n", Stack_size(stk));
-	Stack_print(stk);
-	Stack_pop(stk);
-	printf("Stack after popping: \n");
-	Stack_print(stk);
-	Stack_pop(stk);
-	printf("Stack after popping: \n");
-	Stack_print(stk);
+    Stack_print(stk);
+    Stack_pop(stk);
+    printf("Stack after popping: \n");
+    Stack_print(stk);
+    Stack_pop(stk);
+    printf("Stack after popping: \n");
+    Stack_print(stk);
     return 0;
 }
