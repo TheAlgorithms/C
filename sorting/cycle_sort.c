18a8ca22684bce1919bb12a4dfc6ad6157013ef5
Gabriele Bruno Franco <gabrielebrunofranco@gmail.com>
2019-08-11 13:25:49 +0200

    Bugfix and C89-compatible loops

--- sorting/cycle_sort.c
+++ sorting/cycle_sort.c
@@ -4,26 +4,29 @@
 /*I translated the python version on wikipedia to C */
 void sort (int *numbers, int size) 
 {
-    int i, item, pos;
+    int i, j, item, pos;
     for (i = 0; i < size-1; ++i)
     {
         item = numbers[i];
         pos = i;
-        for (int j = i+1; j < size; ++j)
+        for (j = i+1; j < size; ++j)
             if (numbers[j] < item)
                 pos++;
+
         if (pos == i)
             continue;
+
         while (item == numbers[pos])
             pos++;
+
         int tmp = numbers[pos];
         numbers[pos] = item;
         item = tmp;
-
+        
         while (pos != i){
             pos = i;
-            for (int j = i+1; j < size; ++j)
-                if (numbers[i] < item)
+            for (j = i+1; j < size; ++j)
+                if (numbers[j] < item)
                     pos++;
             while (item == numbers[pos])
                 pos++;
@@ -31,22 +34,23 @@ void sort (int *numbers, int size)
             numbers[pos] = item;
             item = tmp;
         }
-    }	
+    }
 }
 
 void display(int *array, int n)
 {
-    for (int i = 0; i < n; ++i)
+    int i;
+    for (i = 0; i < n; ++i)
         printf("%d ", array[i]);
     printf("\n");
 }
 
 int main()
 {
-    int size = 6;
+    int i, size = 6;
     int *numbers = malloc(size*sizeof(int));
     printf("Insert %d unsorted numbers: \n", size);
-    for (int i = 0; i < size; ++i)
+    for (i = 0; i < size; ++i)
         scanf("%d", &numbers[i]);
     printf("Initial array: ");
     display(numbers, size); 
