b75256229a4cd0cc2220f57a807f50865c13c997
dang hai <haihoangdang91@gmail.com>
2019-09-03 08:52:24 -0700

    Add some solution for leetcode

---
+++ leetcode/src/101.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+bool checkSymmetric(struct TreeNode *left, struct TreeNode *right) {
+    if (!left || !right)
+        return left == right;
+    if (left->val != right->val)
+        return 0;
+    return checkSymmetric(left->left, right->right) && checkSymmetric(left->right, right->left);
+}
+
+bool isSymmetric(struct TreeNode* root){
+    return root == NULL || checkSymmetric(root->left, root->right);
+}
