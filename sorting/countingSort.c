13138dd0e9578dd8b62881d47bf2513d0eef1405
harshraj22 <harshraj22aug@gmail.com>
2019-09-18 00:06:51 +0530

    added useful changes and comments in countingSort.c

--- sorting/countingSort.c
+++ sorting/countingSort.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
-  int main()
- {
- 	int i,n,l=0;
-  scanf("%d",&n);
- 	int a[n];
-  for(i=0;i<n;i++)
- 	{
-  	 	scanf("%d",&a[i]);
-  	 	if(a[i]>l)
-  	 	 l=a[i];
- 	}
- 	int b[l+1]={0};
- 	for(i=0;i<n;i++)
- 	 b[a[i]]++; //hashing number to array index
- 	for(i=0;i<(l+1);i++)
- 	{
- 		if(b[i]>0)
- 	{
- 			while(b[i]!=0) //for case when number exists more than once
- 			{
- 			printf("%d ",i); 
- 			b[i]--;
- 			}
-    	}
- 	}
- 	return 0;
- } 
+#include <string.h>	// for memset
+#include <assert.h>
+
+int main()
+{
+	int i, n, l = 0;
+	scanf("%d", &n);
+
+	assert(n < 1e7);
+	int a[n];
+
+
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+		//for now, We can't hash negative indexes
+		assert(a[i] >= 0);
+		if (a[i] > l)
+			l = a[i];
+	}
+	// to save us from accidenty declaring insanely large array
+	assert(l < 1e7);
+	// In C,it is considered a bad practice to initialise a array at definition that was initialised at runtime 
+	int b[l + 1];
+	// Initialising all the values to zero
+	memset(b,0,sizeof(b));
+
+	for (i = 0; i < n; i++)
+		b[a[i]]++; //hashing number to array index
+
+	for (i = 0; i < (l + 1); i++)
+	{
+		if (b[i] > 0)
+		{
+			while (b[i] != 0) //for case when number exists more than once
+			{
+				printf("%d ", i);
+				b[i]--;
+			}
+		}
+	}
+	printf("\n");
+	return 0;
+}
