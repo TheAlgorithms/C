2f5817cbe09afdd2a90ecfbfc4278a3f2fded6f1
Sayan Mondal <mondalsayan556@gmail.com>
2019-10-01 00:10:07 +0530

    Dynamic Programming added

---
+++ dynamic_programming/Longest_Common_subquince.c
@@ -0,0 +1,37 @@
+/* A Naive recursive implementation of LCS problem */
+#include<stdio.h> 
+
+int max(int a, int b); 
+
+/* Returns length of LCS for X[0..m-1], Y[0..n-1] */
+int lcs( char *X, char *Y, int m, int n ) 
+{ 
+if (m == 0 || n == 0) 
+	return 0; 
+if (X[m-1] == Y[n-1]) 
+	return 1 + lcs(X, Y, m-1, n-1); 
+else
+	return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n)); 
+} 
+
+/* Utility function to get max of 2 integers */
+int max(int a, int b) 
+{ 
+	return (a > b)? a : b; 
+} 
+
+
+//Test
+/* Driver program to test above function */
+int main() 
+{ 
+char X[] = "AGGTAB"; 
+char Y[] = "GXTXAYB"; 
+
+int m = strlen(X); 
+int n = strlen(Y); 
+
+printf("Length of LCS is %d", lcs( X, Y, m, n ) ); 
+
+return 0; 
+} 
