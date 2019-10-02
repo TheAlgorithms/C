eb05d2a9b6b77485ca57115f95860e22a1bfd28e
Prathamesh Sahasrabhojane <ssahasrabhojane@gmail.com>
2019-10-02 22:32:02 +0530

    Added UDP client algo

---
+++ Simple Client Server/UDPClient.c
@@ -0,0 +1,49 @@
+// Client side implementation of UDP client-server model 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+
+#define PORT	 8080 
+#define MAXLINE 1024 
+
+// Driver code 
+int main() { 
+	int sockfd; 
+	char buffer[MAXLINE]; 
+	char *hello = "Hello from client"; 
+	struct sockaddr_in	 servaddr; 
+
+	// Creating socket file descriptor 
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+		perror("socket creation failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+
+	memset(&servaddr, 0, sizeof(servaddr)); 
+	
+	// Filling server information 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_port = htons(PORT); 
+	servaddr.sin_addr.s_addr = INADDR_ANY; 
+	
+	int n, len; 
+	
+	sendto(sockfd, (const char *)hello, strlen(hello), 
+		MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
+			sizeof(servaddr)); 
+	printf("Hello message sent.\n"); 
+		
+	n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
+				MSG_WAITALL, (struct sockaddr *) &servaddr, 
+				&len); 
+	buffer[n] = '\0'; 
+	printf("Server : %s\n", buffer); 
+
+	close(sockfd); 
+	return 0; 
+} 
