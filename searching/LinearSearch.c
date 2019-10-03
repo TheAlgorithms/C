b82ed40cdfcb9c84415b96f495636f7005f71496
nityaprakash10 <43900148+nityaprakash10@users.noreply.github.com>
2019-10-03 18:48:02 +0530

    Update LinearSearch.c

--- searching/LinearSearch.c
+++ searching/LinearSearch.c
@@ -1,27 +1,23 @@
-#include <stdio.h>
+#include <stdio.h> 
+  
+int search(int arr[], int n, int x) 
+{ 
+    int i; 
+    for (i = 0; i < n; i++) 
+        if (arr[i] == x) 
+            return i; 
+    return -1; 
+} 
+  
+int main(void) 
+{ 
+    int arr[] = { 2, 3, 4, 10, 40 }; 
+    int x = 10; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    int result = search(arr, n, x); 
+    (result == -1) ? printf("Element is not present in array") 
+                   : printf("Element is present at index %d", 
+                            result); 
+    return 0; 
+} 
 
-int linearsearch(int *arr, int size, int val){
-	int i;
-	for (i = 0; i < size; i++){
-		if (arr[i] == val)
-			return 1;
-	}
-	return 0;
-}
-
-void main(){
-	int n,i,v;
-	printf("Enter the size of the array:\n");
-	scanf("%d",&n); //Taking input for the size of Array
-
-	int a[n];
-	printf("Enter the contents for an array of size %d:\n", n);
-	for (i = 0; i < n; i++)	scanf("%d", &a[i]);// accepts the values of array elements until the loop terminates//
-
-	printf("Enter the value to be searched:\n");
-	scanf("%d", &v); //Taking input the value to be searched
-	if (linearsearch(a,n,v))
-		printf("Value %d is in the array.\n", v);
-	else
-		printf("Value %d is not in the array.\n", v);
-}
