15ba68e1104298ee420deb05173c40b8b1987c10
shivam agarwal <43515429+shivamagarwal1999@users.noreply.github.com>
2019-10-03 16:55:32 +0530

    Update stack.c

--- data_structures/stack.c
+++ data_structures/stack.c
@@ -1,147 +1,78 @@
-/**
- * Kyler Smith, 2017
- * Stack data structure implementation.
- */
-
-////////////////////////////////////////////////////////////////////////////////
-//INCLUDES
-#include <stdio.h>
-#include <stdlib.h>
-
-////////////////////////////////////////////////////////////////////////////////
-//MACROS: CONSTANTS
-
-////////////////////////////////////////////////////////////////////////////////
-//DATA STRUCTURES
-struct node {
-    int data;
-    struct node* next;
-    struct node* pre;
-} *head, *tmp;
-
-////////////////////////////////////////////////////////////////////////////////
-//GLOBAL VARIABLES
-int count = 0;
-
-////////////////////////////////////////////////////////////////////////////////
-//FUNCTION PROTOTYPES
-void create();
-void push(int x);
-int pop();
-int peek();
-int size();
-int isEmpty();
-
-////////////////////////////////////////////////////////////////////////////////
-//MAIN ENTRY POINT
-
-int main(int argc, char const *argv[]) {
-
-    int x, y, z;
-
-    create();
-    push(4);
-    x = pop();
-    // 4. Count: 0. Empty: 1.
-    printf("%d.\t\tCount: %d.\tEmpty: %d.\n", x, size(), isEmpty());
-
-    push(1);
-    push(2);
-    push(3);
-    x = pop();
-    y = pop();
-    // 3, 2. Count: 1. Empty: 0;
-    printf("%d, %d.\t\tCount: %d.\tEmpty: %d.\n", x, y, size(), isEmpty());
-    pop();  // Empty the stack.
-
-    push(5);
-    push(6);
-    x = peek();
-    push(7);
-    y = pop();
-    push(8);
-    z = pop();
-    // 1, 6, 7, 8. Count: 2. Empty: 0.
-    printf("%d, %d, %d.\tCount: %d.\tEmpty: %d.\n", x, y, z, size(), isEmpty());
-
-	return 0;
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 10
+struct node
+{
+	int info;
+	struct node *link;
+}*top=NULL;
+void display()
+{
+	struct node *ptr;
+	ptr=top;
+	int i;
+	if(top==NULL)
+	{
+		printf("stack underflow");
+		exit(1);
+	}
+	 printf("stack elements :\n");
+	 while(ptr!=NULL)
+	 {
+	 	printf("%d\n",ptr->info);
+	 	ptr=ptr->link;
+	 }
+    printf("\n");
 }
-
-/**
- * Initialize the stack to NULL.
- */
-void create() {
-    head = NULL;
+void push(int item)
+{
+	struct node *tmp;
+	tmp=(struct node *)malloc(sizeof(struct node));
+	if(tmp==NULL)
+	 {
+	   printf("stack overflow");
+	   exit(1);
+     }
+	 tmp->info=item;
+	 tmp->link=top;
+	 top=tmp;
 }
-
-/**
- * Push data onto the stack.
- */
-void push(int x) {
-    if(head == NULL) {
-        head = (struct node *)malloc(1 * sizeof(struct node));
-        head->next = NULL;
-        head->pre = NULL;
-        head->data = x;
-    } else {
-        tmp = (struct node *)malloc(1 * sizeof(struct node));
-        tmp->data = x;
-        tmp->next = NULL;
-        tmp->pre = head;
-        head->next = tmp;
-        head = tmp;
-    }
-    ++count;
+int pop()
+{
+	struct node *tmp;
+	int item;
+	if(top==NULL)
+	 {
+	  printf("stack underflow");
+	  exit(1);
+     }
+     tmp=top;
+     item=tmp->info;
+     top=top->link;
+     free(tmp);
+		return item;
 }
-
-/**
- * Pop data from the stack
- */
-int pop() {
-    int returnData;
-    if(head == NULL) {
-        printf("ERROR: Pop from empty stack.\n");
-        exit(1);
-    } else {
-        returnData = head->data;
-
-        if(head->pre == NULL){
-            free(head);
-            head = NULL;
-        }
-        else {
-            head = head->pre;
-            free(head->next);
-        }
-    }
-    --count;
-    return returnData;
-}
-
-/**
- * Returns the next value to be popped.
- */
-int peek() {
-    if(head != NULL)
-        return head->data;
-    else {
-        printf("ERROR: Peeking from empty stack.");
-        exit(1);
-    }
-}
-
-/**
- * Returns the size of the stack.
- */
-int size() {
-    return count;
-}
-
-/**
- * Returns 1 if stack is empty, returns 0 if not empty.
- */
-int isEmpty() {
-    if(count == 0)
-        return 1;
-    return 0;
+int main()
+{
+	int op,item;
+	while(1)
+	{
+			printf("\nenter 1 for push\t enter 2 for pop\t enter 3 for display\t enter 4 for exit");
+	        scanf("%d",&op);
+		switch(op)
+		{
+			case 1:printf("enter elements in array\n");
+			       scanf("%d",&item);
+			       push(item);
+			break;
+			case 2: item=pop();
+			       printf("the popped elements %d\n", item);
+			break;
+			case 3: display();
+			break;
+			case 4: exit(1);
+			default:
+				printf("Invalid option");
+		}
+	}
 }
