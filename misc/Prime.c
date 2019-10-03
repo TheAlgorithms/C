3d59871bde0630ef4b722808a936596ee18d27fa
Lucas <0Zeta@protonmail.com>
2019-10-03 20:05:52 +0200

    Correct and improve prime test
    There are no prime numbers smaller than 2, but 2 itself is prime.

--- misc/Prime.c
+++ misc/Prime.c
@@ -2,7 +2,13 @@
 #include <math.h>
 
 int isPrime(int x) {
-    for (int i = 2; i < sqrt(x); i++) {
+    if (x == 2) {
+        return 1;
+    }
+    if (x < 2 || x % 2 == 0) {
+        return 0;
+    }
+    for (int i = 3; i < sqrt(x); i += 2) {
         if (x%i == 0)
             return 0;
     }
