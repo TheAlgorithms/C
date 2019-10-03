30b87a2cbfd738fe272a070592a33efcfb6617f2
harsh vora <harsh.k.vora@gmail.com>
2019-10-03 22:17:54 +0530

    added insert right insert left delete right and delete left functions

--- data_structures/linked_list/singly_link_list_deletion.c
+++ data_structures/linked_list/singly_link_list_deletion.c
@@ -2,97 +2,136 @@
   It is assumed that the data in nodes will be an integer, though
   function can be modified according to the data type, easily.
   deleteNode deletes a node when passed with a key of the node.
+  The linked list can be used as a stack by implementing the functions insertLeft and deleteLeft
+  The linked list can be used as a queue by implementing the functions insertRight and deleteLeft
+  The linked list can be used as a double ended queue(dequeue) by implementing the functions insertRight and insertLeft to insert nodes on both sides and deleteLeft and deleteRight to delete nodes on both sides
 */
-#include<stdio.h>
-struct node
-{int info;
- struct node *link;
-};
-struct node *start=NULL;
 ///////////////////////////////////////////////////////////
-struct node * createnode()//function to create node
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node
 {
-  struct node *t;
-  t=(struct node*)malloc(sizeof(struct node));
-  return(t);
-}
-////////////////////////////////////////////////////////
-void insert()//function to insert at first location
+ int info;
+ struct node *link;
+}*NODE;
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+NODE insertLeft(NODE start) //function to insert the new node towards the right and function to create node as well
 {
-  struct node *p;
-  p=createnode();
-  printf("\nenter the number to insert");
-  scanf("%d",&p->info);
+  NODE p;
+  p=(NODE)malloc(sizeof(struct node));
+  printf("enter the number to insert\n");
+  scanf("%d",&(p->info));
   p->link=NULL;
   if(start==NULL)
   {
       start=p;
+      return start;
   }
-  else
-  {
-      p->link=start;
-      start=p;
-  }
+  p->link=start;
+  start=p;
+  return start;
+
 }
 ///////////////////////////////////////////////////////////
-void deleteion()//function to delete from first position
+NODE insertRight(NODE start)
 {
-    struct node *t;
+  NODE p;
+  p=(NODE)malloc(sizeof(struct node));
+  printf("enter the number to be inserted\n");
+  scanf("%d",&(p->info));
+  p->link=NULL;
+  if(start==NULL)
+  {
+    start=p;
+    return start;
+  }
+  NODE temp=start;
+  while(temp->link!=NULL)
+  {
+    temp=temp->link;
+  }
+  temp->link=p;
+  return start;
+
+}
+///////////////////////////////////////////////////////////
+NODE deleteLeft(NODE start)//function to delete from the left
+{
+    NODE t=NULL;
     if(start==NULL)
     {
-        printf("\nlist is empty");
+        printf("list is empty\n");
+        return start;
     }
-    else
-    {
-        struct node *p;
-        p=start;
-        start=start->link;
-        free(p);
-    }
+    t=start;
+    start=start->link;
+    printf("number deleted is: %d",t->info);
+    free(t);
+    return start;
 }
 ///////////////////////////////////////////////////////
-void viewlist()//function to display values
+NODE deleteRight(NODE start)
 {
-    struct node *p;
+  NODE t=start,prev=NULL;
+  while(t->link!=NULL)
+  {
+    prev=t;
+    t=t->link;
+  }
+  printf("number deleted is: %d",t->info);
+  free(t);
+  prev->link=NULL;
+  return start;
+
+}
+///////////////////////////////////////////////////////
+void viewlist(NODE start)//function to display values
+{
+    NODE p=start;
     if(start==NULL)
     {
-        printf("\nlist is empty");
+        printf("list is empty\n");
+        return;
     }
-    else
-    {   p=start;
-        while(p!=NULL)
-        {
-            printf("%d ",p->info);
-            p=p->link;
-        }
+    while(p->link!=NULL)
+    {
+      printf("%d\t",p->info);
+      p=p->link;
     }
+    printf("%d",p->info);
 }
 //////////////////////////////////////////////////////////////////////
 
 int main()
 {
-    int n;
-    while(1)
+    int ch;
+    NODE start=NULL;
+    do
     {
-        printf("\n1.add value at first location");
-        printf("\n2.delete value from first location");
-        printf("\n3.view value");
-        printf("\nenter your choice");
-        scanf("%d",&n);
-        switch(n)
-        {
+      printf("\nenter choice:\n1:insert left\n2:insert right\n3:delete left\n4:delete right\n5:display\n6:exit\n");
+      scanf("%d",&ch);
+      switch(ch)
+      {
         case 1:
-            insert();
-            break;
+        start=insertLeft(start);
+        break;
         case 2:
-            deleteion();
-            break;
+        start=insertRight(start);
+        break;
         case 3:
-            viewlist();
-            break;
-        default:
-            printf("\ninvalid choice");
-        }
-    }
-  return(0);
+        start=deleteLeft(start);
+        break;
+        case 4:
+        start=deleteRight(start);
+        break;
+        case 5:
+        viewlist(start);
+        break;
+        case 6:
+        printf("exiting...");
+        break;
+      }
+    }while(ch!=6);
 }
+
