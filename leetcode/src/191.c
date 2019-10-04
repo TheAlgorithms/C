d2ad4b0109c02ca61e1f4f918d7ac3d66526b4f2
SaurusXI <vermashantanu@hotmail.com>
2019-10-04 16:49:19 +0530

    Revert "Added Solution to leetcode Problem 191"
    This reverts commit 5a6ed8e245e7544fce060d92919a353393bfd064.
    
    To modify code to add comments
    
    0

--- leetcode/src/191.c
+++
@@ -1,9 +0,0 @@
-int hammingWeight(uint32_t n) {
-    int TotalBits = 32;
-    int i, weight = 0;
-    for(i = 0; i < TotalBits; i++) {
-        if((n & (UINT32_C(1) << i)) >> i)
-            weight += 1;
-    }
-    return weight;
-}
