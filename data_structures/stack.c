c1448e272789e8bdcd7849f1824bccd1f84be91b
nityaprakash10 <43900148+nityaprakash10@users.noreply.github.com>
2019-10-05 17:15:39 +0530

    Update stack.c

--- data_structures/stack.c
+++ data_structures/stack.c
@@ -1,147 +1,72 @@
-/**
- * Kyler Smith, 2017
- * Stack data structure implementation.
- */
-
-////////////////////////////////////////////////////////////////////////////////
-//INCLUDES
+Live Demo
 #include <stdio.h>
-#include <stdlib.h>
 
-////////////////////////////////////////////////////////////////////////////////
-//MACROS: CONSTANTS
+int MAXSIZE = 8;       
+int stack[8];     
+int top = -1;            
 
-////////////////////////////////////////////////////////////////////////////////
-//DATA STRUCTURES
-struct node {
-    int data;
-    struct node* next;
-    struct node* pre;
-} *head, *tmp;
+int isempty() {
 
-////////////////////////////////////////////////////////////////////////////////
-//GLOBAL VARIABLES
-int count = 0;
+   if(top == -1)
+      return 1;
+   else
+      return 0;
+}
+   
+int isfull() {
 
-////////////////////////////////////////////////////////////////////////////////
-//FUNCTION PROTOTYPES
-void create();
-void push(int x);
-int pop();
-int peek();
-int size();
-int isEmpty();
-
-////////////////////////////////////////////////////////////////////////////////
-//MAIN ENTRY POINT
-
-int main(int argc, char const *argv[]) {
-
-    int x, y, z;
-
-    create();
-    push(4);
-    x = pop();
-    // 4. Count: 0. Empty: 1.
-    printf("%d.\t\tCount: %d.\tEmpty: %d.\n", x, size(), isEmpty());
-
-    push(1);
-    push(2);
-    push(3);
-    x = pop();
-    y = pop();
-    // 3, 2. Count: 1. Empty: 0;
-    printf("%d, %d.\t\tCount: %d.\tEmpty: %d.\n", x, y, size(), isEmpty());
-    pop();  // Empty the stack.
-
-    push(5);
-    push(6);
-    x = peek();
-    push(7);
-    y = pop();
-    push(8);
-    z = pop();
-    // 1, 6, 7, 8. Count: 2. Empty: 0.
-    printf("%d, %d, %d.\tCount: %d.\tEmpty: %d.\n", x, y, z, size(), isEmpty());
-
-	return 0;
+   if(top == MAXSIZE)
+      return 1;
+   else
+      return 0;
 }
 
-/**
- * Initialize the stack to NULL.
- */
-void create() {
-    head = NULL;
-}
-
-/**
- * Push data onto the stack.
- */
-void push(int x) {
-    if(head == NULL) {
-        head = (struct node *)malloc(1 * sizeof(struct node));
-        head->next = NULL;
-        head->pre = NULL;
-        head->data = x;
-    } else {
-        tmp = (struct node *)malloc(1 * sizeof(struct node));
-        tmp->data = x;
-        tmp->next = NULL;
-        tmp->pre = head;
-        head->next = tmp;
-        head = tmp;
-    }
-    ++count;
-}
-
-/**
- * Pop data from the stack
- */
-int pop() {
-    int returnData;
-    if(head == NULL) {
-        printf("ERROR: Pop from empty stack.\n");
-        exit(1);
-    } else {
-        returnData = head->data;
-
-        if(head->pre == NULL){
-            free(head);
-            head = NULL;
-        }
-        else {
-            head = head->pre;
-            free(head->next);
-        }
-    }
-    --count;
-    return returnData;
-}
-
-/**
- * Returns the next value to be popped.
- */
 int peek() {
-    if(head != NULL)
-        return head->data;
-    else {
-        printf("ERROR: Peeking from empty stack.");
-        exit(1);
-    }
+   return stack[top];
 }
 
-/**
- * Returns the size of the stack.
- */
-int size() {
-    return count;
+int pop() {
+   int data;
+	
+   if(!isempty()) {
+      data = stack[top];
+      top = top - 1;   
+      return data;
+   } else {
+      printf("Could not retrieve data, Stack is empty.\n");
+   }
 }
 
-/**
- * Returns 1 if stack is empty, returns 0 if not empty.
- */
-int isEmpty() {
-    if(count == 0)
-        return 1;
-    return 0;
+int push(int data) {
+
+   if(!isfull()) {
+      top = top + 1;   
+      stack[top] = data;
+   } else {
+      printf("Could not insert data, Stack is full.\n");
+   }
+}
+
+int main() {
+   // push items on to the stack 
+   push(3);
+   push(5);
+   push(9);
+   push(1);
+   push(12);
+   push(15);
+
+   printf("Element at top of the stack: %d\n" ,peek());
+   printf("Elements: \n");
+
+   // print stack data 
+   while(!isempty()) {
+      int data = pop();
+      printf("%d\n",data);
+   }
+
+   printf("Stack full: %s\n" , isfull()?"true":"false");
+   printf("Stack empty: %s\n" , isempty()?"true":"false");
+   
+   return 0;
 }
