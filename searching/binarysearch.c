3ae2ca0eea96c5b8f5c93612a64f9b87b2c82055
mateuseap <52019009+mateuseap@users.noreply.github.com>
2019-10-07 14:11:05 -0300

    Update binarysearch.c
    As you asked, I removed my large Portuguese comment that was explaining my code, and now I'm calling the function binarySearch in the main function and using it (I explained with comments in English what I'm doing).

--- searching/binarysearch.c
+++ searching/binarysearch.c
@@ -2,29 +2,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-/*
-*PARÂMETROS DA FUNÇÃO**
--O primeiro parâmetro é o meu vetor (ordenado)
--O segundo parâmetro é o valor procurado
--O terceiro parâmetro é o tamanho do meu vetor
-*VARIÁVEIS DA FUNÇÃO*
--l -> começo do segmento
--r -> final do segmento
--m -> meio do segmento ((l+r)/2);
--k -> valor procurado
--n -> tamanho do vetor
-*EXPLICAÇÃO DO ALGORITMO*
-O algoritmo basicamente compara o valor procurado com o elemento que esta
-localizado no meio do array, se forem iguais, paramos e retornamos o índice em que está o elemento, caso não sejam iguais ele verifica
-se o valor procurado é menor do que o elemento que esta localizado no meio do array, caso seja, o final do segmento é
-atualizado e se torna m-1, porque já que o elemento procurado é menor do que o valor que está no meio do array, podemos assumir
-que ele vai estar antes desse elemento, então atualizamos o final do segmento. Caso o valor procurado seja maior que o elemento localizado no meio
-do array, nós atualizamos o começo do nosso segmento, pois sabemos que o nosso valor procurado é maior do que o elemento 
-que está no meio do array, então o começo do nosso segmento se torna m+1, e assim
-continuamos até que l > r ou até encontramos o valor e retornamos o índice em que o mesmo se encontra. Caso l > r e não tenhamos achado o valor, retornamos -1!
-Podemos assumir tudo o que falei acima porque o array vai estar previamente ordenado.
-*/
-
 int binarySearch(int vet[], int k, int n){ 
     int l = 0, r = n-1, m;
     while(l<=r){
@@ -41,6 +18,14 @@ int binarySearch(int vet[], int k, int n){
 }
 
 int main(){
+    
+    int vet[] = {1,2,3,4,5,6,7,8,9,10}, res; //Here I create a array called vet, with ten positions, and a variable called res
+    
+    res = binarySearch(vet, 5, 9); //Now I call my binarySearch function, I send to my function my array called vet, the number that I wanna find
+                                   //in my array and the size of my array, respectivly.
+    
+    printf("%d", res); //Now, I just show what my binarySearch sent to me, it show -1 if the number isn't in the array and if 
+                       //my number is in the array, it will show the index that the number is localized.
     
     return 0;
 }
