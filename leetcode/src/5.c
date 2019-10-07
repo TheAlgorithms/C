4c624a2f29d6e8014076abcc600f4fa64911cef4
jonohein <50930342+jonohein@users.noreply.github.com>
2019-10-07 09:44:06 -0700

    Add more solution for leetcode
    Added solution for #5 Longest Palindromic Substring

---
+++ leetcode/src/5.c
@@ -0,0 +1,34 @@
+char * longestPalindrome(char * s){
+	char *result = NULL;
+	int strSize = 0;
+	for(int i = 0; *(s+i) != '\0'; i++){
+		strSize = i + 1;
+	}
+	int start = 0;
+	int end = 0;
+	int largest = 0;
+	for(int i = 0; i < strSize; i++){
+		for(int j = i + largest; j < strSize; j++){
+			if(*(s+i) == *(s+j)){
+				int pass = 1;
+				for(int k = 1; k <= (j-i)/2; k++){
+					if(*(s+i+k) != *(s+j-k)){
+						pass = 0;						
+						break;
+					}
+				}
+				if(pass && largest < j-i+1){
+					largest = j - i + 1;
+					start = i;
+					end = j;
+				}
+			}
+		}
+	}
+	result = (s+start);
+	// Destructive
+	if(largest > 0 && end+1 != strSize+1){
+		s[end+1] = '\0';
+	}
+	return result;
+}
