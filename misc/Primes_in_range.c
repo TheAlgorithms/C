2ffc6980be3e4c260a78934188f339cd6f3d24f2
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2019-10-08 22:21:52 +0530

    format code (add even spaces)

--- misc/Primes_in_range.c
+++ misc/Primes_in_range.c
@@ -6,45 +6,49 @@
 #include<stdlib.h>
 #include<math.h>
 
-int * Sieve(int);
+int *Sieve(int);
 
 void printPrimes(int *, int);
 
 int main() {
- int range;
- printf("Input a number to find all primes in the range:\t");
- scanf("%d", &range);
- Sieve(range);
+    int range;
+    printf("Input a number to find all primes in the range:\t");
+    scanf("%d", &range);
+    Sieve(range);
+
+    return 0;
 }
 
 
-int * Sieve(int n) {
-  // Create an array to hold the sieve of sizw `n`
-  int * sieve = malloc(n*sizeof(int));
+int *Sieve(int n) {
 
-  // Initialize the array with all 1`s upto 
-  for (size_t i = 0; i < n; i++) {
-    sieve[i] = 1;
-  }
+    // Create an array to hold the sieve of sizw `n`
+    int * sieve = malloc(n*sizeof(int));
 
-  for(int i = 2; i < sqrt(n); i++) {
-    if(sieve[i] == 1) {
-      for (size_t j = i*i; j < n; j+=i) {
-        sieve[j] = 0;
-      }
+    // Initialize the array with all 1`s upto
+    for (size_t i = 0; i < n; i++) {
+        sieve[i] = 1;
     }
-  }
-  printPrimes(sieve, n);
-  return sieve;
+
+    for(int i = 2; i < sqrt(n); i++) {
+        if(sieve[i] == 1) {
+            for (size_t j = i*i; j < n; j+=i) {
+                sieve[j] = 0;
+            }
+        }
+    }
+
+    printPrimes(sieve, n);
+    return sieve;
 }
 
 void printPrimes(int * sieve, int size) {
-  printf("\nPrimes in range %d are:\n\t", size);
-  for (int i = 2; i < size; i++) {
-    if(sieve[i] == 1) {
-      printf(" %d", i);
-    }
-  }
- printf("\n");
-}
 
+    printf("\nPrimes in range %d are:\n\t", size);
+    for (int i = 2; i < size; i++) {
+        if(sieve[i] == 1) {
+            printf(" %d", i);
+        }
+    }
+    printf("\n");
+}
