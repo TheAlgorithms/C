d239e058d658cc89321427e97d165ba3929122ba
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2019-10-09 16:32:03 +0530

    Update and rename Conversions/decimal-to-binary-without-array.c to conversions/decimal_to_binary_without_array.c
    **div** is a predefined function in **math.h**.
    Replacing **div** with **temp**

--- conversions/decimal_to_binary_without_array.c
+++ conversions/decimal_to_binary_without_array.c
@@ -3,7 +3,7 @@
 
 void main()
 {
-	int rem, div, num;
+	int rem, temp, num;
 	// Taking large value for i and sum
 	long int i = 1, sum = 0;
 
@@ -11,11 +11,12 @@ void main()
 	printf("Enter any number");
 	scanf("%d" , &num);
 
-	while(div>0)
+	temp = num;
+	while(temp>0)
 	{
 		rem = num%2;
-		div = num/2;
-		num = div;
+		temp = num/2;
+		num = temp;
 		// Taking the number as a sum in an integer.
 		sum = sum + (rem*i);
 		 i  = i*10;
