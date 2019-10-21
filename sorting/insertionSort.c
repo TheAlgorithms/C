f82fe815368318fd06126bfe407680fc19ac19f2
leoperd <56191198+leoperd@users.noreply.github.com>
2019-10-22 01:59:49 +0530

    Update insertionSort.c

--- sorting/insertionSort.c
+++ sorting/insertionSort.c
@@ -16,7 +16,7 @@ void display(int arr[], int n) {
  */
 void insertionSort(int arr[], int size) {
     int i, j, key;
-    for(i = 0; i < size; i++) {
+    for(i = 1; i < size; i++) {
         j = i - 1;
         key = arr[i];
         /* Move all elements greater than key to one position */
