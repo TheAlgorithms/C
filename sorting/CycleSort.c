7680d3830c249d78693d13bee374c92f83d552d0
ErxDev <48471587+ErxDev@users.noreply.github.com>
2019-10-21 13:47:10 +0800

    Change tab into space format

--- sorting/CycleSort.c
+++ sorting/CycleSort.c
@@ -26,56 +26,56 @@ void swap(int *first, int *second){
 // Function sort the array using Cycle sort
 void cycleSort(int arr[], int n)
 {
-	// count number of memory writes
-	int writes = 0;
+    // count number of memory writes
+    int writes = 0;
 
-	// traverse array elements and put it to on
-	// the right place
-	for (int cycle_start = 0; cycle_start <= n - 2; cycle_start++) {
-		// initialize item as starting point
-		int item = arr[cycle_start];
+    // traverse array elements and put it to on
+    // the right place
+    for (int cycle_start = 0; cycle_start <= n - 2; cycle_start++) {
+        // initialize item as starting point
+        int item = arr[cycle_start];
 
-		// Find position where we put the item. We basically
-		// count all smaller elements on right side of item.
-		int pos = cycle_start;
-		for (int i = cycle_start + 1; i < n; i++)
-			if (arr[i] < item)
-				pos++;
+        // Find position where we put the item. We basically
+        // count all smaller elements on right side of item.
+        int pos = cycle_start;
+        for (int i = cycle_start + 1; i < n; i++)
+            if (arr[i] < item)
+                pos++;
 
-		// If item is already in correct position
-		if (pos == cycle_start)
-			continue;
+        // If item is already in correct position
+        if (pos == cycle_start)
+            continue;
 
-		// ignore all duplicate elements
-		while (item == arr[pos])
-			pos += 1;
+        // ignore all duplicate elements
+        while (item == arr[pos])
+            pos += 1;
 
-		// put the item to it's right position
-		if (pos != cycle_start) {
-			swap(&item, &arr[pos]);
-			writes++;
-		}
+        // put the item to it's right position
+        if (pos != cycle_start) {
+            swap(&item, &arr[pos]);
+            writes++;
+        }
 
-		// Rotate rest of the cycle
-		while (pos != cycle_start) {
-			pos = cycle_start;
+        // Rotate rest of the cycle
+        while (pos != cycle_start) {
+            pos = cycle_start;
 
-			// Find position where we put the element
-			for (int i = cycle_start + 1; i < n; i++)
-				if (arr[i] < item)
-					pos += 1;
+            // Find position where we put the element
+            for (int i = cycle_start + 1; i < n; i++)
+                if (arr[i] < item)
+                    pos += 1;
 
-			// ignore all duplicate elements
-			while (item == arr[pos])
-				pos += 1;
+            // ignore all duplicate elements
+            while (item == arr[pos])
+                pos += 1;
 
-			// put the item to it's right position
-			if (item != arr[pos]) {
-				swap(&item, &arr[pos]);
-				writes++;
-			}
-		}
-	}
+            // put the item to it's right position
+            if (item != arr[pos]) {
+                swap(&item, &arr[pos]);
+                writes++;
+            }
+        }
+    }
 
 }
 
@@ -83,7 +83,7 @@ void cycleSort(int arr[], int n)
 // Driver program to test above function
 int main()
 {
-	int n;	// Size of array elements
+    int n;  // Size of array elements
 
     printf("Enter size of array:\n");
     scanf("%d", &n); // E.g. 8
@@ -95,12 +95,12 @@ int main()
         scanf("%d", &arr[i] );
     }
 
-	printf("Original array: ");
-	display(arr, n);
+    printf("Original array: ");
+    display(arr, n);
 
-	cycleSort(arr, n);
-	printf("Sorted array: ");
-	display(arr, n);
+    cycleSort(arr, n);
+    printf("Sorted array: ");
+    display(arr, n);
 
-	return 0;
+    return 0;
 }
