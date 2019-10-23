315d5042a766200426f6b54fef2d9eb2d6e77ca2
Sai-nook73 <43719535+Sai-nook73@users.noreply.github.com>
2019-10-23 07:53:03 +0530

    Update SelectionSort.c

--- sorting/SelectionSort.c
+++ sorting/SelectionSort.c
@@ -19,7 +19,7 @@ void selectionSort(int arr[], int size){
     for(int i=0; i<size; i++) {
         for(int j=i+1; j<size; j++) {
             if(arr[i] > arr[j]) {
-                temp==arr[i];
+                temp=arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
             }
