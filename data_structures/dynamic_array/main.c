c3e10d552ad24c7c41db5a3e69eab6d63a591f9f
Wesllhey Holanda <wesllhey.holanda@gmail.com>
2020-04-15 00:16:22 -0300

    dynamic array data structure

---
+++ data_structures/dynamic_array/main.c
@@ -0,0 +1,33 @@
+#include "dynamic_array.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    dynamic_array_t *da = init_dynamic_array();
+
+    for (int i = 1; i <= 50; i++) {
+        add(da, &i);
+    }
+
+    delete (da, 10);
+
+    int value = 1000;
+
+    put(da, &value, 0);
+
+    value = 5000;
+
+    int another_value = 7000;
+
+    add(da, &another_value);
+
+    for (int i = 0; i < da->size; i++) {
+        printf("value %d\n", *(int *)get(da, i));
+    }
+
+    int value_for_invalid_index = 10000;
+
+    put(da, &value_for_invalid_index, 150);
+    return 0;
+}
