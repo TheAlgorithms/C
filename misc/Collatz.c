447b1160a44cff6ae45cbfde1b3b703f26baba0d
Krishna Vedala <krishna.vedala@ieee.org>
2020-04-28 17:03:10 -0400

    read number from commandline & use long long unsigned integers

--- misc/Collatz.c
+++ misc/Collatz.c
@@ -2,25 +2,30 @@
 collatz conjecture: a series for a number n in which if n even then the next number is n/2 ,but if n is odd then the next number is 3n+1.
 this series continues till it reaches 1*/
 
-#include<stdio.h>
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
 {
-    int n,curr_no;   
-    scanf("%d",&n);     //input number 
-    curr_no=n;        //curr_no stores input number n   
-    while(curr_no!=1)     //loop till series reaches 1
+    unsigned long long n, curr_no, num_steps = 0;
+    if (argc == 2)
+        n = strtoull(argv[1], NULL, 10);
+    else
     {
-        if(curr_no%2==0)      //condition   for even number
-        {
-            curr_no=curr_no/2;
-            printf("%d->",curr_no);
-        }    
-        else 
-        {
-            curr_no=(curr_no*3)+1;      //condition for odd number
-            printf("%d->",curr_no);
-        }
+        printf("Enter starting number: ");
+        scanf("%lu", &n); //input number
     }
-    printf("1");
+
+    curr_no = n;         //curr_no stores input number n
+    while (curr_no != 1) //loop till series reaches 1
+    {
+        num_steps++;
+        printf("%llu->", curr_no);
+        if (curr_no % 2 == 0) //condition   for even number
+            curr_no = curr_no / 2;
+        else
+            curr_no = (curr_no * 3) + 1; //condition for odd number
+    }
+    printf("1\nNumber of steps: %llu\n", num_steps);
     return 0;
 }
