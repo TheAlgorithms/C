cf696ddc6d946442e42a8d936602df9c081267f1
Sai Sriram Kalavagunta <sk842@snu.edu.in>
2020-05-20 15:40:14 +0530

    Modified code of misc/Factorial.c to accomodate larger numbers.

--- misc/Factorial.c
+++ misc/Factorial.c
@@ -1,20 +1,32 @@
-#include <stdio.h>
+#include<stdio.h>
+int main()
+{
+    int a[200],n,counter,temp,i;
+    a[0]=1;
+    counter=0;
+    printf("Enter a whole number to Find its Factorial: ");
+    scanf("%d",&n);
+   if(n<0)
+    printf("Cannot Calculate factorials for negative numbers.");
+   else{
+    for(; n>=2; n--)
+    {
+        temp=0;
+        for(i=0; i<=counter; i++)
+        {
+            temp=(a[i]*n)+temp;
+            a[i]=temp%10;
+            temp=temp/10;
+        }
+        while(temp>0)
+        {
+            a[++counter]=temp%10;
+            temp=temp/10;
+        }
+    }
+    for(i=counter; i>=0; i--)
+        printf("%d",a[i]);
+	}
+    return 0;
 
-int fat(int number){
-	if (number == 1 || number == 0) 
-		return 1;
-	else 
-		return number*fat(number-1);
-}
-
-int main(){
-	int number;
-
-	//Asks the factorial of the number n 
-	printf("Number: ");
-	scanf("%d", &number);
-	
-	printf("%d\n", fat(number));
-	
-	return 0;
 }
