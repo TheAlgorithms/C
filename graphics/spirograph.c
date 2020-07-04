f36746ccd06197674c7d883e541c5715d1521640
Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2020-07-04 11:57:52 -0400

    provide custom glutBitmapString cuntion

--- graphics/spirograph.c
+++ graphics/spirograph.c
@@ -106,17 +106,16 @@ void test()
 #ifdef USE_GLUT  // this is set by CMAKE automatically, if available
 #ifdef __APPLE__
 #include <GLUT/glut.h>  // include path on Macs is different
-
-/** A wrapper that is not implemented on MacOS
- */
-void glutBitmapString(void *font, char *string)
-{
-    for (char *ch = string; *ch != '\0'; ch++) glutBitmapCharacter(font, *ch);
-}
-
 #else
 #include <GL/glut.h>
 #endif
+
+/** A wrapper that is not available in all GLUT implementations.
+ */
+static inline void glutBitmapString(void *font, char *string)
+{
+    for (char *ch = string; *ch != '\0'; ch++) glutBitmapCharacter(font, *ch);
+}
 
 /**
  * @brief Function to graph (x,y) points on the OpenGL graphics window.
