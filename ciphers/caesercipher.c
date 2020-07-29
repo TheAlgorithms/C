8692f95904d9f7a7781359b89b895573fc718e33
Rishabh Patel <56493772+Rishabhpatel803@users.noreply.github.com>
2020-07-29 09:39:03 +0530

    Create caesercipher.c

---
+++ ciphers/caesercipher.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<conio.h>
+
+void encrypt()
+{
+    char msg[100], ch;
+    int i,key;
+    printf("Enter a plaintext : ");
+    scanf("%s",&msg);
+    printf("\nEnter key : ");
+    scanf("%d",&key);
+    for(i=0;msg[i]!='\0';i++)
+    {
+	ch=msg[i];
+	if(ch>='a'&&ch<='z')
+	{
+	    ch=ch+key;
+	    if(ch>'z')
+	    {
+		ch=ch-'z'+'a'-1;
+	    }
+	    msg[i]=ch;
+	}
+	else if(ch>='A'&&ch<'Z')
+	{
+	    ch=ch+key;
+	    if(ch>'Z')
+	    {
+		ch=ch='Z'+'A'-1;
+	    }
+	    msg[i]=ch;
+	}
+    }
+    printf("\nEncrypted message: %s", msg);
+    printf("\nPress Enter to continue....");
+    getch();
+}
+
+void decrypt()
+{
+    char msg[100], ch;
+    int i,key;
+    printf("Enter a Encrypted message : ");
+    scanf("%s",&msg);
+    printf("\nEnter key : ");
+    scanf("%d",&key);
+    for(i=0;msg[i]!='\0';i++)
+    {
+	ch=msg[i];
+	if(ch>='a'&&ch<='z')
+	{
+	    ch=ch-key;
+	    if(ch<'a')
+	    {
+		ch=ch+'z'-'a'+1;
+	    }
+	    msg[i]=ch;
+	}
+	else if(ch>='A'&&ch<'Z')
+	{
+	    ch=ch+key;
+	    if(ch<'A')
+	    {
+		ch=ch+'Z'-'A'+1;
+	    }
+	    msg[i]=ch;
+	}
+    }
+    printf("\nDycrypted message: %s", msg);
+    printf("\nPress Enter to continue....");
+    getch();
+}
+void main()
+{
+	int choice;
+	while(1)
+	{
+		clrscr();
+		printf("Enter \n 1.Encrytpion\n 2.Decryption\n 3.Exit");
+		printf("\nEnter your choice : ");
+		scanf("%d",&choice);
+		if(choice==1)
+		encrypt();
+		else if(choice==2)
+		decrypt();
+		else if(choice==3)
+		exit(0);
+		else
+		printf("\nWrong choice! Enter again");
+		getch();
+	}
+}
