878f0fa33cc2b78b604c34b4220d1a5d3ca26978
Rishabh Patel <56493772+Rishabhpatel803@users.noreply.github.com>
2020-07-30 10:02:01 +0530

    Update caeser_cipher.c

--- ciphers/caeser_cipher.c
+++ ciphers/caeser_cipher.c
@@ -1,92 +1,76 @@
-//Code by Rishabh Patel
+// Code by Rishabh Patel
 
-#include<stdio.h>
+#include <stdio.h>
 
-void encrypt()
-{
-    char msg[100], ch;
-    int i,key;
-    printf("Enter a plaintext : ");
-    scanf("%s",&msg);
-    printf("\nEnter key : ");
-    scanf("%d",&key);
-    for(i=0;msg[i]!='\0';i++)
-    {
-	ch=msg[i];
-	if(ch>='a'&&ch<='z')
-	{
-	    ch=ch+key;
-	    if(ch>'z')
-	    {
-		ch=ch-'z'+'a'-1;
-	    }
-	    msg[i]=ch;
-	}
-	else if(ch>='A'&&ch<'Z')
-	{
-	    ch=ch+key;
-	    if(ch>'Z')
-	    {
-		ch=ch='Z'+'A'-1;
-	    }
-	    msg[i]=ch;
-	}
+void encrypt() {
+  char msg[100], ch;
+  int i, key;
+  printf("Enter a plaintext : ");
+  scanf("%s", &*msg);
+  printf("\nEnter key : ");
+  scanf("%d", &key);
+  for (i = 0; msg[i] != '\0'; i++) {
+    ch = msg[i];
+    if (ch >= 'a' && ch <= 'z') {
+      ch = ch + key;
+      if (ch > 'z') {
+        ch = ch - 'z' + 'a' - 1;
+      }
+      msg[i] = ch;
+    } else if (ch >= 'A' && ch < 'Z') {
+      ch = ch + key;
+      if (ch > 'Z') {
+        ch = ch - 'Z' + 'A' - 1;
+      }
+      msg[i] = ch;
     }
-    printf("\nEncrypted message: %s", msg);
-    printf("\nPress Enter to continue....");
-    getch();
+  }
+  printf("\nEncrypted message: %s", msg);
+  printf("\nPress Enter to continue....");
 }
 
-void decrypt()
-{
-    char msg[100], ch;
-    int i,key;
-    printf("Enter a Encrypted message : ");
-    scanf("%s",&msg);
-    printf("\nEnter key : ");
-    scanf("%d",&key);
-    for(i=0;msg[i]!='\0';i++)
-    {
-	ch=msg[i];
-	if(ch>='a'&&ch<='z')
-	{
-	    ch=ch-key;
-	    if(ch<'a')
-	    {
-		ch=ch+'z'-'a'+1;
-	    }
-	    msg[i]=ch;
-	}
-	else if(ch>='A'&&ch<'Z')
-	{
-	    ch=ch+key;
-	    if(ch<'A')
-	    {
-		ch=ch+'Z'-'A'+1;
-	    }
-	    msg[i]=ch;
-	}
+void decrypt() {
+  char msg[100], ch;
+  int i, key;
+  printf("Enter a Encrypted message : ");
+  scanf("%s", &*msg);
+  printf("\nEnter key : ");
+  scanf("%d", &key);
+  for (i = 0; msg[i] != '\0'; i++) {
+    ch = msg[i];
+    if (ch >= 'a' && ch <= 'z') {
+      ch = ch - key;
+      if (ch < 'a') {
+        ch = ch + 'z' - 'a' + 1;
+      }
+      msg[i] = ch;
+    } else if (ch >= 'A' && ch < 'Z') {
+      ch = ch + key;
+      if (ch < 'A') {
+        ch = ch + 'Z' - 'A' + 1;
+      }
+      msg[i] = ch;
     }
-    printf("\nDycrypted message: %s", msg);
-    printf("\nPress Enter to continue....");
-    getch();
+  }
+  printf("\nDycrypted message: %s", msg);
+  printf("\nPress Enter to continue....");
 }
-void main()
-{
-	int choice;
-	while(1)
-	{
-		printf("Enter \n 1.Encrytpion\n 2.Decryption\n 3.Exit");
-		printf("\nEnter your choice : ");
-		scanf("%d",&choice);
-		if(choice==1){
-		encrypt();}
-		else if(choice==2){
-		decrypt();}
-		else if(choice==3){
-		exit(0);}
-		else{
-		printf("\nWrong choice! Enter again");}
-		getch();
-	}
+int main() {
+  int choice;
+  while (1) {
+    printf("Enter \n 1.Encrytpion\n 2.Decryption");
+    printf("\nEnter your choice : ");
+    scanf("%d", &choice);
+    if (choice == 1) {
+      encrypt();
+    } else if (choice == 2) {
+      decrypt();
+    }
+    /*else if(choice==3){
+    exit(0);}*/
+    else {
+      printf("\nWrong choice! Enter again");
+    }
+  }
+  return 0;
 }
