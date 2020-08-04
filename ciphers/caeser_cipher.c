e83051571b7df87c52befdf864fffa7d35fd234e
Rishabh Patel <56493772+Rishabhpatel803@users.noreply.github.com>
2020-08-04 08:48:47 +0530

    Update caeser_cipher.c

--- ciphers/caeser_cipher.c
+++ ciphers/caeser_cipher.c
@@ -1,31 +1,35 @@
 /**
  * @file
- * 
- *
+ * @brief: caeser cipher is the one of the basic cipher in cryptography
+ * @details: it is encoded by shifting each letter to the forward and 
+ * backward to make it encrypted
  * \author [Rishabh Patel](https://github.com/Rishabhpatel803)
  */
 
 #include <stdio.h>
 
+/*
+* Encrypt function encrypts the message to the required one
+*/
 void encrypt() {
-  char msg[100], ch;
+  char msg[100], ch;//msg is used to store the message
   int i, key;
   printf("Enter a plaintext : ");
-  scanf("%s", &*msg);
+  scanf("%s", &*msg);//Inputting of message
   printf("\nEnter key : ");
-  scanf("%d", &key);
+  scanf("%d", &key);//Inoutting key
   for (i = 0; msg[i] != '\0'; i++) {
     ch = msg[i];
     if (ch >= 'a' && ch <= 'z') {
       ch = ch + key;
       if (ch > 'z') {
-        ch = ch - 'z' + 'a' - 1;
+        ch = ch - 'z' + 'a' - 1;//doing encryption of lower case letters
       }
       msg[i] = ch;
     } else if (ch >= 'A' && ch < 'Z') {
       ch = ch + key;
       if (ch > 'Z') {
-        ch = ch - 'Z' + 'A' - 1;
+        ch = ch - 'Z' + 'A' - 1;//doing encryption of upper case letters
       }
       msg[i] = ch;
     }
@@ -34,25 +38,28 @@ void encrypt() {
   printf("\nPress Enter to continue....");
 }
 
+/*
+* Decrypt function encrypts the message to the required one
+*/
 void decrypt() {
-  char msg[100], ch;
+  char msg[100], ch;//msg is for storing message
   int i, key;
   printf("Enter a Encrypted message : ");
-  scanf("%s", &*msg);
+  scanf("%s", &*msg);//reading enccypted message
   printf("\nEnter key : ");
-  scanf("%d", &key);
+  scanf("%d", &key);//reading key
   for (i = 0; msg[i] != '\0'; i++) {
     ch = msg[i];
     if (ch >= 'a' && ch <= 'z') {
       ch = ch - key;
       if (ch < 'a') {
-        ch = ch + 'z' - 'a' + 1;
+        ch = ch + 'z' - 'a' + 1;//decryption of lower case letter
       }
       msg[i] = ch;
     } else if (ch >= 'A' && ch < 'Z') {
       ch = ch + key;
       if (ch < 'A') {
-        ch = ch + 'Z' - 'A' + 1;
+        ch = ch + 'Z' - 'A' + 1;//decryption of upper case letter
       }
       msg[i] = ch;
     }
@@ -63,6 +70,7 @@ void decrypt() {
 int main() {
   int choice;
   while (1) {
+    //Giving user choice that he wants to encrypt or decrypt
     printf("Enter \n 1.Encrytpion\n 2.Decryption");
     printf("\nEnter your choice : ");
     scanf("%d", &choice);
