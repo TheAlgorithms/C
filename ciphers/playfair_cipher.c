ac41391abfce19840830224dc75d4b9b7dd2f22d
Rishabh Patel <56493772+Rishabhpatel803@users.noreply.github.com>
2020-08-06 09:45:30 +0530

    Update playfair_cipher.c

--- ciphers/playfair_cipher.c
+++ ciphers/playfair_cipher.c
@@ -13,9 +13,9 @@
 
 int choice;
 /*
-* designing the function of encryption and
-* decryption using playfair cipher
-*/
+ * designing the function of encryption and
+ * decryption using playfair cipher
+ */
 void playfair(char ch1, char ch2, char key[MX][MX]) {
   int i, j, w, x, y, z;
   for (i = 0; i < MX; i++) {
@@ -70,99 +70,130 @@ void removeDuplicates(char str[]) {
   }
   *(str + lastUniqueIndex) = '\0';
 }
+
+void playfaircipher(char *str,char *keystr)
+{
+    int i, j, k = 0, l, m = 0, n;
+    char key[MX][MX], keyminus[25];
+
+    char alpa[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                     'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                     'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    printf("\t\t\t\tPlayFair Cipher");
+    // convert the characters to uppertext
+    for (i = 0; i < n; i++)
+    {
+        if (keystr[i] == 'j')
+        {
+            keystr[i] = 'i';
+        }
+        else if (keystr[i] == 'J')
+        {
+            keystr[i] = 'I';
+        }
+        keystr[i] = toupper(keystr[i]);
+    }
+
+    // convert all the characters of plaintext to uppertext
+    for (i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == 'j')
+        {
+            str[i] = 'i';
+        }
+        else if (str[i] == 'J')
+        {
+            str[i] = 'I';
+        }
+        str[i] = toupper(str[i]);
+    }
+    // store all characters except key
+    j = 0;
+    for (i = 0; i < 26; i++)
+    {
+        for (k = 0; k < n; k++)
+        {
+            if (keystr[k] == alpa[i])
+            {
+                break;
+            }
+            else if (alpa[i] == 'J')
+            {
+                break;
+            }
+        }
+        if (k == n)
+        {
+            keyminus[j] = alpa[i];
+            j++;
+        }
+    }
+
+    // construct key keymatrix
+    k = 0;
+    for (i = 0; i < MX; i++)
+    {
+        for (j = 0; j < MX; j++)
+        {
+            if (k < n)
+            {
+                key[i][j] = keystr[k];
+                k++;
+            }
+            else
+            {
+                key[i][j] = keyminus[m];
+                m++;
+            }
+            printf("%c ", key[i][j]);
+        }
+        printf("\n");
+    }
+
+    // construct diagram and convert to cipher text
+    printf("\nEntered text :%s\nOutput Text :", str);
+    for (i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == 'J')
+        {
+            str[i] = 'I';
+        }
+        if (str[i + 1] == '\0')
+        {
+            playfair(str[i], 'X', key);
+        }
+        else
+        {
+            if (str[i + 1] == 'J')
+            {
+                str[i + 1] = 'I';
+            }
+            if (str[i] == str[i + 1])
+            {
+                playfair(str[i], 'X', key);
+            }
+            else
+            {
+                playfair(str[i], str[i + 1], key);
+                i++;
+            }
+        }
+    }
+}
+
+void test() 
+{ 
+    char msg[] = "Playfair", key[] = "Good";
+    choice = 1;
+    printf("First Test Case");
+    playfaircipher(msg, key);
+    printf("Second Test Case");
+    choice = 2;
+    char msg1[] = "Cryptogaphy", key1[] = "encrypt";
+    playfaircipher(msg1, key1);
+}
 int main() {
-  int i, j, k = 0, l, m = 0, n;
-  char key[MX][MX], keyminus[25], keystr[10], str[25] = {0};
-
-  char alpa[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-                   'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                   'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-  printf("\t\t\t\tPlayFair Cipher");
-  printf("\n1.Encryption\n2.Decryption\n\nChoice(1 or 2):");
-  scanf("%d", &choice);
-  if (choice != 1 && choice != 2) {
-    printf("Invalid Choice");
-    return 0;
-  }
-  fflush(stdin);
-  printf("\nEnter key:");
-  scanf("%s", &*keystr);
-  printf("Enter the text:");
-  scanf("%s", &*str);
-  removeDuplicates(keystr);
-  n = strlen(keystr);
-
-  // convert the characters to uppertext
-  for (i = 0; i < n; i++) {
-    if (keystr[i] == 'j') {
-      keystr[i] = 'i';
-    } else if (keystr[i] == 'J') {
-      keystr[i] = 'I';
-    }
-    keystr[i] = toupper(keystr[i]);
-  }
-
-  // convert all the characters of plaintext to uppertext
-  for (i = 0; i < strlen(str); i++) {
-    if (str[i] == 'j') {
-      str[i] = 'i';
-    } else if (str[i] == 'J') {
-      str[i] = 'I';
-    }
-    str[i] = toupper(str[i]);
-  }
-
-  // store all characters except key
-  j = 0;
-  for (i = 0; i < 26; i++) {
-    for (k = 0; k < n; k++) {
-      if (keystr[k] == alpa[i]) {
-        break;
-      } else if (alpa[i] == 'J') {
-        break;
-      }
-    }
-    if (k == n) {
-      keyminus[j] = alpa[i];
-      j++;
-    }
-  }
-
-  // construct key keymatrix
-  k = 0;
-  for (i = 0; i < MX; i++) {
-    for (j = 0; j < MX; j++) {
-      if (k < n) {
-        key[i][j] = keystr[k];
-        k++;
-      } else {
-        key[i][j] = keyminus[m];
-        m++;
-      }
-      printf("%c ", key[i][j]);
-    }
-    printf("\n");
-  }
-
-  // construct diagram and convert to cipher text
-  printf("\nEntered text :%s\nOutput Text :", str);
-  for (i = 0; i < strlen(str); i++) {
-    if (str[i] == 'J') {
-      str[i] = 'I';
-    }
-    if (str[i + 1] == '\0') {
-      playfair(str[i], 'X', key);
-    } else {
-      if (str[i + 1] == 'J') {
-        str[i + 1] = 'I';
-      }
-      if (str[i] == str[i + 1]) {
-        playfair(str[i], 'X', key);
-      } else {
-        playfair(str[i], str[i + 1], key);
-        i++;
-      }
-    }
-  }
+  
+  test();
   return 0;
 }
