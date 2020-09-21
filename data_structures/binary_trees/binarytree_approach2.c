24ff07f6acee3babe0b85cdf3d12ffd294ccd855
Aarti002 <70666307+Aarti002@users.noreply.github.com>
2020-09-21 08:19:13 +0530

    Update binarytree_approach2.c
    *proper implementation of a binary search tree with documentation.

--- data_structures/binary_trees/binarytree_approach2.c
+++ data_structures/binary_trees/binarytree_approach2.c
@@ -1,15 +1,30 @@
-#include <stdio.h>
+/*
+this file have compelete implementation of a binary search tree and has all function defined 
+including insert,find,findMax,delet_tree,delete(to delete a node),preOrder and height.
+which is required for compelete implementation of a tree
+*/
+
+#include <stdio.h>				/*This header files are required to take user input and it has some predefined function which enable us to use them in our code (*if required)*/
 #include <stdlib.h>
 
+/*this is a struct method in c/c++ through which we create our own datatype and this datatype would create a memory block or lets name it by NODE
+with three portion in it two part of a node will store address of any other node of same datatype (*Node) 
+=> left= stores address of left part of each subtree in a tree
+=> right= stores address of right part of each subtree in a tree
+=> data= contains value of each node of datatype int.
+and remaining one part of each node will store data of type integer.*/
 
-typedef struct Node
+typedef struct Node			
 {
     struct Node *left;  
     struct Node *right; 
     int data;           
-} Node;
+} Node;			// here we assign a name (making it easier for the user to use in code)to our explicitly defined datatype;)
 
-Node *newNode(int data)
+/*the function newNode defining below will create a node of of datatype Node and 
+put *left=NULL and *right=NULL at the time of creating a new node and returns the address of that newly created node as its return type is Node*
+*/
+Node *newNode(int data)				
 {
     // creates a node
     Node *tmp = (Node *)malloc(sizeof(Node));
@@ -21,6 +36,12 @@ Node *newNode(int data)
     return tmp;
 }
 //inserting data in nodes
+/*function insert would put a value in data part of each node and has return type Node* (address of the node) 
+according to the defination of a binary tree this function will work in a same way i.e if value to be kept in a node is less than the value stored at root or head node then that
+node will be the part of left subtree,
+similerly,
+if value to be kept in a node is greater than the value of root or head node then that node will be the part of right subtree
+*/
 Node *insert(Node *root, int data)
 {
     // If the root of the subtree is null, insert key here
@@ -35,7 +56,9 @@ Node *insert(Node *root, int data)
     // Returns the modified tree
     return root;
 }
-
+/* getMax function will work only on the right part of the tree and will return the address of the right most node in a tree
+as right most node has the maximum value stored in it data part (according to the defination of BST)
+*/
 Node* getMax(Node* root)
 {
 	if(root==NULL)
@@ -48,7 +71,11 @@ Node* getMax(Node* root)
 	else
 		return root; 
 }
+/*
+function delete_node returns address of root node after deleting that required node 
+first it seach for that node if it is there then it delete 
 
+*/
 Node *delete_node (Node *root, int data)
 {
     // If the root is null, nothing to be done
@@ -92,7 +119,10 @@ Node *delete_node (Node *root, int data)
     }
     return root;
 }
-
+/*
+function Find search the node with value data which we provide as parameter in this function which returns 0 if node with value "data" is not present in the tree
+or 1 if such node is there is present
+*/
 
 int find(Node *root, int data)
 {
@@ -110,7 +140,10 @@ int find(Node *root, int data)
         return 0;
     }
 }
-
+/* 
+function Height return the height of the tree which is the highest number of node lieing in any path from root/head node to a leaf node(having no child)
+return type oviously would be integer.
+*/
 int height(Node *root)
 {
     // If the root is null, this is the bottom of the tree (height 0)
@@ -132,7 +165,9 @@ int height(Node *root)
             return (left_h + 1);
     }
 }
-
+/*
+this function removes the whole tree (which was created dynamically using malloc) using free method
+*/
 void deletetree(Node *root)
 {
 	if(root==NULL)
@@ -141,7 +176,10 @@ void deletetree(Node *root)
 	deletetree(root->right);
 	free(root);
 }
-
+/*
+this preOrder function would print value (stored at data part of the node) and this prints the value in following manner
+head/root  ->  left-subtree  ->  right-subtree
+*/
 void preOrder(Node *root)
 {
     if (root != NULL)
@@ -152,7 +190,8 @@ void preOrder(Node *root)
     }
 }
 
-/** Main funcion */
+/** Main funcion 
+it has a menu (switch-cases) containg the list of function (defined above) to perform any particuler action*/
 int main()
 {
     // this reference don't change.
