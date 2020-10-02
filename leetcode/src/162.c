296007c2bfceaa61a106eec97c2128b812cc9317
PalAditya <adityapal.nghss@gmail.com>
2020-10-02 08:43:53 +0530

    Update comments to Javadoc style

--- leetcode/src/162.c
+++ leetcode/src/162.c
@@ -1,39 +1,49 @@
-//We use binary Search to find the local minimum
-//If middle element is greater than any of its neighbors, it is a peak element
-//ELse, if element on right is greater, a peak element on right must exist as numbers either increase to a point and decrease (which
-//is the peak) or continuously increase to the end of the array, which is then the peak
-//Same if element on left is greater
+/** We use binary search to find the local minimum efficiently
+ * If middle element is greater than any of its neighbors, it is a peak element
+ * ELse, if element on right is greater, a peak element on right must exist as numbers either increase to a point and decrease (which
+ * is the peak) or continuously increase to the end of the array, which is then the peak.
+ * Same if element on left is greater
+ */
 int findPeakElement(int* nums, int numSize)
 {
     int i, mid, start = 0, end = numSize - 1;
-    if (numSize == 1) // Handle edge case first
+    /**
+     * Handle the edge case with single element first
+	 */
+    if (numSize == 1)
 	{ 
         return 0;
     }
     while (start <= end)
 	{
         mid = (start + end) / 2;
-        // We check that there is no out of bounds case first
+        /**
+         * First, we check and ensure that there is no out of bounds case first, which can happen if only 2 elements are there
+         * If we enter the next if block, we are at rightmost edge so we need only be greater than left element
+         * If we enter the 3rd if block, we are at leftmost edge so we need only be greater than right element
+         * After that, both blocks are checked to see which half to go to
+         * Finally if start = end = mid, we return that as answer is guaranteed to exist as per the constraints
+         */
         if (mid - 1 >= 0 && nums[mid - 1] < nums[mid] && mid + 1 < numSize && nums[mid] > nums[mid + 1]) 
 		{ 
             return mid;
         }
-        else if (mid + 1 == numSize && nums[mid] > nums[mid - 1]) // At rightmost edge
+        else if (mid + 1 == numSize && nums[mid] > nums[mid - 1])
 		{ 
             return mid;
         }
-        else if (mid - 1 < 0 && nums[mid] > nums [mid + 1]) // At leftmost edge
+        else if (mid - 1 < 0 && nums[mid] > nums [mid + 1])
 		{ 
             return mid;
         }
-        else if (nums[mid + 1] > nums[mid]) // Start checking in right half
+        else if (nums[mid + 1] > nums[mid])
 		{
             start = mid + 1;
         }
-        else // Start checking in left half
+        else
 		{
             end = mid - 1;
         }
     }
-    return mid; // If start = end = mid, return that as answer is guaranteed to exist as per the constraints
+    return mid;
 }
