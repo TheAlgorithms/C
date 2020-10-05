93332ee448d37f113100d5a3421cda957cf4a586
Yashasvi Goel <31849052+yashasvi-goel@users.noreply.github.com>
2020-10-05 18:45:27 +0530

    Create sol1.c

---
+++ project_euler/problem_59/sol1.c
@@ -0,0 +1,89 @@
+/**
+ * \file
+ * \brief [Problem 59](https://projecteuler.net/problem=59) solution -
+ * optimization using look-up array
+ * \author [Yashasvi Goel](https://github.com/yashasvi-goel/)
+ *
+ */
+#include<stdio.h>
+int check(int a)
+{
+    if(a>=65&&a<=90)
+        return 1;
+    else if(a>=97&&a<=122)
+        return 1;
+    else if(a>=48&&a<=59)
+        return 1;
+    else if(a=='('||a==')'||a=='.'||a==','||a=='\''||a=='?'||a=='-'||a=='!'||a==' ')
+        return 1;
+    return 0;
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int *a=(int*)malloc(sizeof(int)*n);
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    int x=0;
+    int h=0;
+    for(int i=97;i<123;i++)
+    {
+        x=0;
+        h=0;
+        for(int j=0;j<n;j=j+3)
+        {
+            x=i^a[j];
+            if(!check(x))
+            {
+                h=1;
+                break;
+            }
+        }
+        if(h==0)
+        {
+            printf("%c",i);
+            break;
+        }
+    }
+    for(int i='a';i<='z';i++)
+    {
+        x=0;
+        h=0;
+        for(int j=1;j<n;j=j+3)
+        {                
+            x=i^a[j];
+            if(!check(x))
+            {
+                h=1;
+        //        printf(":%c:%c\n",x,i);
+                break;
+            }
+        }
+        if(h==0)
+        {
+            printf("%c",i);
+            break;
+        }
+    }
+    for(int i=97;i<123;i++)
+    {
+        x=0;
+        h=0;
+        for(int j=2;j<n;j=j+3)
+        {                
+            x=i^a[j];
+            if(!check(x))
+            {
+                h=1;
+                break;
+            }
+        }
+        if(h==0)
+        {
+            printf("%c",i);
+            break;
+        }
+    }
+    return 0;
+}
