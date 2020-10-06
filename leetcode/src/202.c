9a95e846f7b1a7662fb0be5d5c08a9fbe2ad21f4
Nguyen Long Nhat <torn4dom4n@gmail.com>
2020-10-07 00:16:06 +0700

    Solve 202.c

---
+++ leetcode/src/202.c
@@ -0,0 +1,25 @@
+int numSquareSum(int n)
+{ 
+    int squareSum = 0;
+    while (n)
+    { 
+        squareSum += (n % 10) * (n % 10); 
+        n /= 10; 
+    }
+    return squareSum; 
+}
+
+bool isHappy(int n)
+{
+    int slow, fast;
+    slow = fast = n;
+    
+    do
+    {
+        slow = numSquareSum(slow);
+        fast = numSquareSum(numSquareSum(fast));
+    } 
+    while (slow != fast);
+
+    return (slow == 1); 
+}
