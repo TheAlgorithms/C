503d6a9c557dc505435039d44aeb2791358f645e
Gowry Sugathan <38757089+gowry189@users.noreply.github.com>
2020-10-08 21:14:02 +0530

    Update best_time_to_buy_and_sell_stocks.cpp

--- leetcode/src/best_time_to_buy_and_sell_stocks.cpp
+++ leetcode/src/best_time_to_buy_and_sell_stocks.cpp
@@ -6,15 +6,15 @@ Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxProfit=0;
-        int min=INT_MAX;
+        int maxProfit = 0;
+        int min = INT_MAX;
         int n = prices.size();
-        for(int i=0;i<n;i++){
-            if(prices[i]<min){
-                min=prices[i];
+        for(int i=0; i<n; i++){
+            if(prices[i] < min){
+                min = prices[i];
             }
-            if(maxProfit<(prices[i]-min)){
-                maxProfit =prices[i]-min;
+            if(maxProfit < (prices[i]-min)){
+                maxProfit = prices[i]-min;
             }
         }
         return maxProfit;
