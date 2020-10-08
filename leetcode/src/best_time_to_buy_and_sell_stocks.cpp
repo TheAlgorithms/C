a98c08b831dda850986719b298dbf1962d3df522
Gowry Sugathan <38757089+gowry189@users.noreply.github.com>
2020-10-08 21:13:18 +0530

    Update and rename 121.cpp to best_time_to_buy_and_sell_stocks.cpp

--- leetcode/src/best_time_to_buy_and_sell_stocks.cpp
+++ leetcode/src/best_time_to_buy_and_sell_stocks.cpp
@@ -1,3 +1,8 @@
+/*Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.*/
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
