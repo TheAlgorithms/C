2345a19f3b56fdfff93bdc2e8c0683d3eff85e94
Hariprasad004 <57026806+Hariprasad004@users.noreply.github.com>
2020-10-13 17:59:05 +0530

    Optimized bubblesort.c

--- sorting/bubble_sort.c
+++ sorting/bubble_sort.c
@@ -43,31 +43,19 @@ void swap(int *first, int *second)
 void bubbleSort(int *arr, int size)
 {
     for (int i = 0; i < size - 1; i++)
-    {                         /* for each array index */
-        bool swapped = false; /* flag to check if any changes had to be made */
-        /* perform iterations until no more changes were made or outer loop
-            executed for all array indices */
+    {
         for (int j = 0; j < size - 1 - i; j++)
-        { /* for each element in the array */
-            if (arr[j] > arr[j + 1])
-            { /* if the order of successive elements needs update */
-                swap(&arr[j], &arr[j + 1]);
-                swapped = true; /* set flag */
-            }
-        }
-        if (!swapped)
         {
-            /* since no more updates we made, the array is already sorted
-                this is an optimization for early termination */
-            break;
+            if (arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+            }
         }
     }
 }
 
-/**
- * Test function
- */
-void test()
+/** Driver Code */
+int main(int argc, const char *argv[])
 {
     const int size = 10;
     int *arr = (int *)calloc(size, sizeof(int));
@@ -78,18 +66,7 @@ void test()
         arr[i] = rand() % 100;
     }
     bubbleSort(arr, size);
-    for (int i = 0; i < size - 1; ++i)
-    {
-        assert(arr[i] <= arr[i + 1]);
-    }
+    display(arr, size);
     free(arr);
-}
-
-/** Driver Code */
-int main(int argc, const char *argv[])
-{
-    /* Intializes random number generator */
-    srand(time(NULL));
-    test();
     return 0;
 }
