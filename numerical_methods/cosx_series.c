8cfe8890be5044877cfaee7727280858e473a347
Shreya <harshrey@users.noreply.github.com>
2020-10-22 18:59:32 +0530

    Update cosx_series.c

--- numerical_methods/cosx_series.c
+++ numerical_methods/cosx_series.c
@@ -7,12 +7,13 @@ int main()
     float x, x1, sum = 0.0f;
     int i, lim, j;
     printf("\n\t Cos X series: ");
-    printf("\n\t Enter X (deg): ") scanf("%f", &x);
+    printf("\n\t Enter X (deg): ");
+    scanf("%f", &x);
     printf("Enter the limit upto which you want to expand the series: ");
     scanf("%d", &lim);
     x1 = x;
     x = x * (3.1415 / 180);
-    for (i = 0; i < limit; i++)
+    for (i = 0; i < lim; i++)
     {
         j = 2 * i;
         if (i % 2 == 0)
