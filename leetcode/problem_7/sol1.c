c683a6dd12d86616a64f4bd24ea655f990c0b275
Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2020-10-30 02:59:40 -0400

    problem 7
    Signed-off-by: Krishna Vedala <7001608+kvedala@users.noreply.github.com>

--- leetcode/problem_7/sol1.c
+++ leetcode/problem_7/sol1.c
@@ -1,6 +1,22 @@
-#include <limits.h>
+/**
+ * \file
+ * \brief [7. Reverse integer](https://leetcode.com/problems/reverse-integer/)
+ * solution
+ * \details Given a 32-bit signed integer, reverse digits of an integer.
+ */
 
-int reverse(int x)
+#include <assert.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * @brief Reverse the digits of a signed number
+ *
+ * @param x input signed number
+ * @return number with reversed digits
+ */
+int32_t reverse(int32_t x)
 {
     int rev = 0;
     while (x != 0)
@@ -14,4 +30,21 @@ int reverse(int x)
         rev = rev * 10 + pop;
     }
     return rev;
+}
+
+/** Main function
+ * @returns 0
+ */
+int main()
+{
+    assert(reverse(123) == 321);
+    printf("Test 1 passed.\n");
+
+    assert(reverse(-123) == -321);
+    printf("Test 2 passed.\n");
+
+    assert(reverse(120) == 21);
+    printf("Test 3 passed.\n");
+
+    return 0;
 }
