b9ebc05b73f26ac34084d9343ad6c98e4dad4f34
Suraj Patro <47288675+Suraj-Patro@users.noreply.github.com>
2020-10-30 08:56:36 +0530

    Update conversions/binary_to_decimal.c

--- conversions/binary_to_decimal.c
+++ conversions/binary_to_decimal.c
@@ -1,5 +1,5 @@
-/**
- * Modified 07/12/2017, Kyler Smith
+/*
+ * Modified 30/10/2020, Suraj Patro
  *
  */
 
@@ -8,7 +8,7 @@
 int main()
 {
     int remainder, number = 0, decimal_number = 0, temp = 1;
-    printf("\n Enter any binary number= ");
+    printf("Enter any binary number: ");
     scanf("%d", &number);
 
     // Iterate over the number until the end.
@@ -20,5 +20,6 @@ int main()
         temp = temp * 2;  // used as power of 2
     }
 
-    printf("%d\n", decimal_number);
+    printf("Decimal equivalent is: %d\n", decimal_number);
+    return 0;
 }
