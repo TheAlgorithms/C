f23987e8ce7334e82acc66c2fceff4d213d8ae47
Suraj Patro <47288675+Suraj-Patro@users.noreply.github.com>
2020-10-30 22:09:02 +0530

    Update octal_to_binary.c

--- conversions/octal_to_binary.c
+++ conversions/octal_to_binary.c
@@ -1,12 +1,12 @@
 /**
  * @brief Octal to binay conversion by scanning user input
  * @details
- * The octalTobinary function take the octal number as long  
+ * The octalTobinary function take the octal number as long
  * return a long binary nuber after conversion
  * @author [Vishnu P](https://github.com/vishnu0pothan)
  */
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 /**
  * @brief Converet  octal number to binary
@@ -21,14 +21,14 @@ long octalToBinary(int octalnum)
     /* This loop converts octal number "octalnum" to the
      * decimal number "decimalnum"
      */
-    while(octalnum != 0)
+    while (octalnum != 0)
     {
-	decimalnum = decimalnum + (octalnum%10) * pow(8,i);
-	i++;
-	octalnum = octalnum / 10;
+        decimalnum = decimalnum + (octalnum % 10) * pow(8, i);
+        i++;
+        octalnum = octalnum / 10;
     }
 
-    //i is re-initialized
+    // i is re-initialized
     i = 1;
 
     /* This loop converts the decimal number "decimalnum" to the binary
@@ -36,17 +36,17 @@ long octalToBinary(int octalnum)
      */
     while (decimalnum != 0)
     {
-	binarynum = binarynum + (long)(decimalnum % 2) * i;
-	decimalnum = decimalnum / 2;
-	i = i * 10;
+        binarynum = binarynum + (long)(decimalnum % 2) * i;
+        decimalnum = decimalnum / 2;
+        i = i * 10;
     }
 
-    //Returning the binary number that we got from octal number
+    // Returning the binary number that we got from octal number
     return binarynum;
 }
 
 /**
- * @brief Main function  
+ * @brief Main function
  * @returns 0 on exit
  */
 int main()
@@ -56,7 +56,7 @@ int main()
     printf("Enter an octal number: ");
     scanf("%d", &octalnum);
 
-    //Calling the function octaltoBinary
+    // Calling the function octaltoBinary
     printf("Equivalent binary number is: %ld", octalToBinary(octalnum));
     return 0;
 }
