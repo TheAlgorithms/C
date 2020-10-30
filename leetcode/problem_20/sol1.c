09ed0bae7da2e43e70a6bfceccacfa84fc8cee61
Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2020-10-30 21:45:02 +0530

    problem 20

--- leetcode/problem_20/sol1.c
+++ leetcode/problem_20/sol1.c
@@ -1,6 +1,34 @@
-bool isValid(char *s)
+/**
+ * \file
+ * \brief [20. Valid
+ * Parentheses](https://leetcode.com/problems/valid-parentheses/)
+ * solution
+ * \details
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ * * Open brackets must be closed by the same type of brackets.
+ * * Open brackets must be closed in the correct order.
+ */
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief Function to check if input string has valid parantheses.
+ *
+ * @param s stricng to check
+ * @return `true` if valid
+ * @return `false` if invalid
+ */
+bool isValid(const char *s)
 {
-    int i, k = 0, len = strlen(s);
+    int i, k = 0;
+    size_t len = strlen(s);
     char *store = calloc(len, sizeof(char));
 
     for (i = 0; s[i] != '\0'; i++)
@@ -29,4 +57,24 @@ bool isValid(char *s)
 out:
     free(store);
     return s[i] == '\0' && k == 0;
+}
+
+/**
+ * @brief Main function
+ * @return 0
+ */
+int main()
+{
+    assert(isValid("()") == true);
+    printf("Test 1 passed\n");
+    assert(isValid("()[]{}") == true);
+    printf("Test 2 passed\n");
+    assert(isValid("(]") == false);
+    printf("Test 3 passed\n");
+    assert(isValid("([)]") == false);
+    printf("Test 4 passed\n");
+    assert(isValid("{[]}") == true);
+    printf("Test 5 passed\n");
+
+    return 0;
 }
