f9b8af9a0a1adf45bc6603913f603e4db873b6f4
Kasala Fahadh <67508953+Fahadh4444@users.noreply.github.com>
2020-12-10 01:18:58 +0530

    Update factorial.c

--- misc/factorial.c
+++ misc/factorial.c
@@ -1,31 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
+
 int main()
 {
-    int a[200], n, counter, temp, i;
-    a[0] = 1;
-    counter = 0;
-    printf("Enter a whole number to Find its Factorial: ");
-    scanf("%d", &n);
-    if (n < 0)
-        printf("Cannot Calculate factorials for negative numbers.");
-    else
-    {
-        for (; n >= 2; n--)
-        {
-            temp = 0;
-            for (i = 0; i <= counter; i++)
-            {
-                temp = (a[i] * n) + temp;
-                a[i] = temp % 10;
-                temp = temp / 10;
-            }
-            while (temp > 0)
-            {
-                a[++counter] = temp % 10;
-                temp = temp / 10;
-            }
-        }
-        for (i = counter; i >= 0; i--) printf("%d", a[i]);
-    }
-    return 0;
+	long long int n, factorial = 1;
+	printf("Enter a whole number to Find its Factorial:\n");
+	scanf("%lld",&n);
+	
+	if(n<0)
+	{
+		printf("Cannot Calculate factorials for negative numbers.");
+	}
+	else
+	{
+		for(int i=n; i>0 ;i--)
+		{
+			factorial = factorial * i;
+		}
+	}
+	
+	
+	printf("%lld",factorial);
 }
