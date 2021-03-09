0c4f1a5d9cfaf07cca72f50b90319e0df40026db
ali naderi <70060241+khod-naderi@users.noreply.github.com>
2021-03-09 09:40:30 +0330

    edit camment  games/game_of_life.c
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- games/game_of_life.c
+++ games/game_of_life.c
@@ -1,5 +1,11 @@
 /**
-* Conway's Game of Life
+ * @brief [Game of life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life)
+ * implementation
+ *
+ * @details The Game of Life, also known simply as Life, is a cellular automaton devised by the British
+ * mathematician John Horton Conway in 1970. It is a zero-player game, meaning that its evolution is determined
+ * by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial
+ * configuration and observing how it evolves. It is Turing complete and can simulate a universal constructor or any other Turing machine.
  * @file
 * @author [ali naderi](github.com/khod-naderi)
 * @author [Conway](https://en.wikipedia.org/wiki/John_Horton_Conway)
