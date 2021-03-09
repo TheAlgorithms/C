0b5b5875ec1f01d4b58a4498581a286b7413e035
ali naderi <70060241+khod-naderi@users.noreply.github.com>
2021-03-09 09:41:19 +0330

    Update author games/game_of_life.c
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- games/game_of_life.c
+++ games/game_of_life.c
@@ -7,7 +7,7 @@
  * by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial
  * configuration and observing how it evolves. It is Turing complete and can simulate a universal constructor or any other Turing machine.
  * @file
-* @author [ali naderi](github.com/khod-naderi)
+ * @author [Ali Naderi](https://github.com/khod-naderi)
 * @brief [game of life game](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life)
 * implementation in C
 * @details  game of life, zero player
