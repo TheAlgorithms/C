546c5bd80a5a3ee219f4278b672d7d227462b2db
David Leal <halfpacho@gmail.com>
2021-03-10 11:53:12 -0600

    Apply suggestions from code review

--- games/game_of_life.c
+++ games/game_of_life.c
@@ -6,7 +6,6 @@
  * mathematician John Horton Conway in 1970. It is a zero-player game, meaning that its evolution is determined
  * by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial
  * configuration and observing how it evolves. It is Turing complete and can simulate a universal constructor or any other Turing machine.
- * @file
  * @author [Ali Naderi](https://github.com/khod-naderi)
 */
 
