2ad0af6d6f36513ec5782122d066680f48657a06
Timothy Maloney <tmaloney@pdx.edu>
2021-03-30 11:40:36 -0700

    Docs: Changed function docs & made test function static

--- greedy_approach/prim.c
+++ greedy_approach/prim.c
@@ -29,17 +29,16 @@
  * (https://visualgo.net/en/mst)
  */
 
-#include <stdio.h>       /// for IO operations
-#include <string.h>     /// for string comparison
-#include <assert.h>    /// for assert()
-#include <inttypes.h> /// for uint16_t
+#include <stdio.h>        /// for IO operations
+#include <string.h>      /// for string comparison
+#include <assert.h>     /// for assert()
+#include <inttypes.h>  /// for uint16_t
 
 #define MAX 20
 #define INF 999
 
 /**
- * @brief Function minimum
- * Finds index of minimum element in edge list for an arbitrary vertex
+ * @brief Finds index of minimum element in edge list for an arbitrary vertex
  * @param arr graph row
  * @param N number of elements in arr
  * @returns index of minimum element in arr
@@ -61,8 +60,7 @@ uint16_t minimum(uint16_t arr[], uint16_t N)
 }
 
 /**
- * @brief Function prim
- * Used to find MST of user-generated adj matrix G
+ * @brief Used to find MST of user-generated adj matrix G
  * @returns void
  */
 void prim(uint16_t G[][MAX], uint16_t MST[][MAX], uint16_t V)
@@ -110,11 +108,10 @@ void prim(uint16_t G[][MAX], uint16_t MST[][MAX], uint16_t V)
 }
 
 /**
- * @brief Function test
- * uses test matrix and solution of test to verify correctness of prim()
- * @returns 0:succfess || -1:fail
+ * @brief Self-test implementations
+ * @returns void
  */
-void test(uint16_t G[][MAX], uint16_t MST[][MAX], uint16_t V)
+static void test(uint16_t G[][MAX], uint16_t MST[][MAX], uint16_t V)
 {
  
   uint16_t test[4][4] = {{0,1,2,3},{1,0,4,6},{2,4,0,5},{3,6,5,0}};
