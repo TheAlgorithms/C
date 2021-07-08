7185941c68857d0261244367c3a141bb34cbef30
lazy-dude <atorrses@gmail.com>
2021-07-08 15:52:52 +0430

    Auto indent

--- conversions/binary_to_decimal.c
+++ conversions/binary_to_decimal.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <stdlib.h>
 #include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 bool is_binary(intmax_t num);
 int num_len(intmax_t num);
@@ -10,20 +10,19 @@ int num_len(intmax_t num);
 int main(void)
 {
     intmax_t remainder, number = 0, decimal_number = 0, temp = 1;
-    
-    int length = num_len(INTMAX_MAX)-1;
-    printf("\n Enter any binary number , max %d digits: ",length);
+
+    int length = num_len(INTMAX_MAX) - 1;
+    printf("\n Enter any binary number , max %d digits: ", length);
     scanf("%jd", &number);
-    assert(num_len(number)<=length);
+    assert(num_len(number) <= length);
     assert(is_binary(number));
-    
+
     // Iterate over the number until the end.
-    while (number > 0)
-    {
+    while (number > 0) {
         remainder = number % 10;
         number = number / 10;
         decimal_number += remainder * temp;
-        temp = temp * 2;  // used as power of 2
+        temp = temp * 2; // used as power of 2
     }
 
     printf("%jd\n", decimal_number);
@@ -32,28 +31,24 @@ int main(void)
 
 bool is_binary(intmax_t num)
 {
-	int remainder=0;
-	
-	while(num>0)
-	{
-		remainder=num%10;
-		if(remainder==0 || remainder==1)
-		{
-			num /= 10;
-			continue;
-		}
-		else
-			return false;
-	}
-	return true;
+    int remainder = 0;
+
+    while (num > 0) {
+        remainder = num % 10;
+        if (remainder == 0 || remainder == 1) {
+            num /= 10;
+            continue;
+        } else
+            return false;
+    }
+    return true;
 }
 
 int num_len(intmax_t num)
 {
-	int i;
-	for(i=0; num>0; i++)
-	{
-		num /= 10;
-	}
-	return i;
+    int i;
+    for (i = 0; num > 0; i++) {
+        num /= 10;
+    }
+    return i;
 }
