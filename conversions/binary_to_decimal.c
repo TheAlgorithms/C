af4b346488105cf2f76460a32fd0d11a5f2eec87
lazy-dude <76253503+lazy-dude@users.noreply.github.com>
2021-07-26 11:33:14 +0430

    Update conversions/binary_to_decimal.c
    Added number
    
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- conversions/binary_to_decimal.c
+++ conversions/binary_to_decimal.c
@@ -50,7 +50,7 @@ uint16_t num_len(uintmax_t num)
 
 /**
  * @brief The `binary_decimal` function does the actual job of conversion
- * @param the number binary to be converted
+ * @param number the number binary to be converted
  * @returns decimal_number decimal representation of a binary number
  */
 uintmax_t binary_decimal(uintmax_t number)
