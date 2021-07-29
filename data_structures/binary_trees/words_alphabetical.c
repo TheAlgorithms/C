bbbfcdbcd0b2130c7654d5d0f7a95706b8ea317d
Randy Kwalar <rkwalar@gmail.com>
2021-07-29 16:33:21 +0100

    Update data_structures/binary_trees/words_alphabetical.c
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- data_structures/binary_trees/words_alphabetical.c
+++ data_structures/binary_trees/words_alphabetical.c
@@ -241,8 +241,8 @@ struct Node *readWordsInFileToTree(FILE *file, struct Node *root)
  */
 static void test()
 {
-    struct Node *root = NULL;  // pointer to root node
-    FILE *file = NULL;         // pointer to file
+    struct Node *root = NULL;  ///< pointer to root node
+    FILE *file = NULL;         ///< pointer to file
 
     file = fopen("file.txt", "w");  // creates test file in write mode
 
