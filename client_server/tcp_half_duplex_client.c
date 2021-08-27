58b36a95ab413c2ec937eb0586077a0facc79441
NVombat <nvombatkere@gmail.com>
2021-08-28 02:26:24 +0530

    Made Requested Changes - Added Documentation & Comments

--- client_server/tcp_half_duplex_client.c
+++ client_server/tcp_half_duplex_client.c
@@ -1,7 +1,8 @@
 /**
  * @file
  * @author [NVombat](https://github.com/NVombat)
- * @brief Client-side implementation of [TCP Half Duplex Communication](http://www.tcpipguide.com/free/t_SimplexFullDuplexandHalfDuplexOperation.htm)
+ * @brief Client-side implementation of [TCP Half Duplex
+ * Communication](http://www.tcpipguide.com/free/t_SimplexFullDuplexandHalfDuplexOperation.htm)
  * @see tcp_half_duplex_client.c
  *
  * @details
@@ -15,32 +16,80 @@
  */
 
 // Imports
-#include <netdb.h>  // For structures returned by the network database library - formatted internet addresses and port numbers
-#include <netinet/in.h>  // Needed for in_addr and sockaddr_in structs
-#include <stdio.h>  // Variable types, several macros, and various functions for performing input and output
-#include <stdlib.h>  // Variable types, several macros, and various functions for performing general functions
-#include <string.h>  // Various functions for manipulating arrays of characters
-#include <sys/socket.h>  // Contains macro definitions related to the creation of sockets
-#include <sys/types.h>  // Contains definitions to allow for the porting of BSD programs
-#include <unistd.h>  // Defines miscellaneous symbolic constants and types, and declares miscellaneous functions
+#include <netdb.h>  /// For structures returned by the network database library - formatted internet addresses and port numbers
+#include <netinet/in.h>  /// For in_addr and sockaddr_in structures
+#include <stdio.h>  /// Variable types, several macros, and various functions for performing input and output
+#include <stdlib.h>  /// Variable types, several macros, and various functions for performing general functions
+#include <string.h>  /// Various functions for manipulating arrays of characters
+#include <sys/socket.h>  /// For macro definitions related to the creation of sockets
+#include <sys/types.h>  /// For definitions to allow for the porting of BSD programs
+#include <unistd.h>  /// For miscellaneous symbolic constants and types, and miscellaneous functions
 
-#define PORT 8100  // Define port over which communication will take place
+#define PORT 8100  /// Define port over which communication will take place
 
+/*
+Function is used to print an error message to stderr.
+It prints str and an implementation-defined error
+message corresponding to the global variable errno.
+*/
+void error()
+{
+    perror("Socket Creation Failed");
+    exit(EXIT_FAILURE);
+}
+
+/* MAIN DRIVER CODE */
 int main()
 {
-    int sockfd;
-    struct sockaddr_in server_addr;
-    char serverResponse[10000], clientResponse[10000];
+    /* Variable Declarations */
+    int sockfd;  // Socket descriptors - Like file handles but for sockets
+    struct sockaddr_in
+        server_addr;  // Basic structures for all syscalls and functions that
+                      // deal with internet addresses. Structures for handling
+                      // internet addresses
+    char serverResponse[10000],
+        clientResponse[10000];  // Character arrays to read and store string
+                                // data for communication
 
-    // Socket descriptor used to create socket handle for TCP connection with
-    // specific family, type and protocol
+    /*
+    The TCP socket is created using the socket function.
+
+    AF_INET (Family) - it is an address family that is used to designate the
+    type of addresses that your socket can communicate with
+
+    SOCK_STREAM (Type) - Indicates TCP Connection - A stream socket provides for
+    the bidirectional, reliable, sequenced, and unduplicated flow of data
+    without record boundaries. Aside from the bidirectionality of data flow, a
+    pair of connected stream sockets provides an interface nearly identical to
+    pipes.
+
+    0 (Protocol) - Specifies a particular protocol to be used with the socket.
+    Specifying a protocol of 0 causes socket() to use an unspecified default
+    protocol appropriate for the requested socket type.
+    */
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        perror("Socket Creation Failed");
-        exit(EXIT_FAILURE);
-    }
+        error();
 
-    // Server information
+    /*
+    Server Address Information
+
+    The bzero() function erases the data in the n bytes of the memory
+    starting at the location pointed to, by writing zeros (bytes
+    containing '\0') to that area.
+
+    We bind the server_addr to the internet address and port number thus giving
+    our socket an identity with an address and port where it can listen for
+    connections
+
+    htons - The htons() function translates a short integer from host byte order
+    to network byte order
+
+    htonl - The htonl() function translates a long integer from host byte order
+    to network byte orde
+
+    These functions are necessary so that the binding of address and port takes
+    place with data in the correct format
+    */
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
@@ -48,21 +97,42 @@ int main()
 
     printf("Client is running...\n");
 
-    // Connecting to server
+    /*
+    Connects the client to the server address using the socket descriptor
+    This enables the two to communicate and exchange data
+    */
     connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
     printf("Client is connected...\n");
 
-    // Sending & receiving data one at a time - HALF DUPLEX
+    /*
+    Communication between client and server
+
+    The bzero() function erases the data in the n bytes of the memory
+    starting at the location pointed to, by writing zeros (bytes
+    containing '\0') to that area. The variables are emptied and then
+    ready for use
+
+    First the CLIENT receives the servers message and displays it (recv())
+
+    The CLIENT is then prompted to type in a message and send it to the server.
+    (send())
+
+    The server and client can communicate till one of them exits the connection
+
+    Since the exchange of information between the server and client take place
+    one at a time this represents HALF DUPLEX COMMUNICATION
+    */
     while (1)
     {
-        // Empty the contents of the variables in memory
         bzero(&serverResponse, sizeof(serverResponse));
         bzero(&clientResponse, sizeof(clientResponse));
 
-        // Send and receive messages
+        // Receive Message
         recv(sockfd, serverResponse, sizeof(serverResponse), 0);
         printf("\nServer message: %s \n", serverResponse);
+
+        // Send Message
         printf("\nEnter message here: ");
         fgets(clientResponse, 10000, stdin);
         send(sockfd, clientResponse, strlen(clientResponse) + 1, 0);
--- client_server/tcp_half_duplex_server.c
+++ client_server/tcp_half_duplex_server.c
@@ -1,7 +1,8 @@
 /**
  * @file
  * @author [NVombat](https://github.com/NVombat)
- * @brief Server-side implementation of [TCP Half Duplex Communication](http://www.tcpipguide.com/free/t_SimplexFullDuplexandHalfDuplexOperation.htm)
+ * @brief Server-side implementation of [TCP Half Duplex
+ * Communication](http://www.tcpipguide.com/free/t_SimplexFullDuplexandHalfDuplexOperation.htm)
  * @see tcp_half_duplex_server.c
  *
  * @details
@@ -15,67 +16,146 @@
  */
 
 // Imports
-#include <netdb.h>  // For structures returned by the network database library - formatted internet addresses and port numbers
-#include <netinet/in.h>  // Needed for in_addr and sockaddr_in structs
-#include <stdio.h>  // Variable types, several macros, and various functions for performing input and output
-#include <stdlib.h>  // Variable types, several macros, and various functions for performing general functions
-#include <string.h>  // Various functions for manipulating arrays of characters
-#include <sys/socket.h>  // Contains macro definitions related to the creation of sockets
-#include <sys/types.h>  // Contains definitions to allow for the porting of BSD programs
-#include <unistd.h>  // Defines miscellaneous symbolic constants and types, and declares miscellaneous functions
+#include <netdb.h>  /// For structures returned by the network database library - formatted internet addresses and port numbers
+#include <netinet/in.h>  /// For in_addr and sockaddr_in structures
+#include <stdio.h>  /// Variable types, several macros, and various functions for performing input and output
+#include <stdlib.h>  /// Variable types, several macros, and various functions for performing general functions
+#include <string.h>  /// Various functions for manipulating arrays of characters
+#include <sys/socket.h>  /// For macro definitions related to the creation of sockets
+#include <sys/types.h>  /// For definitions to allow for the porting of BSD programs
+#include <unistd.h>  /// For miscellaneous symbolic constants and types, and miscellaneous functions
 
-#define PORT 8100  // Define port over which communication will take place
+#define PORT 8100  /// Define port over which communication will take place
 
+/*
+Function is used to print an error message to stderr.
+It prints str and an implementation-defined error
+message corresponding to the global variable errno.
+*/
+void error()
+{
+    perror("Socket Creation Failed");
+    exit(EXIT_FAILURE);
+}
+
+/* MAIN DRIVER CODE */
 int main()
 {
-    int sockfd, conn;
-    char server_msg[10000], client_msg[10000];
-    struct sockaddr_in server_addr, client_addr;
+    /* Variable Declarations */
+    int sockfd, conn;  // Socket descriptors - Like file handles but for sockets
+    char server_msg[10000],
+        client_msg[10000];  // Character arrays to read and store string data
+                            // for communication
+    struct sockaddr_in server_addr,
+        client_addr;  // Basic structures for all syscalls and functions that
+                      // deal with internet addresses. Structures for handling
+                      // internet addresses
 
-    // Creating the TCP Socket with specific family, type and protocol
+    /*
+    The TCP socket is created using the socket function.
+
+    AF_INET (Family) - it is an address family that is used to designate the
+    type of addresses that your socket can communicate with
+
+    SOCK_STREAM (Type) - Indicates TCP Connection - A stream socket provides for
+    the bidirectional, reliable, sequenced, and unduplicated flow of data
+    without record boundaries. Aside from the bidirectionality of data flow, a
+    pair of connected stream sockets provides an interface nearly identical to
+    pipes.
+
+    0 (Protocol) - Specifies a particular protocol to be used with the socket.
+    Specifying a protocol of 0 causes socket() to use an unspecified default
+    protocol appropriate for the requested socket type.
+    */
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        perror("Socket Creation Failed");
-        exit(EXIT_FAILURE);
-    }
+        error();  // Error if the socket descriptor has a value lower than 0 -
+                  // socket wasnt created
 
-    // Server Address Information
+    /*
+    Server Address Information
+
+    The bzero() function erases the data in the n bytes of the memory
+    starting at the location pointed to, by writing zeros (bytes
+    containing '\0') to that area.
+
+    We bind the server_addr to the internet address and port number thus giving
+    our socket an identity with an address and port where it can listen for
+    connections
+
+    htons - The htons() function translates a short integer from host byte order
+    to network byte order
+
+    htonl - The htonl() function translates a long integer from host byte order
+    to network byte orde
+
+    These functions are necessary so that the binding of address and port takes
+    place with data in the correct format
+    */
     bzero(&server_addr, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
+    server_addr.sin_family = AF_INET;    // Domain/Family to be used
+    server_addr.sin_port = htons(PORT);  // Port to be used
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     printf("Server is running...\n");
 
-    // Bind server to socket
+    /*
+    This binds the socket descriptor to the server thus enabling the server to
+    listen for connections and communicate with other clients
+    */
     if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-    {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
-    }
+        error();  // If binding is unsuccessful
 
-    // Listening for clients
+    /*
+    This is to listen for clients or connections made to the server
+
+    The limit is currently at 5 but can be increased to listen for
+    more connections
+
+    It listens to connections through the socket descriptor
+    */
     listen(sockfd, 5);
 
     printf("Server is listening...\n");
 
-    // Accepting a connection from the client
+    /*
+    When a connection is found, a socket is created and connection is
+    accepted and established through the socket descriptor
+    */
     conn = accept(sockfd, (struct sockaddr *)NULL, NULL);
 
     printf("Server is connected...\n");
 
-    // Exchanging data one at a time - HALF DUPLEX
+    /*
+    Communication between client and server
+
+    The bzero() function erases the data in the n bytes of the memory
+    starting at the location pointed to, by writing zeros (bytes
+    containing '\0') to that area. The variables are emptied and then
+    ready for use
+
+    First the SERVER is prompted to type a message which is read from
+    stdin and then sent over the connection that was established - the socket
+    - to be received by the client (send())
+
+    The SERVER then waits for the client to reply. It then receives the reply in
+    the string variable and displays it (recv())
+
+    The server and client can communicate till one of them exits the connection
+
+    Since the exchange of information between the server and client take place
+    one at a time this represents HALF DUPLEX COMMUNICATION
+    */
     while (1)
     {
-        // Empty the contents of the variables in memory
         bzero(&server_msg, sizeof(server_msg));
         bzero(&client_msg, sizeof(client_msg));
 
-        // Send and receive messages
+        // Send message
         printf("\nEnter message here: ");
-        // Read input from stdin
         fgets(server_msg, 10000, stdin);
         send(conn, server_msg, strlen(server_msg) + 1, 0);
+
+        // Receive Message
         recv(conn, client_msg, sizeof(client_msg), 0);
         printf("\nClient Message: %s\n", client_msg);
     }
