9c90d04b6badd5f15d957408d247ddbd962bfb00
NVombat <nvombatkere@gmail.com>
2021-08-27 23:38:58 +0530

    Making all the requested changes

--- client_server/tcp_half_duplex_client.c
+++ client_server/tcp_half_duplex_client.c
@@ -2,38 +2,47 @@
  * @file
  * @author [NVombat](https://github.com/NVombat)
  * @brief Client side implementation of TCP Half Duplex Communication
- * @see client_server/tcp_half_duplex_client.c
+ * @see tcp_half_duplex_client.c
+ * @link
+ * http://www.tcpipguide.com/free/t_SimplexFullDuplexandHalfDuplexOperation.htm
+ *
+ * The algorithm is based on the simple TCP client and server model. However
+ * Instead of the server only sending and the client only receiving data,
+ * The server and client can both send data but only one at a time. This is
+ * implemented by using a particular ordering of the send() and recv()
+ * functions. When one of the client Or server is sending the other can only
+ * receive and vice-versa. In this way the Half Duplex Form of communication can
+ * be represented using the TCP server client model & socket programming
  */
 
 // Imports
-#include <netdb.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <netdb.h>  // For structures returned by the network database library - formatted internet addresses and port numbers
+#include <netinet/in.h>  // Needed for in_addr and sockaddr_in structs
+#include <stdio.h>  // Variable types, several macros, and various functions for performing input and output
+#include <stdlib.h>  // Variable types, several macros, and various functions for performing general functions
+#include <string.h>  // Various functions for manipulating arrays of characters
+#include <sys/socket.h>  // Contains macro definitions related to the creation of sockets
+#include <sys/types.h>  // Contains definitions to allow for the porting of BSD programs
+#include <unistd.h>  //Defines miscellaneous symbolic constants and types, and declares miscellaneous functions
 
-#define PORT 8100
+#define PORT 8100  // Define port over which communication will take place
 
 int main()
 {
-    // Socket descriptor used to create socket handle
     int sockfd;
+    struct sockaddr_in server_addr;
+    char serverResponse[10000], clientResponse[10000];
+
+    // Socket descriptor used to create socket handle for TCP connection with
+    // specific family, type and protocol
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror("Socket Creation Failed");
         exit(EXIT_FAILURE);
     }
 
-    char serverResponse[1024];
-    char clientResponse[1024];
-
-    struct sockaddr_in server_addr;
+    // Server information
     bzero(&server_addr, sizeof(server_addr));
-
-    // Filling Server information
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -48,8 +57,11 @@ int main()
     // Sending & receiving data one at a time - HALF DUPLEX
     while (1)
     {
+        // Empty the contents of the variables in memory
         bzero(&serverResponse, sizeof(serverResponse));
         bzero(&clientResponse, sizeof(clientResponse));
+
+        // Send and receive messages
         recv(sockfd, serverResponse, sizeof(serverResponse), 0);
         printf("\nServer message: %s \n", serverResponse);
         printf("\nEnter message here: ");
@@ -57,6 +69,7 @@ int main()
         send(sockfd, clientResponse, strlen(clientResponse) + 1, 0);
     }
 
+    // Close Socket
     close(sockfd);
     printf("Client is offline...\n");
     return 0;
--- client_server/tcp_half_duplex_server.c
+++ client_server/tcp_half_duplex_server.c
@@ -1,36 +1,46 @@
 /**
  * @file
  * @author [NVombat](https://github.com/NVombat)
- * @brief Client side implementation of TCP Half Duplex Communication
- * @see client_server/tcp_half_duplex_client.c
+ * @brief Server side implementation of TCP Half Duplex Communication
+ * @see tcp_half_duplex_server.c
+ * @link
+ * http://www.tcpipguide.com/free/t_SimplexFullDuplexandHalfDuplexOperation.htm
+ *
+ * The algorithm is based on the simple TCP client and server model. However
+ * Instead of the server only sending and the client only receiving data,
+ * The server and client can both send data but only one at a time. This is
+ * implemented by using a particular ordering of the send() and recv()
+ * functions. When one of the client Or server is sending the other can only
+ * receive and vice-versa. In this way the Half Duplex Form of communication can
+ * be represented using the TCP server client model & socket programming
  */
 
 // Imports
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <netdb.h>  // For structures returned by the network database library - formatted internet addresses and port numbers
+#include <netinet/in.h>  // Needed for in_addr and sockaddr_in structs
+#include <stdio.h>  // Variable types, several macros, and various functions for performing input and output
+#include <stdlib.h>  // Variable types, several macros, and various functions for performing general functions
+#include <string.h>  // Various functions for manipulating arrays of characters
+#include <sys/socket.h>  // Contains macro definitions related to the creation of sockets
+#include <sys/types.h>  // Contains definitions to allow for the porting of BSD programs
+#include <unistd.h>  // Defines miscellaneous symbolic constants and types, and declares miscellaneous functions
 
-#define PORT 8100
+#define PORT 8100  // Define port over which communication will take place
 
 int main()
 {
-    // Creating the TCP Socket
     int sockfd, conn;
+    char server_msg[10000], client_msg[10000];
+    struct sockaddr_in server_addr, client_addr;
+
+    // Creating the TCP Socket with specific family, type and protocol
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror("Socket Creation Failed");
         exit(EXIT_FAILURE);
     }
 
-    char server_msg[1024];
-    char client_msg[1024];
-
     // Server Address Information
-    struct sockaddr_in server_addr, client_addr;
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
@@ -58,15 +68,20 @@ int main()
     // Exchanging data one at a time - HALF DUPLEX
     while (1)
     {
+        // Empty the contents of the variables in memory
         bzero(&server_msg, sizeof(server_msg));
         bzero(&client_msg, sizeof(client_msg));
+
+        // Send and receive messages
         printf("\nEnter message here: ");
+        // Read input from stdin
         fgets(server_msg, 10000, stdin);
         send(conn, server_msg, strlen(server_msg) + 1, 0);
         recv(conn, client_msg, sizeof(client_msg), 0);
         printf("\nClient Message: %s\n", client_msg);
     }
 
+    // Close socket
     close(sockfd);
     printf("Server is offline...\n");
     return 0;
