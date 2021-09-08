e85ec3c960da2ea21cf7810e53ee12e95d067057
Edwin B. Ajong <edwinajong@gmail.com>
2021-09-08 21:42:49 +0100

    Update odd_even_sort.c

--- sorting/odd_even_sort.c
+++ sorting/odd_even_sort.c
@@ -15,6 +15,7 @@
 #include <stdio.h>      /// for IO operations
 #include <stdlib.h>     /// for dynammic memory allocation
 #include <time.h>       /// for random number generation
+#include <inttypes.h>   /// for int32_t types
 
 /**
  * @brief Swap numbers by reference(using pointers)
@@ -23,15 +24,28 @@
  * @returns void
  */
 
-void swap(int *first, int *second)
+void swap(int32_t *first, int32_t *second)
 {
-    int temp = *first;
+    int32_t temp = *first;
     *first = *second;
     *second = temp;
 }
 
 /**
  * @brief oddEvenSort sorts the array using the algorithm described above.
+ * @details 
+ * A boolean varaible(isSorted) is declared and initialised to "false".
+ * In the while loop, the variable(isSorted) is then set to "true".
+ * During even phase the for loop loops through the array, touching just the even indexes.
+ * i.e arr[0], arr[2], arr[4] and so on.
+ * While during the odd phase, the for loop loops through the array, touching just the odd indexes.
+ * i.e arr[1], arr[3], arr[5] and so on.
+ * During these phases, if the if statement check if the interger at the current position in the array
+ * is greater than the interger at the next array index (i.e arr[index + 2], to make sure the index is odd
+ * during the odd phase and even during the even phase).
+ * If the condition is true, the function "swap" is called and address of the intergers in question are passed as
+ * parameters. After the swap is completed, "isSorted" is set to "false".
+ * The while loop will keep running till the array is propertly sorted.
  * @param arr array to be sorted
  * @param size size of array
  * @returns void
@@ -43,9 +57,9 @@ void oddEvenSort(int *arr, int size)
     while(!isSorted)
     {
         isSorted = true;
-        int i;
+        int32_t i;
 
-        //Even phase
+        // Even phase
         for(i = 0; i <= size - 2; i += 2)
         {
             if(arr[i] > arr[i + 1])
@@ -55,7 +69,7 @@ void oddEvenSort(int *arr, int size)
             }
         }
 
-        //Odd phase
+        // Odd phase
         for(i = 1; i <= size - 2; i += 2)
         {
             if(arr[i] > arr[i + 1])
@@ -73,20 +87,20 @@ void oddEvenSort(int *arr, int size)
  */
 static void test()
 {
-    int arr1[] = {-9, 2, 3, 1};
-    int arr1Soln[] = {-9, 1, 2, 3};
-    int arr2[] = {9, 7, 5, 3, 8, 2, 1, 4, 0, 6};
-    int arr2Soln[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int32_t arr1[] = {-9, 2, 3, 1};
+    int32_t arr1Soln[] = {-9, 1, 2, 3};
+    int32_t arr2[] = {9, 7, 5, 3, 8, 2, 1, 4, 0, 6};
+    int32_t arr2Soln[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     oddEvenSort(arr1, 4);
     oddEvenSort(arr2, 10);
 
-     for (int i = 0; i < 4; i++)
+     for (int32_t i = 0; i < 4; i++)
      {
          assert(arr1[i] == arr1Soln[i]);    
      }
 
-    for (int i = 0; i < 10; i++)
+    for (int32_t i = 0; i < 10; i++)
     {
         assert(arr2[i] == arr2Soln[i]);    
     }
