d86f6d6a890c6c9c50306d1de6a1a2c2969d2f49
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2021-10-01 01:20:35 +0530

    Create 14.Longest Common Prefix.c

---
+++ leetcode/leetcode/README.md/14.Longest Common Prefix.c
@@ -0,0 +1,18 @@
+
+char * longestCommonPrefix(char ** strs, int strsSize)
+{
+
+    if(strsSize==0) return "";
+    for(int i=0;i<strlen(strs[0]);i++)
+    {
+        for(int j=1;j<strsSize;j++)
+        {
+            if(strs[0][i]!=strs[j][i])
+            {
+               strs[0][i]='\0';
+               return strs[0];
+            }
+       }
+    }
+ return strs[0];
+}
