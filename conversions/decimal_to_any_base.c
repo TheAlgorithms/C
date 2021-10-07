e6f654e89ea93364f3cada1919a243d5b9f877d7
jucollet972 <cloudtoilet@gmail.com>
2021-10-07 16:32:39 -0400

    Enhencement of decimal_to_any_base.c

--- conversions/decimal_to_any_base.c
+++ conversions/decimal_to_any_base.c
@@ -1,21 +1,37 @@
-/*
- *	 C program to convert positive decimal integer to any positive base with base's alphabet given in input (recursive way)
- */
+/**
+ * 	@file
+ * 	@author [jucollet972](https://github.com/jucollet972)
+ *	@brief decimal_to_anybase is a tiny C program wich convert positive decimal integer to any positive base with base's alphabet given in input (recursive way)
+ **/
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
+#include <stdio.h>   ///for scanf
+#include <unistd.h>  ///for write
+#include <string.h>  ///for strchr and strlen
 
+/**
+ * @brief Checking for duplicates in alphabet
+ * @param base alphabet inputed by user
+ * @return int as success or not
+ **/
 int 	isbad_alphabet(char alphabet[85])
 {
+	/* Browse the alphabet */
 	for (int i = 0; i < 85; i++)
 	{
+
 		if (strchr(alphabet + i + 1, alphabet[i]))
 			return (1);
 	}
 	return (0);
 }
 
+/**
+ * @brief Convert positive decimal integer into anybase recursively
+ * @param nb to convert
+ * @param alphabet inputed by user used for base convertion
+ * @param base calculated from alphabet
+ * @return void
+ **/
 void	display_convertion(unsigned long long nb, char alphabet[86], unsigned short base)
 {
 	/* Recursive convertion */
@@ -25,6 +41,10 @@ void	display_convertion(unsigned long long nb, char alphabet[86], unsigned short
 	write(1, alphabet + nb%base, 1);
 }
 
+/**
+ * @brief Main function
+ * @return int as success/error indicator
+ **/
 int	main()
 {
 	unsigned long long	nb = 0;
