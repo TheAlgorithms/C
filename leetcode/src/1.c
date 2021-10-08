1d88036e24823cbdbb9427d7ca317e6e6d01fdc8
Edeyson <74788452+EdeysonGomes@users.noreply.github.com>
2021-10-08 10:47:46 -0300

    Update 1.c

--- leetcode/src/1.c
+++ leetcode/src/1.c
@@ -2,20 +2,17 @@
  * @file
  * @brief Program to return indices of the two numbers such that they add up to target.
  */
-int *twoSum(int *nums, int nums_size, int target, int *return_size)
-{
+int *twoSum(int *nums, int nums_size, int target, int *return_size) {
     int i, j;
     int *ret = calloc(2, sizeof(int));
-    for (i = 0; i < nums_size; i++)
-    {
-        int key = target - nums[i];
+    for (i = 0; i < nums_size; i++)    {
         for (j = i + 1; j < nums_size; j++)
-            if (nums[j] == key)
-            {
+            if (nums[i] + nums[j] == target) {
                 ret[0] = i;
                 ret[1] = j;
             }
     }
+    
     *return_size = 2;
     return ret;
 }
