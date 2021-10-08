90737809f20996e1ec6e2f5e48c35c43ff55cdab
Edeyson <74788452+EdeysonGomes@users.noreply.github.com>
2021-10-08 10:25:50 -0300

    Update 1.c

--- leetcode/src/1.c
+++ leetcode/src/1.c
@@ -1,17 +1,17 @@
-int *twoSum(int *nums, int numsSize, int target, int *returnSize)
+int *twoSum(int *nums, int nums_size, int target, int *return_size)
 {
     int i, j;
     int *ret = calloc(2, sizeof(int));
-    for (i = 0; i < numsSize; i++)
+    for (i = 0; i < nums_size; i++)
     {
         int key = target - nums[i];
-        for (j = i + 1; j < numsSize; j++)
+        for (j = i + 1; j < nums_size; j++)
             if (nums[j] == key)
             {
                 ret[0] = i;
                 ret[1] = j;
             }
     }
-    *returnSize = 2;
+    *return_size = 2;
     return ret;
 }
