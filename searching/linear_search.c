65dcbe65b412ee4431b40a0941a30e298cd5a6c2
Deepak <60574055+11deepak0209@users.noreply.github.com>
2021-10-09 16:13:09 +0530

    Update linear_search.c
    I did some documentation and changed

--- searching/linear_search.c
+++ searching/linear_search.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//function to search element
 int linearsearch(int *arr, int size, int val)
 {
     int i;
@@ -9,28 +10,28 @@ int linearsearch(int *arr, int size, int val)
         if (arr[i] == val)
             return 1;
     }
-    return 0;
+    
 }
 
-int main()
+void main()
 {
-    int n, i, v;
+    int size, i, v;
     printf("Enter the size of the array:\n");
-    scanf("%d", &n);  // Taking input for the size of Array
+    scanf("%d", &size);  //  the size of Array
 
-    int *a = (int *)malloc(n * sizeof(int));
-    printf("Enter the contents for an array of size %d:\n", n);
-    for (i = 0; i < n; i++)
+    int *a = (int *)malloc(size * sizeof(int));
+    printf("Enter the contents for an array of size %d:\n", size);
+    for (i = 0; i < size; i++)
         scanf("%d", &a[i]);  // accepts the values of array elements until the
                              // loop terminates//
 
     printf("Enter the value to be searched:\n");
     scanf("%d", &v);  // Taking input the value to be searched
-    if (linearsearch(a, n, v))
+    if (linearsearch(a, size, v))
         printf("Value %d is in the array.\n", v);
     else
         printf("Value %d is not in the array.\n", v);
 
     free(a);
-    return 0;
+    
 }
