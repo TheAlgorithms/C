e6b2b3bf5f1dd07fdf837201fca268fb5913b45c
Karan_Chhabra <92259336+Karan-TIET@users.noreply.github.com>
2021-10-11 01:35:07 +0530

    Armstrong Number in C

---
+++ numerical_methods/Armstrong Number in C.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int num, originalNum, remainder, result = 0;
+    printf("Enter a three-digit integer: ");
+    scanf("%d", &num);
+    originalNum = num;
+
+    while (originalNum != 0) {
+       // remainder contains the last digit
+        remainder = originalNum % 10;
+        
+       result += remainder * remainder * remainder;
+        
+       // removing last digit from the orignal number
+       originalNum /= 10;
+    }
+
+    if (result == num)
+        printf("%d This is an Armstrong number.", num);
+    else
+        printf("%d This is not an Armstrong number.", num);
+
+    return 0;
+}
+
