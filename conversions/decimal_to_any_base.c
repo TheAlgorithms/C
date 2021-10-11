d46718dd6b55e155252bceedab562425b7771ed7
jucollet972 <cloudtoilet@gmail.com>
2021-10-10 21:32:36 -0400

    builtin int types declacration changed into arch optimized int types

--- conversions/decimal_to_any_base.c
+++ conversions/decimal_to_any_base.c
@@ -6,8 +6,8 @@
  */
 
 #include <stdio.h>   /// for IO operations
-#include <unistd.h>  /// for write
 #include <string.h>  /// for strchr and strlen
+#include <stdint.h> /// for CPU arch's optimized int types
 
 /**
  * @brief Checking if alphabet is valid
@@ -39,13 +39,13 @@ int isbad_alphabet(char alphabet[85])
  * @param base calculated from alphabet
  * @return void
  */
-void display_convertion(unsigned long long nb, char alphabet[86], unsigned short base)
+void display_convertion(uint64_t nb, char alphabet[86], unsigned short base)
 {
 	/* Recursive convertion */
 	if (nb > base - 1)
 		display_convertion(nb/base, alphabet, base);
 	/* Displaying converted number in order */
-	write(1, alphabet + nb%base, 1);
+	printf("%c", alphabet[nb%base]);
 }
 
 /**
@@ -54,23 +54,23 @@ void display_convertion(unsigned long long nb, char alphabet[86], unsigned short
  */
 int main()
 {
-	unsigned long long	nb = 0;
+	uint64_t		nb = 0;
 	char			alphabet[86] = {0};
 
 	/* Decimal number input */
-	write(1, "Enter the positive number to convert (-1 < number < 18446744073709551615): ", 75);
-	scanf("%llu", &nb);
+	printf("Enter the positive number to convert (-1 < number < 18446744073709551615): "); 
+	scanf("%lu", &nb);
 	/* Base alphabet input */
-	write(1, "Enter the base alphabet (1 < alphabet length < 85): ", 52);
-	scanf("%85s", alphabet);
+	printf("Enter the base alphabet (1 < alphabet length < 85): ");
+	scanf("%85s", alphabet);  
 	/* Verify that every characters of alphabet is unique */
 	if (isbad_alphabet(alphabet))
 	{
-		write(2, "The base alphabet is not valid, Every character should be unique and the length of the alphabet should be greater than 2 and lower than 86 !\n", 141);
+		printf("The base alphabet is not valid, Every character should be unique and the length of the alphabet should be greater than 2 and lower than 86 !\n");
 		return (1);
 	}
 	/* Convertion */
 	display_convertion(nb, alphabet, strlen(alphabet));
-	write(1, "\n", 1);
+	printf("\n");
 	return (0);
 }
