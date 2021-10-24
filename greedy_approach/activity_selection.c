0063b2398ed3cb45efd055eb63774ad6c7aed2e5
Limbad Yash <56826569+limbad-YK@users.noreply.github.com>
2021-10-24 16:09:44 +0530

    Documentation Update

--- greedy_approach/activity_selection.c
+++ greedy_approach/activity_selection.c
@@ -11,14 +11,17 @@ void printMaxActivities(int s[], int f[], int n)
  
     printf ("Following activities are selected n");
  
-    // The first activity always gets selected
+    /* The first activity always gets selected */
     i = 0;
     printf("%d ", i);
  
     // Consider rest of the activities
     for (j = 1; j < n; j++)
     {
-      // If this activity has start time greater than or equal to the finish time of previously selected activity, then select it
+      /* 
+      * If this activity has start time greater than or equal to the finish time of
+      * previously selected activity, then select it 
+      */
       if (s[j] >= f[i])
       {
           printf ("%d ", j);
