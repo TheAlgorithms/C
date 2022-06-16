2706ec8b977e08385d89d013920afdcef94bc173
DanArmor <danilamorozov07@mail.ru>
2022-06-16 18:11:39 +0300

    game_of_life

--- games/game_of_life.c
+++ games/game_of_life.c
@@ -10,15 +10,29 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <poll.h>
+#endif
 
 // Functions Declarations
 static void initGrid(void);
-static void createGridFromFile(char *);
+static void randomGrid(void);
+static void readInitFromFile(char *);
+static void copyGridToPrev(void);
+static int calcRowIndex(int i);
+static int calcColIndex(int i);
+static int countNeighbors(int, int);
+static void updateGrid(void);
+static void showGrid(void);
 static void runGame(void);
 
 // Size of game grid
-static const int maxRows = 32;
-static const int maxCols = 32;
+#define MAX_ROWS 48
+#define MAX_COLS 48
 // Chars, that represents alive and dead cells
 static const char deadChar = '0';
 static const char aliveChar = '1';
@@ -26,23 +40,27 @@ static const char aliveChar = '1';
 /**
  * Current state of grid to draw
  */
-static char grid[maxRows][maxCols];
+static char grid[MAX_ROWS][MAX_COLS];
 /**
  * Previous state of grid
  */
-static char prevGrid[maxRows][maxCols];
+static char prevGrid[MAX_ROWS][MAX_COLS];
 
 int main(int argc, char **argv)
 {
     initGrid();
     if (argc > 2)
     {
-        fprintf(stderr, "Too many args");
+        fprintf(stderr, "Too many args\n");
         exit(1);
     }
     if (argc == 2)
     {
         readInitFromFile(argv[1]);
+    }
+    else
+    {
+        randomGrid();
     }
     runGame();
     return 0;
@@ -50,11 +68,26 @@ int main(int argc, char **argv)
 
 void initGrid(void)
 {
-    for (int i = 0; i < maxRows; i++)
+    for (int i = 0; i < MAX_ROWS; i++)
     {
-        for (int j = 0; j < maxCols; j++)
+        for (int j = 0; j < MAX_COLS; j++)
         {
             grid[i][j] = deadChar;
+        }
+    }
+}
+
+void randomGrid(void)
+{
+    srand(time(NULL) + clock());
+    for (int i = 0; i < MAX_ROWS; i++)
+    {
+        for (int j = 0; j < MAX_COLS; j++)
+        {
+            if (rand() % 5 == 1)
+            {
+                grid[i][j] = aliveChar;
+            }
         }
     }
 }
@@ -75,12 +108,12 @@ void readInitFromFile(char *fileName)
     int currentCol = 0;
     for (char c = fgetc(f); c != EOF; c = fgetc(f))
     {
-        if (currentCol >= maxCols)
+        if (currentCol >= MAX_COLS)
         {
             currentCol = 0;
             currentRow++;
         }
-        if (currentRow >= maxRows)
+        if (currentRow >= MAX_ROWS)
         {
             break;
         }
@@ -93,6 +126,142 @@ void readInitFromFile(char *fileName)
         else if (c == '\n')
         {
             currentRow++;
+            currentCol = 0;
         }
+    }
+}
+
+void copyGridToPrev(void)
+{
+    for (int i = 0; i < MAX_ROWS; i++)
+    {
+        for (int j = 0; j < MAX_COLS; j++)
+        {
+            prevGrid[i][j] = grid[i][j];
+        }
+    }
+}
+
+int calcRowIndex(int i)
+{
+    if (i < 0)
+    {
+        return MAX_ROWS - i;
+    }
+    else
+    {
+        return i % MAX_ROWS;
+    }
+}
+
+int calcColIndex(int i)
+{
+    if (i < 0)
+    {
+        return MAX_COLS - i;
+    }
+    else
+    {
+        return i % MAX_COLS;
+    }
+}
+
+int countNeighbors(int x, int y)
+{
+    int aliveNeighbors = 0;
+    for (int i = -1; i < 2; i++)
+    {
+        for (int j = -1; j < 2; j++)
+        {
+            if (i == 0 && j == 0)
+            {
+                continue;
+            }
+
+            if (prevGrid[calcRowIndex(i + y)][calcColIndex(j + x)] == aliveChar)
+            {
+                aliveNeighbors++;
+            }
+        }
+    }
+    return aliveNeighbors;
+}
+
+void updateGrid(void)
+{
+    copyGridToPrev();
+    for (int i = 0; i < MAX_ROWS; i++)
+    {
+        for (int j = 0; j < MAX_COLS; j++)
+        {
+            switch (countNeighbors(j, i))
+            {
+            case 2:
+                if (prevGrid[i][j] == aliveChar)
+                {
+                    grid[i][j] = aliveChar;
+                }
+                break;
+            case 3:
+                grid[i][j] = aliveChar;
+                break;
+            default:
+                grid[i][j] = deadChar;
+                break;
+            }
+        }
+    }
+}
+
+void showGrid(void)
+{
+    fputs("\033[2J", stdout);
+    fputs("\033[H", stdout);
+    putchar('\n');
+    fputs("    ", stdout);
+    for (int i = 0; i < MAX_COLS * 2 + 3; i++)
+    {
+        putchar('*');
+    }
+    putchar('\n');
+    for (int i = 0; i < MAX_ROWS; i++)
+    {
+        fputs("    * ", stdout);
+        for (int j = 0; j < MAX_COLS; j++)
+        {
+            if (grid[i][j] == aliveChar)
+            {
+                fputs("\033[38;5;46m", stdout);
+            }
+            else
+            {
+                fputs("\033[38;5;52m", stdout);
+            }
+            putchar('O');
+            putchar(' ');
+        }
+        fputs("\033[0m", stdout);
+        putchar('*');
+        putchar('\n');
+    }
+    fputs("    ", stdout);
+    for (int i = 0; i < MAX_COLS * 2 + 3; i++)
+    {
+        putchar('*');
+    }
+    fflush(stdout);
+}
+
+void runGame(void)
+{
+    while (1)
+    {
+        showGrid();
+        updateGrid();
+#ifdef _WIN32
+        Sleep(500);
+#else
+        poll(NULL, 0, 400);
+#endif
     }
 }
