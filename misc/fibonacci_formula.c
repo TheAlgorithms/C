a54067e479bc6e5732e9a8ca3d9b2f8c6548e1b8
GrandSir <69051988+GrandSir@users.noreply.github.com>
2022-09-10 23:41:40 +0300

    Create fibonacci_formula.c

---
+++ misc/fibonacci_formula.c
@@ -0,0 +1,32 @@
+/**
+ * \file
+ *
+ * \brief Implementation of [Fibonacci Sequence
+ * explicit formula solution](https://fabiandablander.com/r/Fibonacci.html)
+ *
+*/
+
+#include <math.h> /// for pow and sqrt
+#include <stdio.h>
+
+/*
+ * formula comes from quadratic formula and recurrence relation
+ * all the proof can be found in the related article in top of the file
+ */
+double fib(int n) {
+    return (1 / sqrt(5) * pow(((1 + sqrt(5)) / 2), n + 1)) -
+           (1 / sqrt(5) * pow(((1 - sqrt(5)) / 2), n + 1));
+}
+
+/* print first 10 elements of fibonacci sequence for testing */
+static void test () {
+    for(int i = 0; i <= 10; i++){
+        printf("%d. fibonacci sequence is: %.0f\n", i, fib(i));
+    }
+}
+
+// driver code
+int main() {
+    test();
+    return 0;
+}
