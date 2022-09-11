b1040c66c41224def07d003cb2ae5301cc7a20c2
GrandSir <69051988+GrandSir@users.noreply.github.com>
2022-09-11 08:23:26 +0300

    fix: proper documentation

--- misc/fibonacci_formula.c
+++ misc/fibonacci_formula.c
@@ -1,38 +1,58 @@
-/**
- * \file
- *
- * \brief Implementation of [Fibonacci Sequence
- * explicit formula solution](https://fabiandablander.com/r/Fibonacci.html)
- *
-*/
+    /**
+     * @file
+     * @brief [finding Fibonacci number of any number n using binet's closed form formula](https://en.wikipedia.org/wiki/Fibonacci_number#Binet's_formula)
+     * @details
+     * compute \f$f_{nth}\f$ Fibonacci number using the binet's formula:
+     * Fn = 1√5 * (1+√5 / 2)^n+1 − 1√5 * (1−√5 / 2)^n+1
+     * @author [GrandSir](https://github.com/GrandSir/)
+    */
 
-#include <math.h> /// for pow and sqrt
-#include <stdio.h>
+    #include <math.h> /// for pow and sqrt
+    #include <stdio.h> /// for printf
+    #include <assert.h> /// for assert
 
-/*
- * formula comes from quadratic formula and recurrence relation
- * all the proof can be found in the related article in top of the file
- */
-double fib(int n) {
-    return (1 / sqrt(5) * pow(((1 + sqrt(5)) / 2), n + 1)) -
-           (1 / sqrt(5) * pow(((1 - sqrt(5)) / 2), n + 1));
-}
+    /**
+     * @param n index of number in Fibonacci sequence
+     * @returns nth value of fibonacci sequence for all n >= 0
+     */
+    int fib(unsigned int n) {
+        float seq = (1 / sqrt(5) * pow(((1 + sqrt(5)) / 2), n + 1)) - (1 / sqrt(5) * pow(((1 - sqrt(5)) / 2), n + 1));
 
-/**
- * @brief Self-test implementations
- * @returns void
- */
-static void test () {
-    for(int i = 0; i <= 10; i++){
-        printf("%d. fibonacci sequence is: %.0f\n", i, fib(i));
+        // removing unnecessary fractional part by implicitly converting float to int
+        return seq;
     }
-}
 
-/**
- * @brief Main function
- * @returns 0 on exit
- */
-int main() {
-    test();  // run self-test implementations
-    return 0;
-}
+    /**
+     * @brief Self-test implementations
+     * @returns void
+     */
+    static void test () {
+        /* this ensures that the first 10 number of fibonacci sequence
+         * (1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89)
+         * matches with algorithm
+        */
+        assert(fib(0) == 1);
+        assert(fib(1) == 1);
+        assert(fib(2) == 2);
+        assert(fib(3) == 3);
+        assert(fib(4) == 5);
+        assert(fib(5) == 8);
+        assert(fib(6) == 13);
+        assert(fib(7) == 21);
+        assert(fib(8) == 34);
+        assert(fib(9) == 55);
+        assert(fib(10) == 89);
+    }
+
+    /**
+     * @brief Main function
+     * @returns 0 on exit
+     */
+    int main() {
+        test();  // run self-test implementations
+
+        for(int i = 0; i <= 10; i++){
+            printf("%d. fibonacci number is: %d\n", i, fib(i));
+        }
+        return 0;
+    }
