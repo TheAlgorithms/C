ed12f12188519fe20d2d4f78f52becd4d2c89ec7
Abhinav Aryan <77077109+07abhi9av@users.noreply.github.com>
2022-09-30 01:23:15 +0530

    Update tower_of_hanoi.c
    comments have been used to make code more understandable
    the code has been made more understandable
    the code has been presented in a readable and understandable format. 
    It includes proper indentation and right amount of whitespace and newlines after expressions.

--- misc/tower_of_hanoi.c
+++ misc/tower_of_hanoi.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 // Function for Tower of Hanoi algorithm
-void TOH(int n, int A, int B, int C){
-    if(n>0){
-        // n-1 disks in the tower
-        TOH(n-1,A,C,B);
-        printf("Moving disc from rod %d to %d\n",A,C);
-        TOH(n-1,B,A,C);
+
+void TOH(int n, int A, int B, int C)   // Taking n discs named A, B and C as the parameters of the function
+{
+    if(n > 0)    // only when n is greater than 0
+    {
+        TOH(n-1, A, C, B);      // n-1 disks in the tower
+        printf("Moving disc from rod %d to %d\n",A,C);    // printing statements for the discs when it moves from one position to other
+        TOH(n-1, B, A, C);      // recursive call of the tower and hanoi function
     }
 }
 
-int main(){
+int main()
+{
     TOH(3,1,2,3);
 }
