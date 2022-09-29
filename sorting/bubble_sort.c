a85a699c736f7346fd89d89b2c7c9bffdb70b571
Abhinav Aryan <77077109+07abhi9av@users.noreply.github.com>
2022-09-30 01:12:00 +0530

    Update bubble_sort.c

--- sorting/bubble_sort.c
+++ sorting/bubble_sort.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 
-int main(){
-#define SIZE 5   // defining a macro
+int main()
+{
+#define SIZE 5   // defining a macro of definite size
 int arr[SIZE] = {3,6,2,7,1};   // taking a demo array
 
 //Bubble Sort Algorithm
-for(int i=0;i<SIZE-1;i++){
-    for(int j=0;j<SIZE-i-1;j++){
-        if(arr[j] > arr[j+1]){
-            int temp = arr[j];   //swapping using a temporary variable
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
+for(int i = 0 ; i < SIZE - 1 ; i++)        //loop till SIZE - 1 index
+{
+    for(int j = 0 ; j < SIZE -i - 1 ; j++)  //loop till SIZE of array - i -1 index
+    {
+        if(arr[j] > arr[j + 1])    //comparing j and j+1 element
+        {
+            int temp = arr[j];   //swapping using a temporary(temp) variable
+            arr[j] = arr[j + 1];    
+            arr[j + 1] = temp;
         }
     }
 }
 
-for(int i=0;i<SIZE;i++){    // Printing the array
-    printf("%d ",arr[i]);
+for(int i = 0; i < SIZE; i++)   // Printing the array using for loop.
+{    
+    printf("%d ", arr[i]);
 }
+
 return 0;
+
 }
