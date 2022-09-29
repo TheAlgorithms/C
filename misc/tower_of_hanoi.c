5ab14b37f2b0b75b51bd3f67a9e660cfafc1104a
Abhinav Aryan <77077109+07abhi9av@users.noreply.github.com>
2022-09-30 00:58:09 +0530

    Optimised function for tower of hanoi

--- misc/tower_of_hanoi.c
+++ misc/tower_of_hanoi.c
@@ -1,26 +1,15 @@
-
 #include <stdio.h>
-#include <stdlib.h>
 
 // Function for Tower of Hanoi algorithm
-void hanoi(int noOfDisks, char where, char to, char extra)
-{
-    if (noOfDisks != 0)
-    {
-        hanoi(noOfDisks - 1, where, extra, to);
-        printf("Move disk : %d from %c to %c\n", noOfDisks, where, to);
-        hanoi(noOfDisks - 1, extra, to, where);
+void TOH(int n, int A, int B, int C){
+    if(n>0){
+        // n-1 disks in the tower
+        TOH(n-1,A,C,B);
+        printf("Moving disc from rod %d to %d\n",A,C);
+        TOH(n-1,B,A,C);
     }
 }
-int main(void)
-{
-    int noOfDisks;
 
-    // Asks the number of disks in the tower
-    printf("Number of disks: \n");
-    scanf("%d", &noOfDisks);
-
-    hanoi(noOfDisks, 'A', 'B', 'C');
-
-    return 0;
+int main(){
+    TOH(3,1,2,3);
 }
