4ffaf3dfaaeb132bae66ea25d5cde444954a91c6
[Ankush Roy] <[imankushroy@gmail.com]>
2022-10-07 01:32:36 +0530

    Documentations re-updated

--- misc/pascal.c
+++ misc/pascal.c
@@ -1,14 +1,22 @@
 //C program to display the Pascal's triangle upto the nth input row
+//Author - Ankush Roy - https://github.com/ankushroy25
+
 #include <stdio.h> //inclusion of header file
 
 int main()// main function body
 {
-    int row, c = 1, j, i, k; // declaration and initialisation of variables
-    
+    // declaration and initialisation of variables
+    int row, c = 1, j, i, k; 
+    /* row - the no. of rows of the pascal triangle user wants to display
+    i,j,k - iterative variables
+    c - the calculate the binomial coefficients    
+    */
+
     printf("Input number of rows: "); 
     scanf("%d", &row); //user input
     
-    for (i = 0; i < row; i++) //loop for each row from top to bottom
+    //loop for each row from top to bottom
+    for (i = 0; i < row; i++) 
     {
         // print spaces to align the numbers properly
         for (j = 1; j <= row - i; j++) 
@@ -19,13 +27,21 @@ int main()// main function body
         //loop to print the numbers
         for (k = 0; k <= i; k++) 
         {
+            //as the first and last number in a row = 1, checking the boundary conditions 
             if (k == 0 || i == 0)
+            {   
                 c = 1;
+            }
             else
-                c = c * (i - k + 1) / k; // calculating the numbers by binomial coefficient method
+            {
+              // calculating the numbers by binomial coefficient method(using factorial reduced formula)
+                c = c * (i - k + 1) / k; 
+            }
             printf("% 4d", c);
         }
-        printf("\n");
+
+        //moving to the next line after all mumbers in a row has been displayed
+        printf("\n"); 
     }
     return 0; //exit by returning 0
 }
--- misc/quadratic.c
+++ misc/quadratic.c
@@ -1,29 +1,46 @@
-//C program to find out the roots and their nature of a quadratic equation
-//including the necessary header files
-#include <math.h>
+/*C program to find out the roots and their nature of a quadratic equation
+Author - Ankush Roy - https://github.com/ankushroy25
+*/
+
+/*including the necessary header files
+math.h- for mathematical operations
+stdio.h - for input-output operations
+*/
+#include <math.h> 
 #include <stdio.h>
+
+//main function body
 int main()
 {
-    float a, b, c, d, root1, root2, real, imag; //variable decalration
+    //variable decalration
+    float a, b, c, d, root1, root2, real, imag; 
+    /*a,b,c - the 3 coefficient numbers
+    root1,root2 - the final roots that will satisfy the equation
+    real - the real part of the root in case of complex roots
+    imag - the imaginary part of the root in case of complex roots    
+    d - the discriminant for the input equation
+    
+    */
+    
     printf("Enter coefficients a, b and c for the quadratic equation of the form 'ax^2+bx+c=0' (separated by spaces): ");
     scanf("%f %f %f", &a, &b, &c); //input of the coefficients of quadratic equation
     
-    d = b * b - 4 * a * c; //d is the discriminant for the input equation
+    d = b * b - 4 * a * c; 
 
-    // condition for real and different roots
+    // condition for real and different roots i.e. discriminant greater than 0
     if (d > 0)
     {
         root1 = (-b + sqrt(d)) / (2 * a);
         root2 = (-b - sqrt(d)) / (2 * a);
         printf("Real and distinct roots \nRoot1 = %.2f and Root2 = %.2f", root1, root2);
     }
-    // condition for real and equal roots
+    // condition for real and equal roots i.e. discriminant equal to 0
     else if (d == 0)
     {
         root1 = root2 = -b / (2 * a);
         printf("Real and equal roots \nRoot1 = Root2 = %.2f", root1);
     }
-    // if roots are not real
+    // if roots are not real i.e. discriminant lesser than 0
     else
     {
         real = -b / (2 * a);
