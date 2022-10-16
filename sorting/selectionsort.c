5c11b3305f7b76311a88b0714c801c37bd74b300
vimarsh11 <104987339+vimarsh11@users.noreply.github.com>
2022-10-16 19:59:10 +0530

    Add files via upload
    i uploaded selection sort in c program.

---
+++ sorting/selectionsort.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+void printArray(int* A, int n){
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
+
+void selectionSort(int *A, int n){
+    int indexOfMin, temp;
+    printf("Running Selection sort...\n");
+    for (int i = 0; i < n-1; i++)
+    {
+        indexOfMin = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if(A[j] < A[indexOfMin]){
+                indexOfMin = j;
+            }
+        }
+        // Swap A[i] and A[indexOfMin]
+        temp = A[i];
+        A[i] = A[indexOfMin];
+        A[indexOfMin] = temp;
+    }
+}
+
+int main(){
+    // Input Array (There will be total n-1 passes. 5-1 = 4 in this case!)
+    //  00  01  02  03  04
+    // |03, 05, 02, 13, 12
+
+    // After first pass
+    //  00  01  02  03  04
+    //  02,|05, 03, 13, 12
+
+    // After second pass
+    // 00  01  02  03  04
+    // 02, 03,|05, 13, 12
+
+    // After third pass
+    // 00  01  02  03  04
+    // 02, 03, 05,|13, 12
+
+    // After fourth pass
+    // 00  01  02  03  04
+    // 02, 03, 05, 12,|13
+
+
+    int A[] = {3, 5, 2, 13, 12};
+    int n = 5;
+    printArray(A, n);
+    selectionSort(A, n);
+    printArray(A, n);
+
+    return 0;
+}
+
