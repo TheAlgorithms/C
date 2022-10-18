c8f2df324004d9d8c97137cfa32876c0bb6d4a51
Anurag Sahay <92026363+anuragsahay5@users.noreply.github.com>
2022-10-19 00:37:48 +0530

    Updates 2427.c

--- leetcode/src/2427.c
+++ leetcode/src/2427.c
@@ -1,12 +1,14 @@
+int commonFactors(int a, int b) {
 
+  int n = fmin(a, b);
+  /* suppose two numbers be 6 and 18.so max number which can divide both
+  must be less than or equal to minimum of the two number i.e 6*/
 
-int commonFactors(int a, int b){
-
-    int n = fmax(fmin(a,b),fmax(a,b)/2),count=0;  
-    for(int i=1;i<=n;i++){
-        if(a%i == 0 && b%i==0){
-            count++;
-        }
+  int count = 0; // to keep count of common divisers
+  for (int i = 1; i <= n; i++) {
+    if (a % i == 0 && b % i == 0) {
+      count++;
     }
-    return count;
+  }
+  return count;
 }
