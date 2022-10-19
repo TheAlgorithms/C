f9cf47b18687bbb2fb15f6ab33038b0afcb48154
Sultan <73010596+sultanrif@users.noreply.github.com>
2022-10-19 09:35:49 +0300

    Create Pascal_triangle.c
    Pascal triangle 📐

---
+++ misc/Pascal_triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+   int rows, coef = 1, space, i, j;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 0; i < rows; i++) {
+      for (space = 1; space <= rows - i; space++)
+         printf("  ");
+      for (j = 0; j <= i; j++) {
+         if (j == 0 || i == 0)
+            coef = 1;
+         else
+            coef = coef * (i - j + 1) / j;
+         printf("%4d", coef);
+      }
+      printf("\n");
+   }
+   return 0;
+}
