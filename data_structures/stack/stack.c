f241b1b4eadebc76fe7d08b8429e84fce4a9e90d
OMsai <97650182+omsai11@users.noreply.github.com>
2022-10-23 22:38:40 +0530

    simple stack
    simple stack code is added below with various operations.

--- data_structures/stack/stack.c
+++ data_structures/stack/stack.c
@@ -124,3 +124,67 @@ void *top()
     /* offset address points to the top element */
     return array[offset];
 }
+
+/*The above code is good but it is very difficult to study the code so i am going to write my code which is far better than above code*/
+//
+//author :Omsai Mutyalwar
+//SY (CSE)
+//CR of the class
+//
+#include<stdio.h>
+int data[5];
+int st=-1;
+
+void push()
+{
+    if(st==4)
+    {
+        printf("Stack is full\n");
+    }
+    else
+    {
+        st++;
+        printf("Enter the data:\n");
+        scanf("%d",&data[st]);
+    }
+}
+void pop()
+{
+    if(st==-1)
+    {
+        printf("Stack is empty");
+    }
+    else
+    {
+        printf("Deleted element is :%d",data[st]);
+        st--;
+    }
+}
+void display()
+{
+    for(int i=0;i<=st;i++)
+    {
+        printf("%d-->",data[i]);
+    }
+}
+void main()
+{
+    int o;
+    while (1)
+    {
+      printf("Enter\n1.push\n2.pop\n3.display\n");
+      scanf("%d",&o);
+      switch(o)
+      {
+        case 1:push(); break;
+
+        case 2:pop(); break;
+
+        case 3: display(); break;
+
+        default : printf("Invalid\n");
+        break;
+      }  /* code */
+    }
+    
+}
