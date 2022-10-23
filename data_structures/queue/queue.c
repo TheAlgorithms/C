0a69691a6ac3b490da85c8d476b6846a5fe8ba30
OMsai <97650182+omsai11@users.noreply.github.com>
2022-10-23 22:05:48 +0530

    queue using array
    code for queue using array is added in simple format in comments

--- data_structures/queue/queue.c
+++ data_structures/queue/queue.c
@@ -71,3 +71,63 @@ int deque()
  * Returns the size of the Queue.
  */
 int size() { return count; }
+
+//Queue using array
+/*#include<stdio.h>
+int data[10];
+int f=0;
+int r=-1;
+void insert()
+{
+    if(r==9)
+    {
+        printf("Queue is full\n");
+    }
+        else
+        {
+            r++;
+            printf("Enter the data:\n");
+            scanf("%d",&data[r]);
+        }
+    
+}
+void delete()
+{
+    if(f==-1||f>r)
+    {
+        printf("Queue is Empty\n");
+    }
+    else
+    {
+        printf("Deleted element is :%d\n",data[f]);
+        f++;
+    }
+}
+void display()
+{
+    for(int i=f;i<=r;i++)
+    {
+        printf("%d-->",data[i]);
+    }
+}
+void main()
+{  
+    int o;
+    while (1)
+    {
+      printf("Enter\n1.push\n2.pop\n3.display\n");
+      scanf("%d",&o);
+      switch(o)
+      {
+        case 1:insert(); break;
+
+        case 2:display(); break;
+
+        case 3: display(); break;
+
+        default : printf("Invalid\n");
+        break;
+      }  /* code */
+    }
+    
+}*/
