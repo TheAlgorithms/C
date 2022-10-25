23bae3186d4533ca072fded6b2d9d72ebb42f052
Ebin <79463025+ebinvarghese020@users.noreply.github.com>
2022-10-25 21:57:58 +0530

    Char_to_ASCII
    converts character to ASCII value

---
+++ conversions/char_to_ascii.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  
+void main()  
+{  
+ int k;   // variable declaration   
+ for(int k=0;k<=255;k++)  // for loop from 0-255  
+ {  
+     printf("\nThe ascii value of %c is %d", k,k);  
+ }  
+} 
