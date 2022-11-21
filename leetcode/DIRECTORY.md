a9afa7318dab14e5bcf90bc7113ca6d624b90f25
alexpantyukhin <apantykhin@gmail.com>
2022-11-21 12:52:34 +0400

    add leetcode Lowest Common Ancestor of a Binary Tree

--- leetcode/DIRECTORY.md
+++ leetcode/DIRECTORY.md
@@ -59,6 +59,7 @@
 | 226  | [Invert Binary Tree](https://leetcode.com/problems/invert-binary-tree/)                                                         | [C](./src/226.c)  | Easy       |
 | 231  | [Power of Two](https://leetcode.com/problems/power-of-two/)                                                                     | [C](./src/231.c)  | Easy       |
 | 234  | [Palindrome Linked List](https://leetcode.com/problems/palindrome-linked-list/)                                                 | [C](./src/234.c)  | Easy       |
+| 236  | [Lowest Common Ancestor of a Binary Tree](https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/)               | [C](./src/236.c)  | Medium     |
 | 242  | [Valid Anagram](https://leetcode.com/problems/valid-anagram/)                                                                   | [C](./src/242.c)  | Easy       |
 | 268  | [Missing Number](https://leetcode.com/problems/missing-number/)                                                                 | [C](./src/268.c)  | Easy       |
 | 278  | [First Bad Version](https://leetcode.com/problems/first-bad-version/)                                                           | [C](./src/278.c)  | Easy       |
---
+++ leetcode/src/236.c
@@ -0,0 +1,80 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct List {
+    struct TreeNode* node;
+    struct List* next;
+};
+
+bool findPathOfTarget(struct TreeNode* node, struct TreeNode* target, struct List* path){
+    if (node == NULL){
+        return false;
+    }
+    
+    struct List* pathItem = malloc(sizeof(struct List));
+    pathItem->node = node;
+    pathItem->next = NULL;
+    path->next = pathItem;
+    
+    if (node->val == target->val){
+        return true;
+    }
+
+    if (findPathOfTarget(node->left, target, pathItem)){
+        return true;
+    }
+    
+    if (findPathOfTarget(node->right, target, pathItem)){
+        return true;
+    }
+    
+    path->next = NULL;
+    free(pathItem);
+    return false;
+}
+
+void freeList(struct List* target){
+    if (target->next != NULL){
+        freeList(target->next);
+    }
+    
+    free(target);
+}
+
+
+// Find full path for p and q.
+// Find the longest common path in pathes.
+// Runtime: O(n)
+// Space: O(n)
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    struct List* pPath = malloc(sizeof(struct List));
+    struct List* qPath = malloc(sizeof(struct List));
+    
+    findPathOfTarget(root, p, pPath);
+    findPathOfTarget(root, q, qPath);
+    
+    struct TreeNode* lowestTreeNode = NULL;
+    struct List* pPathCursor = pPath->next;
+    struct List* qPathCursor = qPath->next;
+    while(pPathCursor != NULL && qPathCursor != NULL) {
+        if (pPathCursor->node->val == qPathCursor->node->val){
+            lowestTreeNode = pPathCursor->node;
+            pPathCursor = pPathCursor->next;
+            qPathCursor = qPathCursor->next;
+            continue;
+        }
+
+        break;
+    }
+    
+    freeList(pPath);
+    freeList(qPath);
+    
+    return lowestTreeNode;
+}
