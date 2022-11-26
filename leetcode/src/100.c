061db549c862d32b6e01a0734796f8a0c78e4ddd
arnabpal2022 <arnabpalofficial2003@gmail.com>
2022-11-26 21:25:49 +0530

    Update 100.c

--- leetcode/src/100.c
+++ leetcode/src/100.c
@@ -15,18 +15,8 @@ struct TreeNode
 
 bool isSameTree(struct TreeNode *p, struct TreeNode *q)
 {
-    if (p == NULL && q == NULL)
-        return true;
-    else if (p && q)
-    {
-        if (p->val == q->val && isSameTree(p->left, q->left) &&
-            isSameTree(p->right, q->right))
-            return true;
-        else
-            return false;
-    }
-    else
-        return false;
+    return (p->val == q->val && isSameTree(p->left, q->left) &&
+            isSameTree(p->right, q->right));
 }
 
 void printTreePreOrder(struct TreeNode *p)
