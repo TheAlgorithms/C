b3369b36f230574f465b7f36297b487462b733bd
Alexander Pantyukhin <apantykhin@gmail.com>
2022-12-21 20:07:38 +0400

    Update 274.c
    fix review notes.

--- leetcode/src/274.c
+++ leetcode/src/274.c
@@ -9,7 +9,7 @@ int diff(const int* i, const int* j)
 // Runtime: O(n*log(n))
 // Space: O(1)
 int hIndex(int* citations, int citationsSize){
-    qsort(citations, citationsSize, sizeof(int), (int(*) (const void*, const void*)) comparer);
+    qsort(citations, citationsSize, sizeof(int), (int(*) (const void*, const void*)) diff);
 
     for(int i = 0; i < citationsSize; i++){
         if (citations[citationsSize - 1 - i] <= i){
