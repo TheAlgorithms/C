99472b501b5621ac8129e35301b1c36b4b569188
Alexander Pantyukhin <apantykhin@gmail.com>
2022-12-30 09:55:13 +0400

    Update leetcode/src/2279.c
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- leetcode/src/2279.c
+++ leetcode/src/2279.c
@@ -15,7 +15,7 @@ int maximumBags(int* capacity, int capacitySize, int* rocks, int rocksSize, int 
     qsort(capacityLeft, capacitySize, sizeof (int), (int(*) (const void*, const void*)) compare);
     
     int bags = 0;
-    for (int i = 0; i < capacitySize; i++){
+    for (int i = 0; i < capacitySize; i++) {
         if (additionalRocks < capacityLeft[i]){
             break;
         }
