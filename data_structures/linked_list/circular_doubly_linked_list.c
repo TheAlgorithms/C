2afb38f84bdbdfd9670d1cf8363f3fd7dee0fa68
Sahil Kandhare <104154041+SahilK-027@users.noreply.github.com>
2023-01-02 08:21:51 +0530

    Update data_structures/linked_list/circular_doubly_linked_list.c
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- data_structures/linked_list/circular_doubly_linked_list.c
+++ data_structures/linked_list/circular_doubly_linked_list.c
@@ -2,9 +2,9 @@
  * @file
  *
  * @details
- Circular Doubly Linked List combines the properties of a doubly linked list and a circular linked list in which two consecutive elements are linked or connected by the previous and next pointer and the last node points to the first node via the next pointer and the first node points last node via the previous pointer.
-
- In this implementation, the menu driven code is written along with functions to insert at start, insrt at last, delete first node, delete last node and display list functions are coded.
+ * Circular [Doubly Linked List](https://en.wikipedia.org/wiki/Doubly_linked_list) combines the properties of a doubly linked list and a circular linked list in which two consecutive elements are linked or connected by the previous. Next, the pointer and the last node point to the first node via the next pointer, and the first node points to the last node via the previous pointer.
+ *
+ * In this implementation, the menu-driven code is written along with functions to insert at the start, insert at the last index, delete the first node, delete the last node, and display list functions are coded.
  
  * @author [Sahil Kandhare](https://github.com/SahilK-027)
  *
