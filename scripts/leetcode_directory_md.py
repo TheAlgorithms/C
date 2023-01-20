b3db5142b37227fea63e08b432dab82780735bbf
Alexander Pantyukhin <apantykhin@gmail.com>
2023-01-20 15:53:59 +0400

    Update scripts/leetcode_directory_md.py
    Co-authored-by: Taj <tjgurwara99@users.noreply.github.com>

--- scripts/leetcode_directory_md.py
+++ scripts/leetcode_directory_md.py
@@ -20,7 +20,7 @@ def fetch_leetcode_folder_tasks(solutions_folder: Path) -> list[Task]:
         url="https://leetcode.com/api/problems/algorithms/", 
         headers={"User-Agent": "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"}
     )
-
+    resp = requests.get("https://leetcode.com/api/problems/algorithms/")
     content = urlopen(req).read()
     content_dict = resp.json()
 
