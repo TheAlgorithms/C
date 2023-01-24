57d5231c3b2fee64f60e80f372034cd32fe4369a
Aybars Nazlica <aybarsnazlica@gmail.com>
2023-01-24 15:40:12 +0900

    Fix character comparison

--- misc/hamming_distance.c
+++ misc/hamming_distance.c
@@ -24,7 +24,7 @@ int hammingDistance(char* str1, char* str2)
 
     while (str1[i] != '\0')
     {
-        if (str1[i] == str2[i])
+        if (str1[i] != str2[i])
         {
             distance++;
         }
