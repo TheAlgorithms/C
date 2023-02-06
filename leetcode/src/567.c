86973ac23455591c84250df8aabdc33d785a38f3
Alexander Pantyukhin <apantykhin@gmail.com>
2023-02-06 14:27:43 +0400

    Update 567.c
    fix review notes

--- leetcode/src/567.c
+++ leetcode/src/567.c
@@ -1,7 +1,6 @@
 const int EnglishLettersNumber = 26;
 
 void countCharsForStringSlice(int* charsCounter, char* s, int length, int sign) {
-
     for (int i = 0; i < length; i++) {
 
         charsCounter[s[i] - 'a'] += sign;
@@ -14,7 +13,6 @@ void countCharsForStringSlice(int* charsCounter, char* s, int length, int sign) 
 // Runtime: O(n)
 // Space: O(1) - only number of english lowercase letters.
 bool checkInclusion(char* s1, char* s2) {
-
     int lengthS1 = strlen(s1);
     int lengthS2 = strlen(s2);
 
@@ -31,44 +29,36 @@ bool checkInclusion(char* s1, char* s2) {
 
     int diffChars = 0;
     for (int i = 0; i < EnglishLettersNumber; i++) {
-
         if (charsCounter[i] != 0) {
             diffChars++;
         }
     }
 
     if (diffChars == 0) {
-
         return true;
     }
 
     for (int i = 0; i < lengthS2 - lengthS1; i++) {
-
         int charNumberLeft = s2[i] - 'a';
         int charNumberRight = s2[i + lengthS1] - 'a'; 
 
         charsCounter[charNumberLeft] -= 1;
         if (charsCounter[charNumberLeft] == 0) {
-
             diffChars -= 1;
         }
         else if (charsCounter[charNumberLeft] == -1) {
-
             diffChars += 1;
         }
 
         charsCounter[charNumberRight] += 1;
         if (charsCounter[charNumberRight] == 0) {
-
             diffChars -= 1;
         }
         else if (charsCounter[charNumberRight] == 1) {
-
             diffChars += 1;
         }
 
         if (diffChars == 0) {
-
             return true;
         }
     }
