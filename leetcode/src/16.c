c53dade54d996d8111c66c4d0fb0c8b325f3cda3
Mindaugas Lekaveckas <mindelek@gmail.com>
2023-02-18 19:50:09 +0000

    fix: Update formatting

--- leetcode/src/16.c
+++ leetcode/src/16.c
@@ -8,7 +8,7 @@ int threeSumClosest(int* nums, int nums_size, int target) {
     int i, j, k, result, sum3;
     qsort(nums, nums_size, sizeof(int), cmp);
     result = nums[0] + nums[1] + nums[2];
-    for (i = 0; i < nums_size-2; i++) {
+    for (i = 0; i < nums_size - 2; i++) {
         j = i + 1;
         k = nums_size - 1;
         while (j < k) {
