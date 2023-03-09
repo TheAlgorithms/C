19cf23865a2690f351e44f6eee43b9cb4f57b54d
Niranjan <niranjank.desktop@gmail.com>
2023-03-09 15:58:32 +0530

    Improve the documentation of factorial.c

--- math/fibonacci.c
+++ math/fibonacci.c
@@ -1,23 +1,40 @@
+/**
+ * @file fibonacci.c
+ * @brief Program to print the nth term of the Fibonacci series.
+ * @details Fibonacci series generally starts from 0 and 1. Every next term in
+ *the series is equal to the sum of the two preceding terms.
+ **/
+
 #include <stdio.h>
 
-// Fibonnacci function
-int fib(int number)
-{
-    if (number == 1 || number == 2)
-        return 1;
-    else
-        return fib(number - 1) + fib(number - 2);
-}
+unsigned fib(int);
 
+/* Driver Code */
 int main()
 {
     int number;
 
-    // Asks for the number that is in n position in Fibonnacci sequence
-    printf("Number: ");
+    // Getting n
+    printf("Enter n to find nth fibonacci element: ");
     scanf("%d", &number);
 
-    printf("%d \n", fib(number));
-
+    printf("%u\n", fib(number));
     return 0;
+}
+
+/**
+ * @brief Determines the nth Fibonacci term
+ * @param number - n in "nth term" and it can't be negative
+ * @return nth term in unsigned type
+ * @warning Only till 46th and 47th fibonacci element can be stored in 'int' and
+ * 'unsigned' respectively (takes more than 20 seconds to print)
+ */
+unsigned fib(int number)
+{
+    if (number == 1)
+        return 0;
+    else if (number == 2)
+        return 1;
+    else
+        return fib(number - 1) + fib(number - 2);
 }
