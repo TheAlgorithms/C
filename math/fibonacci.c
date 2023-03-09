6c98363e1b20aaa596f19248768e0b46db1c4549
Niranjan <niranjank.desktop@gmail.com>
2023-03-09 16:12:19 +0530

    Update starting terms of fibonacci as 0 and 1

--- math/fibonacci.c
+++ math/fibonacci.c
@@ -26,12 +26,14 @@ int main()
  * @brief Determines the nth Fibonacci term
  * @param number - n in "nth term" and it can't be negative
  * @return nth term in unsigned type
- * @warning Only till 46th and 47th fibonacci element can be stored in 'int' and
+ * @warning Only till 47th and 48th fibonacci element can be stored in 'int' and
  * 'unsigned' respectively (takes more than 20 seconds to print)
  */
 unsigned fib(int number)
 {
-    if (number == 1 || number == 2)
+    if (number == 1)
+        return 0;
+    else if (number == 2)
         return 1;
     else
         return fib(number - 1) + fib(number - 2);
