2c0da3e03846f99c62d54dea0bb62c3251634505
Niranjan <niranjank.desktop@gmail.com>
2023-03-14 07:34:33 +0530

    test: Add test

--- math/fibonacci.c
+++ math/fibonacci.c
@@ -6,8 +6,9 @@
  * the series is equal to the sum of the two preceding terms.
  */
 
-#include <stdio.h>  /// for input and output
-#include <stdlib.h>
+#include <assert.h>  /// for assert()
+#include <stdio.h>   /// for input, output
+#include <stdlib.h>  /// for exit() - to exit the program
 /**
  * @brief Determines the nth Fibonacci term
  * @param number - n in "nth term" and it can't be negative as well as zero
@@ -19,7 +20,7 @@
 unsigned int fib(int number)
 {
     // Check for negative integers
-    if (number < 0)
+    if (number <= 0)
     {
         fprintf(stderr, "Illegal Argument Is Passed!\n");
         exit(EXIT_FAILURE);
@@ -36,9 +37,20 @@ unsigned int fib(int number)
     return fib(number - 1) + fib(number - 2);
 }
 
+void test()
+{
+    assert(fib(1));
+    assert(fib(2));
+    assert(fib(9));
+}
+
 int main()
 {
     int number;
+
+    // Performing the test
+    test();
+    printf("Tests passed...\n");
 
     // Getting n
     printf("Enter n to find nth fibonacci element: ");
@@ -47,11 +59,3 @@ int main()
     printf("Fibonacci element %d is %u\n", number, fib(number));
     return 0;
 }
-
-/**
- * @brief Determines the nth Fibonacci term
- * @param number - n in "nth term" and it can't be negative
- * @return nth term in unsigned type
- * @warning Only till 47th and 48th fibonacci element can be stored in 'int' and
- * 'unsigned' respectively (takes more than 20 seconds to print)
- */
