c8120919daf8e7900dd9f349d11f0581dd6677b1
Daniel Murrow <dsmurrow@gmail.com>
2023-04-08 16:43:01 -0400

    test: added checks for correct ciphertext

--- cipher/affine.c
+++ cipher/affine.c
@@ -148,7 +148,7 @@ void affine_decrypt(char *s, affine_key_t key)
  *
  * @returns void
  */
-void test_string(const char *s, int a, int b)
+void test_string(const char *s, const char *ciphertext, int a, int b)
 {
     char *copy = malloc((strlen(s) + 1) * sizeof(char));
     strcpy(copy, s);
@@ -156,7 +156,7 @@ void test_string(const char *s, int a, int b)
     affine_key_t key = {a, b};
 
     affine_encrypt(copy, key);
-    assert(strcmp(copy, s) != 0);  // assert that the encryption worked
+    assert(strcmp(copy, ciphertext) == 0);  // assert that the encryption worked
 
     affine_decrypt(copy, key);
     assert(strcmp(copy, s) ==
@@ -172,15 +172,18 @@ void test_string(const char *s, int a, int b)
  */
 void tests()
 {
-    test_string("Hello!", 7, 11);
-    test_string("TheAlgorithms/C", 67, 67);
-    test_string("0123456789", 91, 88);
-    test_string("7W@;cdeRT9uL", 77, 76);
-    test_string("~Qr%^-+++$leM", 8, 90);
-    test_string("The quick brown fox jumps over the lazy dog", 94, 0);
+    test_string("Hello!", "&3ddy2", 7, 11);
+    test_string("TheAlgorithms/C", "DNC}=jHS2zN!7;E", 67, 67);
+    test_string("0123456789", "840,($ {ws", 91, 88);
+    test_string("7W@;cdeRT9uL", "JDfa*we?z&bL", 77, 76);
+    test_string("~Qr%^-+++$leM", "r'qC0$sss;Ahf", 8, 90);
+    test_string("The quick brown fox jumps over the lazy dog",
+                "K7: .*6<4 =-0(1 90' 5*2/, 0):- +7: 3>%& ;08", 94, 0);
     test_string(
         "One-1, Two-2, Three-3, Four-4, Five-5, Six-6, Seven-7, Eight-8, "
         "Nine-9, Ten-10",
+        "H&60>\\2*uY0q\\2*p4660E\\2XYn40x\\2XDB60L\\2VDI0 "
+        "\\2V6B6&0S\\2%D=p;0'\\2tD&60Z\\2*6&0>j",
         51, 18);
 
     printf("all tests have successfully passed!\n");
