f06321895820b83c4cb39109eb0db1ef9660ea24
N.Pranav Krishna <52345192+npranav7619@users.noreply.github.com>
2023-04-22 10:15:54 +0530

    Update shared_memory_reader.c

--- IPC/shared_memory_reader.c
+++ IPC/shared_memory_reader.c
@@ -9,20 +9,14 @@ int main()
 {
     // ftok to generate unique key
     key_t key = ftok("shmfile",65);
-  
     // shmget returns an identifier in shmid
     int shmid = shmget(key,1024,0666|IPC_CREAT);
-  
     // shmat to attach to shared memory
     char *str = (char*) shmat(shmid,(void*)0,0);
-  
     printf("Data read from memory: %s\n",str);
-      
     //detach from shared memory 
     shmdt(str);
-    
     // destroy the shared memory
     shmctl(shmid,IPC_RMID,NULL);
-     
     return 0;
 }
