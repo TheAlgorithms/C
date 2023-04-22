6d0be0057454cdc1d4384f184966b5105b746c6a
N.Pranav Krishna <52345192+npranav7619@users.noreply.github.com>
2023-04-22 10:16:13 +0530

    Update shared_memory_writer.c

--- IPC/shared_memory_writer.c
+++ IPC/shared_memory_writer.c
@@ -8,20 +8,14 @@ int main()
 {
     // ftok to generate unique key
     key_t key = ftok("shmfile",65);
-  
     // shmget returns an identifier in shmid
     int shmid = shmget(key,1024,0666|IPC_CREAT);
-  
     // shmat to attach to shared memory
     char *str = (char*) shmat(shmid,(void*)0,0);
-  
     cout<<"Write Data : ";
     gets(str);
-  
     printf("Data written in memory: %s\n",str);
-      
     //detach from shared memory 
     shmdt(str);
-  
     return 0;
 }
