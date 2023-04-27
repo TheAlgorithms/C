144a178b69b4341603145cbb2bb59d57593ffd47
MRio <55620821+AtlantaEmrys2002@users.noreply.github.com>
2023-04-27 16:31:51 +0100

    Update radix_sort.c
    Added documentation/formatting to radix sort.

--- sorting/radix_sort.c
+++ sorting/radix_sort.c
@@ -1,71 +1,105 @@
-#include <stdio.h>
+/**
+ * @file
+ * @brief C implementation of [Radix Sort](https://en.wikipedia.org/wiki/Radix_sort)
+ * @details
+ * Readable version of radix sort, allowing user to input size of array (up to 10) and specify elements of unordered array.
+ */
 
-int largest(int a[], int n)
-{
-    int large = a[0], i;
-    for (i = 1; i < n; i++)
-    {
-        if (large < a[i])
-            large = a[i];
+#include <stdio.h> /// for radix_sort() and main() - I/O operations
+
+/**
+ * @brief finds largest element in int array
+ * @param a pointer to array of numbers
+ * @param n size of a
+ * @returns large - the largest number in the array
+ */
+int largest(int const a[], int n) {
+
+    int large = a [0]; // sets largest number to first element in array
+    for (int i = 1; i < n; i++) {
+        if (large < a[i]) {
+            large = a[i]; // if number found in array > largest, sets largest to that number
+        }
     }
+
     return large;
 }
 
-void RadixSort(int a[], int n)
-{
-    int bucket[10][10], bucket_count[10];
-    int i, j, k, remainder, NOP = 0, divisor = 1, large, pass;
+/**
+ * @brief radix_sort - takes in unordered array, sorts it, returns ordered array
+ * @param a pointer to unordered array of numbers
+ * @param n size of a (number of elements)
+ * @returns void
+ */
+void radix_sort(int a[], int n) {
 
-    large = largest(a, n);
-    printf("The large element %d\n", large);
-    while (large > 0)
-    {
+    int bucket[10][10]; // creates bucket for radix sort of decimal numbers
+    int bucket_count[10]; // initialises counters (for number of elements) for 10 buckets
+    int remainder; // remainder of division
+    int NOP = 0; // number of passes of array that need to be made
+    int large; // largest number
+    int divisor = 1; // indicates which digit of number algorithm should work with
+
+    large = largest(a,n); // finds largest number in array
+
+    printf("The largest element: %d\n", large);
+
+    while (large > 0) { // finds number of passes to make of array (number of passes = number of digits in largest number)
         NOP++;
         large /= 10;
     }
 
-    for (pass = 0; pass < NOP; pass++)
-    {
-        for (i = 0; i < 10; i++)
-        {
+    for (int pass = 0; pass < NOP; pass++) {
+
+        for (int i = 0; i < 10; i++) { // initialises each bucket count (number of elements in bucket) to 0
             bucket_count[i] = 0;
         }
-        for (i = 0; i < n; i++)
-        {
-            remainder = (a[i] / divisor) % 10;
-            bucket[remainder][bucket_count[remainder]] = a[i];
-            bucket_count[remainder] += 1;
+
+        for (int i = 0; i < n; i++) {
+            remainder = (a[i] / divisor) % 10; // finds value of current digit of current number (e.g. if 657 and on 3rd pass, digit = 6)
+            printf("%d ", remainder);
+            bucket[remainder][bucket_count[remainder]] = a[i]; // stores number in appropriate bucket
+            bucket_count[remainder] += 1; // increases bucket count (number of elements in bucket) by 1
         }
 
-        i = 0;
-        for (k = 0; k < 10; k++)
-        {
-            for (j = 0; j < bucket_count[k]; j++)
-            {
-                a[i] = bucket[k][j];
-                i++;
+        int count = 0; // puts all elements back into original array
+        for (int k = 0; k < 10; k++) {
+            for (int j = 0; j < bucket_count[k]; j++) {
+                a[count] = bucket[k][j];
+                count ++;
             }
         }
-        divisor *= 10;
 
-        for (i = 0; i < n; i++) printf("%d  ", a[i]);
-        printf("\n");
+        divisor *=10; // next pass will focus on next digit along
+
     }
 }
 
-int main()
-{
-    int i, n, a[10];
-    printf("Enter the number of elements :: ");
+
+/*
+ * @brief Main Function
+ * @returns 0 on exit
+ */
+int main() {
+
+    int n; // size of a
+    int a[10]; // array of size 10
+
+    printf("Enter the number of elements: "); // up to 10
     scanf("%d", &n);
-    printf("Enter the elements :: ");
-    for (i = 0; i < n; i++)
-    {
+
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
-    RadixSort(a, n);
-    printf("The sorted elements are ::  ");
-    for (i = 0; i < n; i++) printf("%d  ", a[i]);
+
+    radix_sort(a, n);
+
+    printf("The sorted elements are: "); // outputs sorted array
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
     printf("\n");
     return 0;
+
 }
