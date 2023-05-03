8162ddfed5226d56aee3b75d451bfaaea1456875
disnoca <samuelp2002@gmail.com>
2023-05-03 10:35:17 +0100

    fixed indentation

--- numerical_methods/secant_method.c
+++ numerical_methods/secant_method.c
@@ -1,17 +1,17 @@
 /**
  * @file
- * @brief [Wikipedia link](https://en.wikipedia.org/wiki/Secant_method). Find a continuous function's root by using a succession 
- * of roots of secant lines to approximate it, starting from the 
- * given points' secant line.
+ * @brief [Wikipedia link](https://en.wikipedia.org/wiki/Secant_method). Find a
+ * continuous function's root by using a succession of roots of secant lines to
+ * approximate it, starting from the given points' secant line.
  * @author [Samuel Pires](https://github.com/disnoca)
  */
 
-#include <assert.h>	/// for assert
-#include <math.h>	/// for fabs
-#include <stdio.h>	/// for io operations
+#include <assert.h>  /// for assert
+#include <math.h>    /// for fabs
+#include <stdio.h>   /// for io operations
 
-#define TOLERANCE 0.0001	// root approximation result tolerance
-#define NMAX 100			// maximum number of iterations
+#define TOLERANCE 0.0001  // root approximation result tolerance
+#define NMAX 100          // maximum number of iterations
 
 /**
  * @brief Continuous function for which we want to find the root
@@ -32,36 +32,39 @@ double func(double x)
  * maximum number of iterations
  * @returns `-1` if secant method fails
  */
-double secant_method(double x0, double x1, double tolerance) 
+double secant_method(double x0, double x1, double tolerance)
 {
-	int n = 1;      // step counter
+    int n = 1;  // step counter
 
-    while(n++ < NMAX) {
-		// calculate secant line root
+    while (n++ < NMAX)
+    {
+        // calculate secant line root
         double x2 = x1 - func(x1) * (x1 - x0) / (func(x1) - func(x0));
-		
-		// update values
+
+        // update values
         x0 = x1;
-		x1 = x2;
+        x1 = x2;
 
-		// return value if it meets tolerance
-		if(fabs(x1-x0) < tolerance)
-			return x2;
-	}
+        // return value if it meets tolerance
+        if (fabs(x1 - x0) < tolerance)
+            return x2;
+    }
 
-    return -1;	// method failed (maximum number of steps exceeded)
+    return -1;  // method failed (maximum number of steps exceeded)
 }
 
 /**
  * @brief Self-test implementations
  * @returns void
  */
-static void test() {
-	// calculates two different roots for f(x) = x^2 - 3, which are sqrt(3) and -sqrt(3) respectively
+static void test()
+{
+    // calculates two different roots for f(x) = x^2 - 3, which are sqrt(3) and
+    // -sqrt(3) respectively
     assert(secant_method(0.2, 0.5, TOLERANCE) - sqrt(3) < TOLERANCE);
-	// the algorithm works as expected
-	assert(fabs(secant_method(-2, -5, TOLERANCE)) - sqrt(3) < TOLERANCE); 	
-	// the algorithm works as expected
+    // the algorithm works as expected
+    assert(fabs(secant_method(-2, -5, TOLERANCE)) - sqrt(3) < TOLERANCE);
+    // the algorithm works as expected
 
     printf("All tests have successfully passed!\n");
 }
@@ -70,8 +73,8 @@ static void test() {
  * @brief Main function
  * @returns 0 on exit
  */
-int main() 
+int main()
 {
-	test();		// run self-test implementations
-	return 0;
+    test();  // run self-test implementations
+    return 0;
 }
