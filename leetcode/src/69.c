5b013e41325bb45aa86204c1a1f91c3bd8904742
hemanth8819 <119241921+hemanth8819@users.noreply.github.com>
2023-05-09 22:33:07 +0530

    feat: add LeetCode problem 69
    Here is the code for the problem 69 of leetcode as there are many ways to do it we programmers need to find the most optimal way to solve a problem statement so i used binary-search approach inorder to solve it.
    All  suggestions are accepted!!!

---
+++ leetcode/src/69.c
@@ -0,0 +1,24 @@
+//using binary search method is one of the efficient one for this problem statement.
+int mySqrt(int x){
+int start=0;
+        int end=x;
+        long long int ans=0;
+        while(start <= end){
+               long long int mid=(start+end)/2;
+               if(mid*mid == x){
+                         ans=mid;
+                         break;
+                }
+//if mid is less than the square root of the number(x) store the value of mid in ans.
+               if( mid*mid < x){
+                         ans = mid;
+                       start = mid+1;
+               }
+//if mid is greater than the square root of the number(x) then ssign the value mid-1 to end.               
+               if( mid*mid > x){
+                       end = mid-1;
+               }
+        }
+        return ans;
+
+}
