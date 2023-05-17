900f57ff1b405a989aeb3ec5ee13ff49e5ef5ce5
hemanth8819 <119241921+hemanth8819@users.noreply.github.com>
2023-05-17 18:02:35 +0530

    Update 69.c

--- leetcode/src/69.c
+++ leetcode/src/69.c
@@ -6,16 +6,16 @@ int start=0;
         while(start <= end){
                long long int mid=(start+end)/2;
                 long long int val=mid*mid;
-               if(mid*mid == x){
+               if( val == x){
                          return mid;
                 }
 //if mid is less than the square root of the number(x) store the value of mid in ans.
-               if( mid*mid < x){
+               if( val < x){
                          ans = mid;
                        start = mid+1;
                }
 //if mid is greater than the square root of the number(x) then ssign the value mid-1 to end.               
-               if( mid*mid > x){
+               if( val > x){
                        end = mid-1;
                }
         }
