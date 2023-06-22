0e330b547fc5e95fcb2170cc40aa6b02184d1032
hemanth8819 <119241921+hemanth8819@users.noreply.github.com>
2023-06-22 10:43:13 +0530

    Update 69.c

--- leetcode/src/69.c
+++ leetcode/src/69.c
@@ -1,17 +1,17 @@
-//using binary search method is one of the efficient one for this problem statement.
+//using the binary search method is one of the efficient ones for this problem statement.
 int mySqrt(int x){
 int start=0;
         int end=x;
         long long int ans=0;
         while(start <= end){
                long long int mid=(start+end)/2;
-                long long int val=mid*mid;
+               long long int val=mid*mid;
                if( val == x){
-                         return mid;
+                   return mid;
                 }
 //if mid is less than the square root of the number(x) store the value of mid in ans.
                if( val < x){
-                         ans = mid;
+                       ans = mid;
                        start = mid+1;
                }
 //if mid is greater than the square root of the number(x) then ssign the value mid-1 to end.               
@@ -19,6 +19,5 @@ int start=0;
                        end = mid-1;
                }
         }
-        return ans;
-
+   return ans;
 }
