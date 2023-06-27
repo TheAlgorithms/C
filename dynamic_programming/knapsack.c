cf2e5551f0760592137f7dd6979073141379b877
Kim Areum <101612927+aa001R@users.noreply.github.com>
2023-06-27 10:49:37 +0900

    Update dynamic_programming/knapsack.c

--- dynamic_programming/knapsack.c
+++ dynamic_programming/knapsack.c
@@ -6,7 +6,7 @@
  * From Wikipedia: The knapsack problem is
  * Given a set of items, each with a weight and a value, determine
  * which items to include in the collection
- * so that the total weight is less than or equal to a given limit 
+ * so that the total weight is less than or equal to a given limit
  * and the total value is as large as possible.
  * @author [aa001R](https://github.com/aa001R)
  */
@@ -15,7 +15,10 @@
 #include <stdio.h>
 
 /**
- * @brief max() macro function
+ * @brief Returns the maximum value between two values a and b
+ * @param a the value to be compared
+ * @param b the value to be compared
+ * @returns the maximum value between a and b
  */
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
@@ -34,12 +37,24 @@ struct Item
 };
 
 /**
+ * @brief 0-1 Knapsack algorithm
+ * @param maxWeight the capacity of the knapsack
+ * @param itemCount the number of items(Item structure)
+ * @param items an array of Item structure
+ * @param K the 2D array storing the optimal solution
+ * @returns void
+ */
+void print_knapsack_result(int maxWeight, int itemCount, struct Item items[],int K[][MAXCAPACITY + 1]);
+
+
+/**
  * @brief Print input items function
  * @param items an array of Item structure
  * @param itemCount the number of items(Item structure)
  * @returns void
  */
 void print_input_struct_Items(struct Item items[], int itemCount);
+
 /**
  * @brief Print the combination of items in the knapsack
  * @param maxWeight the capacity of the knapsack
@@ -48,8 +63,7 @@ void print_input_struct_Items(struct Item items[], int itemCount);
  * @param K the 2D array storing the optimal solution
  * @returns void
  */
-void print_knapsack_item(int maxWeight, struct Item items[], int itemCount,
-                         int K[][MAXCAPACITY + 1]);
+void print_knapsack_item(int maxWeight, struct Item items[], int itemCount, int K[][MAXCAPACITY + 1]);
 
 /**
  * @brief 0-1 Knapsack algorithm
@@ -64,7 +78,7 @@ int knapsack(int maxWeight, struct Item items[], int itemCount)
     print_input_struct_Items(items, itemCount);
 
     // 2D array to store the optimal solution of subproblems
-    int K[itemCount + 1][maxWeight + 1];
+    int K[itemCount + 1][MAXCAPACITY + 1];
 
     // Initialize the first row and column to 0
     for (int i = 0; i <= itemCount; i++) K[i][0] = 0;
@@ -93,7 +107,19 @@ int knapsack(int maxWeight, struct Item items[], int itemCount)
         }
     }
 
-    // Print the result of the Knapsack algorithm
+    print_knapsack_result(maxWeight, itemCount, items, K);
+    return K[itemCount][maxWeight];
+}
+
+/**
+ * @brief 0-1 Knapsack algorithm
+ * @param maxWeight the capacity of the knapsack
+ * @param itemCount the number of items(Item structure)
+ * @param items an array of Item structure
+ * @param K the 2D array storing the optimal solution
+ * @returns void
+ */
+void print_knapsack_result(int maxWeight, int itemCount, struct Item items[],int K[][MAXCAPACITY + 1]){
     printf("Knapsack execution result:\n");
     for (int i = 0; i < itemCount + 1; i++)
     {
@@ -109,7 +135,6 @@ int knapsack(int maxWeight, struct Item items[], int itemCount)
     printf("Maximum Value: %d\n", K[itemCount][maxWeight]);
     // Print the combination of items in the knapsack
     print_knapsack_item(maxWeight, items, itemCount, K);
-    return K[itemCount][maxWeight];
 }
 
 /**
@@ -123,13 +148,13 @@ void print_input_struct_Items(struct Item items[], int itemCount)
     printf("Input Item Structures:\n");
     for (int i = 0; i < itemCount; i++)
     {
-        printf("Item %d (Weight: %d, Value: %d)\n", i + 1, items[i].weight,
+        printf("Item %d (Weight: %dkg, Value: %d)\n", i + 1, items[i].weight,
                items[i].value);
     }
     printf("\n");
 }
-/**
 
+/**
  * @brief Print the combination of items in the knapsack
  * @param maxWeight the capacity of the knapsack
  * @param items an array of Item structure
@@ -143,7 +168,7 @@ void print_knapsack_item(int maxWeight, struct Item items[], int itemCount,
     // Backtracking from the final maximum value to track the value changes
     int i = itemCount;  // Number of items (maximum to 0)
     int w = maxWeight;  // Temporary capacity of the knapsack (maximum to 0)
-    printf("Items in the knapsack:\n");
+    printf("Items in the knapsack(%dkg):\n", maxWeight);
     while (i > 0 && w > 0)
     {
         // Check if the i-th item is selected
@@ -151,7 +176,7 @@ void print_knapsack_item(int maxWeight, struct Item items[], int itemCount,
         {
             /* If K[i][j] != K[i-1][j], it means the i-th item is included at
              * the previous (w-items[i-1].weight) weight. */
-            printf("Item %d (Weight: %d, Value: %d)\n", i, items[i - 1].weight,
+            printf("Item %d (Weight: %dkg, Value: %d)\n", i, items[i - 1].weight,
                    items[i - 1].value);
             w = w - items[i - 1].weight;  // Move to the weight excluding the
                                           // i-th item from now on
@@ -160,16 +185,27 @@ void print_knapsack_item(int maxWeight, struct Item items[], int itemCount,
          * included in the knapsack: w remains the same. */
         i--;
     }
+    printf("\n");
 }
 
-static void test()
+/**
+ * @brief Self-test implementations
+ * @returns void
+ */
+static void tests()
 {
-    // Initialize the given items
-    struct Item items[4] = {{5, 10}, {4, 40}, {6, 30}, {3, 50}};
-
+    // test 1
+    // Initialize the given items1
+    struct Item items1[4] = {{5, 10}, {4, 40}, {6, 30}, {3, 50}};
     // Calculate the maximum value that can be obtained in a 10kg knapsack
-    int v = knapsack(MAXCAPACITY, items, 4);
-    assert(v == 90);
+    assert(knapsack(MAXCAPACITY, items1, 4) == 90);
+    printf("=========================\n");
+    // test2
+    // Initialize the given items1
+    struct Item items2[4] = {{6, 13}, {4, 8}, {3, 6}, {5, 12}};
+    // Calculate the maximum value that can be obtained in a 7kg knapsack
+    assert(knapsack(7, items2, 4) == 14);
+
     printf("All tests have passed successfully!\n");
 }
 
@@ -179,6 +215,6 @@ static void test()
  */
 int main()
 {
-    test();
+    tests();
     return 0;
 }
