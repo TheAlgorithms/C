a68d47884e193c267bbf618f3964c93040cf3791
BamaCharanChhandogi <b.c.chhandogi@gmailcom>
2023-08-13 13:45:48 +0530

    added test cases

--- sorting/merge_sort.c
+++ sorting/merge_sort.c
@@ -1,8 +1,9 @@
 /**
  *@author [Bama Charan Chhandogi](https://github.com/BamaCharanChhandogi)
  */
+#include <assert.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 // Function to merge two sorted subarrays into one sorted array
 void merge(int arr[], int left, int mid, int right)
 {
@@ -71,23 +72,47 @@ void merge_sort(int arr[], int left, int right)
     }
 }
 
+#define MAX 100  // Maximum array size for testing
+// Function to check if an array is sorted in ascending order
+int is_sorted(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            return 0;  // Not sorted
+        }
+    }
+    return 1;  // Sorted
+}
+
+// Test function for merge sort
+static void test_merge_sort()
+{
+    int test_arr[MAX];
+
+    // Populate the test array with random integers
+    for (int i = 0; i < MAX; i++)
+    {
+        test_arr[i] = rand() % 1000;  // Using a larger range for better testing
+    }
+
+    // Perform merge sort on the test array
+    merge_sort(test_arr, 0, MAX - 1);
+
+    // Check if the array is sorted
+    assert(is_sorted(test_arr, MAX));
+
+    printf("Merge sort test passed!\n");
+}
+/**
+ * @brief main function
+ * @returns 0 on successful exit
+ **/
 int main()
 {
-    int n;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-
-    int arr[n];
-    printf("Enter the elements: ");
-    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
-
-    printf("Original array: ");
-    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
-
-    merge_sort(arr, 0, n - 1);
-
-    printf("\nSorted array: ");
-    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    // Run the merge sort test function
+    test_merge_sort();
 
     return 0;
 }
