63f80712af376bd652f4ab3d75a50d074605bea0
BenSt099 <ben.steinhauer.de@gmail.com>
2023-08-15 15:40:42 +0200

    feat: add hamming-distance2

---
+++ misc/hamming_distance2.c
@@ -0,0 +1,71 @@
+/**
+ * @file
+ * @author [BenSt099](https://github.com/BenSt099)
+ * @brief Computes the [Hamming distance](https://en.wikipedia.org/wiki/Hamming_distance)
+ * of two Integer-Arrays
+ * @details
+ * In information theory, the Hamming distance between two strings of
+ * equal length is the number of positions at which the corresponding symbols
+ * are different.
+ * @see  
+ */
+
+#include <assert.h>  /// for assert
+#include <stdio.h>   /// for IO operations
+
+/**
+ * @brief Function that calculates the Hamming distance between two Integer-arrays
+ * @param param1 int* Pointer for Hamming distance
+ * @param param2 int length of the two arrays
+ * @param param3 int* first int-array
+ * @param param4 int* second int-array
+ * @returns Pointer to Hamming distance
+ */
+void hamming_distance(int* h_dist, int length, int* s1, int* s2) {
+    int dist = 0;
+    int left = 0;
+    int right = length - 1;
+
+    while(left < right){
+        dist += (int) s1[left] != s2[left];
+        dist += (int) s1[right] != s2[right];
+        ++left;
+        --right;
+    }
+    dist += (length % 2 != 0) * ((int) s1[(length - 1) / 2] != s2[(length - 1) / 2]);
+    *h_dist = dist;
+}
+
+/**
+ * @brief Self-test implementations
+ * @returns void
+ */
+static void test() {
+
+    int s1[10] = {1,2,6,6,8,9,3,4,0,5}; 
+    int s2[10] = {1,2,7,6,8,9,3,2,0,5};
+    int* ptr1 = &s1[0];
+    int* ptr2 = &s2[0];
+    int h_dist;
+    hamming_distance(&h_dist, 10, ptr1, ptr2);
+    assert(h_dist == 2); 
+
+    int s3[13] = {1,2,6,6,8,9,3,4,0,5,6,7,6}; 
+    int s4[13] = {1,2,7,6,8,9,3,2,0,5,6,1,6};
+    int* ptr3 = &s3[0];
+    int* ptr4 = &s4[0];
+    int h_dist2;
+    hamming_distance(&h_dist2, 13, ptr3, ptr4);
+    assert(h_dist2 == 3);
+
+    printf("All tests have successfully passed!\n");
+}
+
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
+int main(void) {
+    test();  // run self-test implementations
+    return 0;
+}
