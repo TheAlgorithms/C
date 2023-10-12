ce07ffc63724d876ffd9fc741840c7e436522480
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-10-12 10:58:45 +0200

    Create GuessMyNumber.c

---
+++ games/GuessMYNumber.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+
+    while (1) {
+        printf("Guess my number between 1-100: ");
+        int number;
+        int r = rand() % 100 + 1;
+
+        while(1){
+            scanf("%d", &number);
+            if (number == r) {
+                printf("Correct number! You guessed it.\n");
+                break;
+            } else if(number>r){
+                printf("Number is to high. Try again.\n");
+            }else{
+                printf("Number is to low. Try again.\n");
+            }
+        }
+    }
+
+    return 0;
+}
