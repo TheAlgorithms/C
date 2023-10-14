fe4d867909a4169b9c6262b83d841bafd7361b02
Aditya Ghosh <adi27.ghosh@gmail.com>
2023-10-14 21:26:25 +0530

    Factorial program in C using recursion

--- math/factorialrecursion.c
+++ math/factorialrecursion.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 
+// This function computes the factorial 
 int factorial(int n)
 {
     if(n == 0)
         return 1;
     else
-        return n * factorial(n-1);
+        return n * factorial(n-1); // factorial is again recursively called from here
 }
 
 int main()
@@ -13,6 +14,6 @@ int main()
     int n;
     printf("Enter a number: ");
     scanf("%d",&n);
-    printf("Factorial: %d", factorial(n));
+    printf("Factorial: %d", factorial(n)); // The factorial function is called from here
     return 0;
 }
