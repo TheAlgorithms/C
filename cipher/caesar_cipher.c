8b760b5a02e5a36d0b1aa937020f99bcedfbdc37
Tuchar Das <dastuchar@gmail.com>
2023-10-31 00:30:07 +0530

    Finalised the code!

--- cipher/caesar_cipher.c
+++ cipher/caesar_cipher.c
@@ -1,63 +1,92 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/**
+ * @file
+ * @brief An [caesar cipher](https://en.wikipedia.org/wiki/Caesar_cipher) is a
+ * letter substitution cipher that uses linear shift to substitute
+ * letters in a message. It is one of the simplest cipher with only 26
+ * possible keys.
+ * @details Given an alphabet of length M with characters with numeric values
+ * 0-(M-1), an arbitrary character x can be transformed with the expression (x
+ * + b) % M into our ciphertext character.
+ * @author [Tuchar Chandra Das](https://github.com/Thunder-Ultra)
+ */
 
-// Function to encrypt a message using the Caesar Cipher
-char* encrypt(char* message, int key)
+#include <stdio.h>   // For standard input/Output
+#include <stdlib.h>  // For dynamic memory management
+#include <string.h>  // For finding the length of a string
+
+/**
+ * @brief Encrypts a given message
+ * @param msg raw message
+ * @param
+ */
+char* encrypt(char* msg, int key)
 {
     int i;
-    char* result = (char*)malloc(strlen(message) + 1);
+    char* result = (char*)malloc(
+        strlen(msg));  // Create a string to store the encrypted string
 
-    for (i = 0; i < strlen(message); i++)
+    for (i = 0; i < strlen(msg); i++)
     {
-        if (message[i] >= 'a' && message[i] <= 'z')
+        if (msg[i] >= 'a' && msg[i] <= 'z')
         {
-            result[i] = 'a' + (message[i] - 'a' + key) % 26;
+            result[i] = 'a' + (msg[i] - 'a' + key) % 26;
         }
-        else if (message[i] >= 'A' && message[i] <= 'Z')
+        else if (msg[i] >= 'A' && msg[i] <= 'Z')
         {
-            result[i] = 'A' + (message[i] - 'A' + key) % 26;
+            result[i] = 'A' + (msg[i] - 'A' + key) % 26;
         }
         else
         {
-            result[i] = message[i];
+            result[i] = msg[i];
         }
     }
 
-    result[i] = '\0';
     return result;
 }
 
-// Function to decrypt a message using the Caesar Cipher
-char* decrypt(char* message, int key)
+/**
+ * @brief Encrypts a given message
+ * @param msg raw message
+ * @param
+ */
+char* decrypt(char* msg, int key)
 {
     int i;
-    char* result = (char*)malloc(strlen(message) + 1);
+    char* result = (char*)malloc(
+        strlen(msg));  // Create string to hold the Decrypted string
 
-    for (i = 0; i < strlen(message); i++)
+    for (i = 0; i < strlen(msg); i++)
     {
-        if (message[i] >= 'a' && message[i] <= 'z')
+        if (msg[i] >= 'a' && msg[i] <= 'z')
         {
-            result[i] = 'a' + (message[i] - 'a' - key + 26) % 26;
+            result[i] = 'a' + (msg[i] - 'a' - key + 26) % 26;
         }
-        else if (message[i] >= 'A' && message[i] <= 'Z')
+        else if (msg[i] >= 'A' && msg[i] <= 'Z')
         {
-            result[i] = 'A' + (message[i] - 'A' - key + 26) % 26;
+            result[i] = 'A' + (msg[i] - 'A' - key + 26) % 26;
         }
         else
         {
-            result[i] = message[i];
+            result[i] = msg[i];
         }
     }
 
-    result[i] = '\0';
     return result;
 }
 
-int main()
+/**
+ * @brief Test a sample string and prints the Raw, Encrypted, and
+ * Unencrypted Message
+ *
+ * @returns void
+ */
+void main()
 {
+    // Some code to sample to check the working of the algorithm
     char message[] = "Hello, World!";
-    int key = 3;
+    printf("Raw Message: %s\n", message);
+    // Here goes the key with which to encrypt the message
+    int key = 7;
 
     char* encrypted_message = encrypt(message, key);
     printf("Encrypted message: %s\n", encrypted_message);
@@ -67,6 +96,4 @@ int main()
 
     free(encrypted_message);
     free(decrypted_message);
-
-    return 0;
 }
