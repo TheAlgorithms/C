5f6b7e92bd50cd4f8f71bbed88c1c64123f74757
Jaspe Micael Ingabire <micaeljaspe@gmail.com>
2024-01-08 15:59:29 +0200

    correction of krishnamurthy.c

--- math/krishnamurthy.c
+++ math/krishnamurthy.c
@@ -1,16 +1,12 @@
-#include <stdio.h> // For I/O ops
-#include <assert.h> //For test cases
+#include <stdio.h> /// For I/O ops
+#include <assert.h> /// For test cases
+#include <stdbool.h> /// For boolean
 
 /**
-SCRIPT DESCRIPTION:
-
  * @description a krishnamurthy number is
  * a number whose sum of the factorial of digits
  * is equal to the number itself.
-
  * @example 145 = 1! + 4! + 5!
-
- * @reference https://www.geeksforgeeks.org/problems/krishnamurthy-number1323/1
 */
 
 
@@ -19,13 +15,14 @@ SCRIPT DESCRIPTION:
  * @brief calculates the factorial of a number
  * @param n the number we want to calculate the factorial
  * @return an integer representing the factorial of n
- */
+*/
 int factorial(int n){
     if (n<2){
 	return n;
     }
-    return n*factorial(n-1);
+    return n * factorial(n-1);
 }
+
 
 /**
  * @brief Calculates the sum of the factorial of each digit
@@ -33,15 +30,12 @@ int factorial(int n){
  * @param sum the sum variable to be incremented 
  * @returns an integer representing that sum
  */
-int get_digit(int n, int sum){
-
+int sum_digit(int n){
     if (n<10){
-	return sum+factorial(n);
+		return factorial(n);
     }
     else{
-	int div = n/10;
-	sum += factorial(n%10);
-	return get_digit(div,sum);
+		return factorial(n%10) + sum_digit(n/10);
     }
 }
 
@@ -49,12 +43,11 @@ int get_digit(int n, int sum){
  * @brief Verifies if a given number
  * is a krishnamurthy number
  * @param n the number to be verified as a krishnamurthy number
- * @returns 1 if n is a krishnamurthy number
- * @returns 0 if n is not a krishnamurthy number
+ * @returns true if n is a krishnamurthy number
+ * @returns false if n is not a krishnamurthy number
  */
-int krishnamurthy(int n){
-    int sum=0;
-    return get_digit(n,sum)==n;
+bool krishnamurthy(int n){
+    return sum_digit(n)==n;
 }
 
 /**
@@ -62,14 +55,19 @@ int krishnamurthy(int n){
  * @return void
  */
 static void tests(){
-    assert(krishnamurthy(1) == 1);
-    assert(krishnamurthy(13) == 0);
-    assert(krishnamurthy(45) == 0);
-    assert(krishnamurthy(145) == 1);
-    assert(krishnamurthy(239) == 0);
+    assert(krishnamurthy(1) == true);
+    assert(krishnamurthy(13) == false);
+    assert(krishnamurthy(45) == false);
+    assert(krishnamurthy(145) == true);
+    assert(krishnamurthy(239) == false);
     printf("All tests were successful!\n");
 }
 
+
+/**
+ * @brief main function
+ * @returns 0 on successful exit
+ */
 int main(){
     tests();
     return 0;
